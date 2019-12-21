4be48b7dc621f217fe8e8c8a2a7122fd551844db
thaicpb <nguyenvietthai1351997@gmail.com>
2019-12-21 13:47:03 +0700

    update search document

---
+++ client/src/modules/manage-form-document/components/HistoryStatistics.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HistoryStatistics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+                HistoryStatistics
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default HistoryStatistics;
