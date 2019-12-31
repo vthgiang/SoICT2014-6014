67f267f0a1f57448598f5abdc61cde200c45b472
hungcuong17 <cuongth.vnist@gmail.com>
2019-12-31 09:46:08 +0800

    fix merge

--- client/src/modules/super-admin-management/manage-user/components/ManageUserTable.jsx
+++ client/src/modules/super-admin-management/manage-user/components/ManageUserTable.jsx
@@ -8,7 +8,7 @@ import DeleteNotificationModal from './DeleteNotificationModal';
 class ManageUserTable extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
         }
         this.editUser = this.editUser.bind(this);
         this.delete = this.delete.bind(this);
@@ -22,40 +22,40 @@ class ManageUserTable extends Component {
         this.props.destroy(id)
     }
 
-    render() { 
+    render() {
         const { user, translate } = this.props;
         console.log("RENDER USER TABLE");
-        return ( 
+        return (
             <React.Fragment>
                 {
                     user.list.length > 0 &&
-                    <table className="table table-bordered table-hover" style={{ marginTop: '50px'}}>
+                    <table className="table table-bordered table-hover" style={{ marginTop: '50px' }}>
                         <thead>
                             <tr>
-                                <th>{ translate('table.name') }</th>
-                                <th>{ translate('table.email') }</th>
-                                <th>{ translate('table.status') }</th>
-                                <th style={{ width: '120px' }}>{ translate('table.action') }</th>
+                                <th>{translate('table.name')}</th>
+                                <th>{translate('table.email')}</th>
+                                <th>{translate('table.status')}</th>
+                                <th style={{ width: '120px' }}>{translate('table.action')}</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                user.list.map( u => (
-                                    <tr 
-                                        key={ u._id }
+                                user.list.map(u => (
+                                    <tr
+                                        key={u._id}
                                         style={{ backgroundColor: u.active ? "white" : "#E2DFE7" }}
                                     >
-                                        <td>{ u.name }</td>
-                                        <td>{ u.email }</td>
-                                        <td>{ u.active ? <p><i className="fa fa-circle text-success" /> Enable</p> : <p><i className="fa fa-circle text-danger" /> Disable</p> }</td>
+                                        <td>{u.name}</td>
+                                        <td>{u.email}</td>
+                                        <td>{u.active ? <p><i className="fa fa-circle text-success" /> Enable</p> : <p><i className="fa fa-circle text-danger" /> Disable</p>}</td>
                                         <td>
-                                            <a className="btn btn-sm btn-primary" data-toggle="modal" href={ `#edit-user-modal-${u._id}` }><i className="fa fa-edit"></i></a>{' '}
-                                            <a className="btn btn-sm btn-danger" data-toggle="modal" href={ `#modal-delete-${u._id}` }><i className="fa fa-trash"></i></a>
-                                            <UserEditForm 
-                                                userEditID={ u._id } 
-                                                email={ u.email }
-                                                username={ u.name }
-                                                active={ u.active }
+                                            <a className="btn btn-sm btn-primary" data-toggle="modal" href={`#edit-user-modal-${u._id}`}><i className="fa fa-edit"></i></a>{' '}
+                                            <a className="btn btn-sm btn-danger" data-toggle="modal" href={`#modal-delete-${u._id}`}><i className="fa fa-trash"></i></a>
+                                            <UserEditForm
+                                                userEditID={u._id}
+                                                email={u.email}
+                                                username={u.name}
+                                                active={u.active}
                                                 editUser={this.editUser}
                                             />
                                             <DeleteNotificationModal
@@ -63,7 +63,7 @@ class ManageUserTable extends Component {
                                                 userEmail={u.email}
                                                 delete={this.delete}
                                             />
-                                            
+
                                         </td>
                                     </tr>
                                 ))
@@ -75,26 +75,20 @@ class ManageUserTable extends Component {
         );
     }
 }
- 
+
 const mapStateToProps = state => {
     return state;
 }
 
 const mapDispatchToProps = (dispatch, props) => {
-    return{
+    return {
         edit: (user) => {
-            dispatch(edit(user)); 
+            dispatch(edit(user));
         },
-<<<<<<< HEAD:client/src/modules/manage-user/components/ManageUserTable.jsx
-        edit: (user) => {
-            dispatch(edit(user)); 
-        },
-=======
->>>>>>> master:client/src/modules/super-admin-management/manage-user/components/ManageUserTable.jsx
         destroy: (id) => {
             dispatch(destroy(id));
         }
     }
 }
 
-export default connect( mapStateToProps, mapDispatchToProps )( withTranslate(ManageUserTable) );
+export default connect(mapStateToProps, mapDispatchToProps)(withTranslate(ManageUserTable));
