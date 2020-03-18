fcf16fcc7a73c91ec4f0d69ca277a403c3b4f3d7
Trinh Tuan-Dat <trinhtuandat.bk@gmail.com>
2020-03-19 00:43:48 +0700

    Fix bug import auth middleware cho module KHoa mới thêm

--- server/modules/task-template-management/task-template-management.route.js
+++ server/modules/task-template-management/task-template-management.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {auth} = require('../../middleware/auth.middleware');
-const {role} = require('../../middleware/auth.middleware');
+const {auth} = require('../../middleware');
 
 const TaskTemplateController = require("../../modules/task-template-management/task-template-management.controller");
 
