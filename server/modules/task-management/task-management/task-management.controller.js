daaff8b9e3bdea399ab92f530e12f3572999027f
thanhdt3009 <thanhdt.vnist@gmail.com>
2020-04-04 15:21:21 +0700

    sua controller va service module taskmanagement, them phan url trong initDB

--- server/modules/task-management/task-management/task-management.controller.js
+++ server/modules/task-management/task-management/task-management.controller.js
@@ -1,5 +1,5 @@
 const TaskManagementService = require('./task-management.service');
-
+const {  LogInfo,  LogError } = require('../../../logs');
 // Điều hướng đến dịch vụ cơ sở dữ liệu của module quản lý công việc
 // Lấy tất cả các công việc
 exports.get = (req, res) => {
@@ -7,46 +7,129 @@ exports.get = (req, res) => {
 }
 
 // Lấy công việc theo id
-exports.getById = (req, res) => {
-    return TaskManagementService.getById(req, res);
+exports.getById =async (req, res) => {
+    try {
+        var task = await TaskManagementService.getById(req.params.id);
+        await LogInfo(req.user.email, ` get task by id `,req.user.company);
+        res.status(200).json(task);
+    } catch (error) {
+        await LogError(req.user.email, ` get task by id `,req.user.company);
+        res.status(400).json({
+            message : error
+        });
+    };
+};
+// Lấy công việc theo chức danh
+exports.getByRole =async (req, res) => {
+try {
+    var tasks= await TaskManagementService.getByRole(req.params.role,req.params.id);
+    await LogInfo(req.user.email, ` get task by role `,req.user.company)
+    res.status(200).json(tasks);
+} catch (error) {
+    await LogError(req.user.email, ` get task by role  `,req.user.company)
+    res.status(400).json({
+        message: error
+    })
 }
 
-// Lấy công việc theo chức danh
-exports.getByRole = (req, res) => {
-    return TaskManagementService.getByRole(req, res);
 }
 
 // Lấy công việc theo vai trò người tạo
-exports.getTaskCreatorByUser = (req, res) => {
-    return TaskManagementService.getTaskCreatorByUser(req, res);
+exports.getTaskCreatorByUser =async (req, res) => {
+    try {
+        var taskCreators = await  TaskManagementService.getTaskCreatorByUser(req.params.perpage,req.params.number,req.params.unit,req.params.status,req.params.user);
+        await LogInfo(req.user.email, ` get task creator by user `,req.user.company)
+        res.status(200).json(taskCreators)
+    } catch (error) {
+        await LogError(req.user.email, ` get task creator by user `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
 
 // Lấy công việc theo vai trò người phê duyệt
-exports.getTaskAccounatableByUser = (req, res) => {
-    return TaskManagementService.getTaskAccounatableByUser(req, res);
+exports.getTaskAccounatableByUser =async (req, res) => {
+    try {
+        var taskAccounatables = await TaskManagementService.getTaskAccounatableByUser(req.params.perpage,req.params.number,req.params.unit,req.params.status,req.params.user);
+        await LogInfo(req.user.email, ` get task accountable by user  `,req.user.company)
+        res.status(200).json(taskAccounatables);
+    } catch (error) {
+        await LogError(req.user.email, ` get task accountable by user `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
 
 // Lấy công việc theo vai trò người hỗ trợ
-exports.getTaskConsultedByUser = (req, res) => {
-    return TaskManagementService.getTaskConsultedByUser(req, res);
+exports.getTaskConsultedByUser =async (req, res) => {
+    try {
+        var taskConsulteds = await  TaskManagementService.getTaskConsultedByUser(req.params.perpage,req.params.number,req.params.unit,req.params.user,req.params.status);
+        await LogInfo(req.user.email, ` get task consulted by user `,req.user.company)
+        res.status(200).json(taskConsulteds)
+    } catch (error) {
+        await LogError(req.user.email, ` get task consulted by user `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
 
 // Lấy công việc theo vai trò người quan sát
-exports.getTaskInformedByUser = (req, res) => {
-    return TaskManagementService.getTaskInformedByUser(req, res);
+exports.getTaskInformedByUser =async (req, res) => {
+    try {
+        var taskInformeds = await TaskManagementService.getTaskInformedByUser(req.params.perpage,req.params.number,req.params.unit,req.params.user,req.params.status);
+        await LogInfo(req.user.email, ` get task informed by user `,req.user.company)
+        res.status(200).json(taskInformeds)
+    } catch (error) {
+        await LogError(req.user.email, ` get task informed by user  `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
 
 // Lấy công việc theo vai trò người thực hiện chính
-exports.getTaskResponsibleByUser = (req, res) => {
-    return TaskManagementService.getTaskResponsibleByUser(req, res);
+exports.getTaskResponsibleByUser =async (req, res) => {
+    try {
+        var taskResponsibles = await TaskManagementService.getTaskResponsibleByUser(req.params.perpage,req.params.number,req.params.unit,req.params.user,req.params.status);
+        await await LogInfo(req.user.email, ` get task responsible by user `,req.user.company)
+        res.status(200).json(taskResponsibles)
+    } catch (error) {
+        await await LogError(req.user.email, ` get task responsible by user `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
-
 // Tạo một công việc mới
-exports.create = (req, res) => {
-    return TaskManagementService.create(req, res);
+exports.create = async (req, res) => {
+    try {
+        var task = await TaskManagementService.create(req.body.parent,req.body.startdate,req.body.enddate,req.body.unit,req.body.creator,req.body.name,req.body.description,req.body.priority,req.body.tasktemplate,req.body.role,req.body.kpi,req.body.responsible,req.body.accounatable,req.body.consulted,req.body.informed); 
+        await LogInfo(req.user.email, ` create task `,req.user.company)
+        res.status(200).json({
+            message: "Create Task Template Successfully!",
+            data: task
+        });
+    } catch (error) {
+        await LogInfo(req.user.email, ` create task  `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
 }
-
 // Xóa một công việc đã thiết lập
-exports.delete = (req, res) => {
-    return TaskManagementService.delete(req, res);
+exports.delete =async (req, res) => {
+    try {
+        TaskManagementService.delete(req.params.id);
+        await LogInfo(req.user.email, ` delete task  `,req.user.company)
+        res.status(200).json("Delete success");
+    } catch (error) {
+        await LogError(req.user.email, ` delete task `,req.user.company)
+        res.status(400).json({
+            message: error
+        })
+    }
+
 }
