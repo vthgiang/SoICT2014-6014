d2f500bd0aff03ef4231496cfba52e8258933fdf
Trinh Tuan-Dat <trinhtuandat.bk@gmail.com>
2020-05-02 14:32:06 +0700

    Xóa nốt các service trùng với các module khác

--- server/modules/kpi/evaluation/dashboard/dashboard.controller.js
+++ server/modules/kpi/evaluation/dashboard/dashboard.controller.js
@@ -1,22 +1,2 @@
-const DashboardKPIMemberService = require('./dashboard.service');
-const EmloyeeKpiManagementService = require('../../employee/management/management.service');
 const { LogInfo, LogError } = require('../../../../logs');
 
-// get all target of member kpi
-exports.getKPIAllMember = async (req, res) => {
-    try {
-        const kpimembers = await DashboardKPIMemberService.getAllEmpoyeeKpiSetsInOrganizationalUnit(req.params);
-        await LogInfo(req.user.email, `Get kpi all member`, req.user.company);
-        res.status(200).json({
-            success: true,
-            messages: ['get_all_kpi_member_success'],
-            content: kpimembers
-        });
-    } catch (error) {
-        await LogError(req.user.email, `Get kpi all  member`, req.user.company);
-        res.status(400).json({
-            messages: ['get_all_kpi_member_fail'],
-            message: error
-        });
-    }
-};
--- server/modules/kpi/evaluation/dashboard/dashboard.route.js
+++ server/modules/kpi/evaluation/dashboard/dashboard.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const DashboardController = require('./dashboard.controller');
 const {auth} = require('../../../../middleware/index');
-// Tìm kiếm KPI nhân viên
-router.get('/all-member/:role/:user/:status/:starttime/:endtime', auth, DashboardController.getKPIAllMember);
+
 
 module.exports = router;
