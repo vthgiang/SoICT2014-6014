0cdc454ab9f6e36fe8dc2a04ed0a88cc2f889ba4
Trinh Tuan-Dat <trinhtuandat.bk@gmail.com>
2020-05-02 22:30:25 +0700

    Server: đổi tên service cho module course

--- server/modules/trainning/course/course.route.js
+++ server/modules/trainning/course/course.route.js
@@ -5,19 +5,19 @@ const { auth } = require('../../../middleware');
 const CourseController = require("./course.controller");
 
 // get all list educationProgram
-router.post('/paginate',auth, CourseController.get);
+router.post('/paginate',auth, CourseController.searchCourses);
 
 // get all list educationProgram by education program
-router.post('/list',auth, CourseController.getByEducation);
+router.post('/list',auth, CourseController.getCoursesOfEducationProgram);
 
 // create a new a educationProgram
-router.post('/',auth, CourseController.create);
+router.post('/',auth, CourseController.createCourse);
 
 // delete a educationProgram
-router.delete('/:id',auth, CourseController.delete);
+router.delete('/:id',auth, CourseController.deleteCourse);
 
 // update a educationProgram
-router.put('/:id',auth, CourseController.update);
+router.put('/:id',auth, CourseController.updateCourse);
 
 
 module.exports = router;
