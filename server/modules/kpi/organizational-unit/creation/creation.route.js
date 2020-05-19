9e5d773afeb7b692348f4f2446625eeaffc392f4
Le Minh Duc <duclm.vnist@gmail.com>
2020-05-19 21:52:53 +0700

    Chuấn hóa giao diện, song ngữ dashboard evaluation kpi + chuẩn hóa service organizational unit kpi

--- server/modules/kpi/organizational-unit/creation/creation.route.js
+++ server/modules/kpi/organizational-unit/creation/creation.route.js
@@ -4,31 +4,31 @@ const KPIUnitController = require("./creation.controller");
 const {auth} = require('../../../../middleware/index');
 
 // Lấy KPI đơn vị hiện tại qua vai trò
-router.get('/current-unit/role/:id',auth, KPIUnitController.getOrganizationalUnitKpiSet);
+router.get('/roles/:id',auth, KPIUnitController.getOrganizationalUnitKpiSet);
 
 // edit kpi unit by id
-router.put('/:id',auth, KPIUnitController.editOrganizationalUnitKpiSet);
+router.patch('/:id',auth, KPIUnitController.editOrganizationalUnitKpiSet);
 
 // delete kpi unit
 router.delete('/:id',auth, KPIUnitController.deleteOrganizationalUnitKpiSet);
 
 // delete target of unit
-router.delete('/target/:kpiunit/:id',auth, KPIUnitController.deleteOrganizationalUnitKpi);
+router.delete('/:kpiunit/organizational-unit-kpis/:id',auth, KPIUnitController.deleteOrganizationalUnitKpi);
 
 // edit status of unit by id
-router.put('/status/:id/:status',auth, KPIUnitController.editOrganizationalUnitKpiSetStatus);
+router.patch('/:id/:status',auth, KPIUnitController.editOrganizationalUnitKpiSetStatus);
 
 // get all parent target kpi of a unit
-router.get('/parent/:id',auth, KPIUnitController.getParentOrganizationalUnitKpiSet);
+router.get('/:id/parent-organizational-unit-kpi-sets',auth, KPIUnitController.getParentOrganizationalUnitKpiSet);
 
 // add a new target of unit
-router.post('/create-target',auth, KPIUnitController.createOrganizationalUnitKpi);
+router.post('/organizational-unit-kpis',auth, KPIUnitController.createOrganizationalUnitKpi);
 
 // edit target of unit by id
-router.put('/target/:id',auth, KPIUnitController.editOrganizationalUnitKpi);
+router.put('/organizational-unit-kpis/:id',auth, KPIUnitController.editOrganizationalUnitKpi);
 
 // Khởi tạo KPI đơn vị(add kpi-unit)
-router.post('/create',auth, KPIUnitController.createOrganizationalUnitKpiSet);
+router.post('/',auth, KPIUnitController.createOrganizationalUnitKpiSet);
 
 
 module.exports = router;
