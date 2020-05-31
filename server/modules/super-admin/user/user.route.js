50e0587a4d7634ef679923411cd6bb9c24959b90
thainv-vnist <thainv.vnist@gmail.com>
2020-05-31 21:42:53 +0700

    thêm api download file - nhận đường dẫn tương đối của file thông qua tham số path trong req.query

--- server/modules/super-admin/user/user.route.js
+++ server/modules/super-admin/user/user.route.js
@@ -8,12 +8,9 @@ router.post("/", auth, UserController.createUser);
 router.get("/:id", auth, UserController.getUser);
 router.patch("/:id", auth, UserController.editUser);
 router.delete("/:id", auth, UserController.deleteUser);
-
 router.get("/same-department/:id", auth, UserController.getAllUsersInSameOrganizationalUnitWithUserRole);
-
 router.get("/users-of-department/:id", auth, UserController.getAllUsersInOrganizationalUnit);
-
 router.get('/:id/organizational-units', auth, UserController.getOrganizationalUnitsOfUser);
-
+router.get("/download-file", auth, UserController.dowloadFile);
 
 module.exports = router;
