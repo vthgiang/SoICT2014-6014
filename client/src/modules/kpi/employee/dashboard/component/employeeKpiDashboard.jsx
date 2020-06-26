2d82a9e3d97edc7193fbe16801570c287764cad6
Nam HoaiVu <namvh.vnist@gmail.com>
2020-06-26 23:59:55 +0700

    thêm chức năng tìm kiếm kết quả kpi cá nhân

--- client/src/modules/kpi/employee/dashboard/component/employeeKpiDashboard.jsx
+++ client/src/modules/kpi/employee/dashboard/component/employeeKpiDashboard.jsx
@@ -9,6 +9,9 @@ import { ResultsOfEmployeeKpiChart } from './resultsOfEmployeeKpiChart';
 import { dashboardEmployeeKpiSetActions } from '../redux/actions';
 import { managerKpiActions } from '../../management/redux/actions';
 
+import { DatePicker } from '../../../../../common-components';
+import Swal from 'sweetalert2';
+
 import CanvasJSReact from '../../../../../chart/canvasjs.react';
 
 
@@ -17,10 +20,20 @@ class DashBoardEmployeeKpiSet extends Component {
     constructor(props) {
         super(props);
 
+        var currentDate = new Date();
+        var currentYear = currentDate.getFullYear();
+        var currentMonth = currentDate.getMonth();
+
+        this.INFO_SEARCH = {
+            startDate: currentYear + '-' + 1,
+            endDate: currentYear + '-' + (currentMonth + 2)
+        }
+
         this.state = {
             showModalCopy: "",
             currentMonth: new Date().getMonth() + 1,
-            currentYear: new Date().getFullYear()
+            startDate: this.INFO_SEARCH.startDate,
+            endDate: this.INFO_SEARCH.endDate
         };
     }
     
@@ -52,17 +65,100 @@ class DashBoardEmployeeKpiSet extends Component {
         modal.style = "display: block; padding-right: 17px;";
     }
 
+    handleSelectMonthStart = (value) => {
+        var month = value.slice(3,7) + '-' + value.slice(0,2);
+        this.INFO_SEARCH.startDate = month;
+    }
+
+    handleSelectMonthEnd = async (value) => {
+        var month = value.slice(3,7) + '-' + (new Number(value.slice(0,2)) + 1);
+        this.INFO_SEARCH.endDate = month;
+    }
+
+    handleSearchData = async () => {
+        var startDate = this.INFO_SEARCH.startDate.split("-");
+        var startdate = new Date(startDate[1], startDate[0], 0);
+        var endDate = this.INFO_SEARCH.endDate.split("-");
+        var enddate = new Date(endDate[1], endDate[0], 28);
+        
+        if (Date.parse(startdate) > Date.parse(enddate)) {
+            Swal.fire({
+                title: "Thời gian bắt đầu phải trước hoặc bằng thời gian kết thúc!",
+                type: 'warning',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Xác nhận'
+            })
+        } else {
+            await this.setState(state => {
+                return {
+                    ...state,
+                    startDate: this.INFO_SEARCH.startDate,
+                    endDate: this.INFO_SEARCH.endDate
+                }
+            })
+        }
+    }
+
     render() {
+        var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+        var defaultEndDate = [month, year].join('-');
+        var defaultStartDate = ['01', year].join('-');
+
         return (
             <React.Fragment>
-                <div className="row">
+                <section className="box">
+                    <div className="box-header with-border">
+                        <h3 className="box-title">Tìm kiếm kết quả KPI cá nhân</h3>
+                    </div>
+
+                    <div className="box-body qlcv">
+                        <div className="col-sm-6 form-inline" >
+                            <label>Từ tháng</label>
+                            <DatePicker 
+                                id="monthStart"      
+                                dateFormat="month-year"             // sử dụng khi muốn hiện thị tháng - năm, mặc định là ngày-tháng-năm 
+                                value={defaultStartDate}                 // giá trị mặc định cho datePicker    
+                                onChange={this.handleSelectMonthStart}
+                                disabled={false}                    // sử dụng khi muốn disabled, mặc định là false
+                            />
+                        </div>
+                        <div className="col-sm-6 form-inline" >
+                            <label>Đến tháng</label>
+                            <DatePicker 
+                                id="monthEnd"      
+                                dateFormat="month-year"             // sử dụng khi muốn hiện thị tháng - năm, mặc định là ngày-tháng-năm 
+                                value={defaultEndDate}                 // giá trị mặc định cho datePicker    
+                                onChange={this.handleSelectMonthEnd}
+                                disabled={false}                    // sử dụng khi muốn disabled, mặc định là false
+                            />
+                        </div>
+                        
+                        <div className="col-sm-6 form-inline qlcv">
+                            <label></label>
+                            <button type="button" className="btn btn-success" onClick={this.handleSearchData}>Tìm kiếm</button>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="row">
                     <div className="col-xs-6">
                         <div className=" box box-primary">
                             <div className="box-header with-border">
-                                <div class="box-title">KPI cá nhân năm {this.state.currentYear}</div>
+                                <div class="box-title">Kết quả KPI cá nhân</div>
                             </div>
                             <div className="box-body">
-                                <ResultsOfEmployeeKpiChart/>
+                                <ResultsOfEmployeeKpiChart
+                                    startDate={this.state.startDate}
+                                    endDate={this.state.endDate}
+                                />
                             </div>
                         </div>
                     </div>
@@ -76,7 +172,7 @@ class DashBoardEmployeeKpiSet extends Component {
                             </div>
                         </div>
                     </div>
-                </div>
+                </section>
             </React.Fragment>
         );
     }
