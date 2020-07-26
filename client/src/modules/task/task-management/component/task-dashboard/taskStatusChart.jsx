fb883387bb03e83a155739bac11a937208b6ea1d
Nam HoaiVu <namvh.vnist@gmail.com>
2020-07-26 17:41:03 +0700

    chuẩn hóa và thêm tùy chọn task-dashboard

--- client/src/modules/task/task-management/component/task-dashboard/taskStatusChart.jsx
+++ client/src/modules/task/task-management/component/task-dashboard/taskStatusChart.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withTranslate } from 'react-redux-multilingual';
+
 import { taskManagementActions } from '../../redux/actions';
+
 import { SelectBox } from '../../../../../common-components/index';
+import { DatePicker } from '../../../../../common-components';
+
+import { withTranslate } from 'react-redux-multilingual';
+
 import c3 from 'c3';
 import 'c3/c3.css';
 
@@ -12,6 +17,10 @@ class TaskStatusChart extends Component {
         super(props);
 
         let { translate } = this.props;
+        let currentDate = new Date();
+        let currentYear = currentDate.getFullYear();
+        let currentMonth = currentDate.getMonth();
+
         this.DATA_STATUS = { NOT_AVAILABLE: 0, QUERYING: 1, AVAILABLE: 2, FINISHED: 3 };
         this.ROLE = { RESPONSIBLE: 1, ACCOUNTABLE: 2, CONSULTED: 3, INFORMED: 4, CREATOR: 5 };
         this.ROLE_SELECTBOX = [
@@ -37,49 +46,107 @@ class TaskStatusChart extends Component {
             }
         ]
 
+        // Sao lưu dữ liệu để sử dụng khi dữ liêu thay đổi
+        this.TASK_PROPS = {
+            responsibleTasks: null,
+            accountableTasks: null,
+            consultedTasks: null,
+            informedTasks: null,
+            creatorTasks: null
+        }
+
+        this.INFO_SEARCH = {
+            role: this.ROLE.RESPONSIBLE,
+            currentMonth: currentYear + '-' + (currentMonth + 1),
+            nextMonth: currentYear + '-' + (currentMonth + 2)
+        }
+
         this.state = {
             userId: localStorage.getItem("userId"),
-            dataStatus: this.DATA_STATUS.QUERYING,
-            role: this.ROLE.RESPONSIBLE,
-            roleName: this.ROLE_SELECTBOX[0].text
-        };
 
-        this.props.getResponsibleTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null, null, null);
-        this.props.getAccountableTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-        this.props.getConsultedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-        this.props.getInformedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-        this.props.getCreatorTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
+            dataStatus: this.DATA_STATUS.NOT_AVAILABLE,
+
+            role: this.INFO_SEARCH.role,
+            currentMonth: this.INFO_SEARCH.currentMonth,
+            nextMonth: this.INFO_SEARCH.nextMonth,
+
+            willUpdate: false       // Khi true sẽ cập nhật dữ liệu vào props từ redux
+        };
+    }
+
+    componentDidMount = () => {
+        this.props.getResponsibleTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth, null, null);
+        this.props.getAccountableTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+        this.props.getConsultedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+        this.props.getInformedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+        this.props.getCreatorTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+        
+        this.setState(state => {
+            return {
+                ...state,
+                dataStatus: this.DATA_STATUS.QUERYING,
+                willUpdate: true       // Khi true sẽ cập nhật dữ liệu vào props từ redux
+            };
+        });
     }
 
     shouldComponentUpdate = async (nextProps, nextState) => {
-        if (nextState.role !== this.state.role) {
-            await this.setState(state => {
+
+        if (nextState.currentMonth !== this.state.currentMonth || nextState.nextMonth !== this.state.nextMonth) {
+            this.props.getResponsibleTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, nextState.currentMonth, nextState.nextMonth, null, null);
+            this.props.getAccountableTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, nextState.currentMonth, nextState.nextMonth);
+            this.props.getConsultedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, nextState.currentMonth, nextState.nextMonth);
+            this.props.getInformedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, nextState.currentMonth, nextState.nextMonth);
+            this.props.getCreatorTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, nextState.currentMonth, nextState.nextMonth);
+
+            this.setState(state => {
                 return {
                     ...state,
-                    role: nextState.role,
+                    dataStatus: this.DATA_STATUS.QUERYING,
+                    willUpdate: true       // Khi true sẽ cập nhật dữ liệu vào props từ redux
                 };
             });
+
+            return false;
+        }
+
+        if (nextState.role !== this.state.role) {
 
             this.pieChart();
         }
 
         if (nextState.dataStatus === this.DATA_STATUS.NOT_AVAILABLE) {
-            this.props.getResponsibleTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null, null, null);
-            this.props.getAccountableTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-            this.props.getConsultedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-            this.props.getInformedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
-            this.props.getCreatorTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, null, null);
+            this.props.getResponsibleTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth, null, null);
+            this.props.getAccountableTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+            this.props.getConsultedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+            this.props.getInformedTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
+            this.props.getCreatorTaskByUser("[]", 1, 100, "[]", "[]", "[]", null, this.state.currentMonth, this.state.nextMonth);
 
             this.setState(state => {
                 return {
                     ...state,
-                    dataStatus: this.DATA_STATUS.QUERYING
+                    dataStatus: this.DATA_STATUS.QUERYING,
+                    willUpdate: true        // Khi true sẽ cập nhật dữ liệu vào props từ redux
                 }
             });
+
             return false
         } else if (nextState.dataStatus === this.DATA_STATUS.QUERYING) {
             // Kiểm tra tasks đã được bind vào props hay chưa
             if (!nextProps.tasks.responsibleTasks || !nextProps.tasks.accountableTasks || !nextProps.tasks.consultedTasks || !nextProps.tasks.informedTasks || !nextProps.tasks.creatorTasks) {
+                /** Sao lưu để sử dụng khi dữ liệu bị thay đổi
+                 *  (Lý do: khi đổi role task, muốn sử dụng dữ liệu cũ nhưng trước đó dữ liệu trong kho redux đã bị thay đổi vì service được gọi ở 1 nơi khác)
+                 */ 
+                if (this.state.willUpdate) {
+                    this.TASK_PROPS = {
+                        responsibleTasks: nextProps.tasks.responsibleTasks,
+                        accountableTasks: nextProps.tasks.accountableTasks,
+                        consultedTasks: nextProps.tasks.consultedTasks,
+                        informedTasks: nextProps.tasks.informedTasks,
+                        creatorTasks: nextProps.tasks.creatorTasks
+                    }
+                }
+
                 return false;           // Đang lấy dữ liệu, ko cần render lại
             };
 
@@ -89,15 +156,17 @@ class TaskStatusChart extends Component {
                     dataStatus: this.DATA_STATUS.AVAILABLE
                 }
             });
+
             return false
-        } else if (nextState.dataStatus === this.DATA_STATUS.AVAILABLE) {
+        } else if (nextState.dataStatus === this.DATA_STATUS.AVAILABLE && this.state.willUpdate) {
 
             this.pieChart();
 
             this.setState(state => {
                 return {
                     ...state,
-                    dataStatus: this.DATA_STATUS.FINISHED
+                    dataStatus: this.DATA_STATUS.FINISHED,
+                    willUpdate: false       // Khi true sẽ cập nhật dữ liệu vào props từ redux
                 }
             });
         }
@@ -106,64 +175,58 @@ class TaskStatusChart extends Component {
     }
 
     handleSelectRole = (value) => {
-        let roleName = this.ROLE_SELECTBOX.filter(x => x.value === Number(value[0])).map(x => x.text);
+        this.INFO_SEARCH.role = Number(value[0])
+    }
 
-        this.setState(state => {
+    handleSelectMonth = (value) => {
+        let nextMonth, currentMonth;
+
+        currentMonth = value.slice(3, 7) + '-' + value.slice(0, 2);
+
+        if (value.slice(0, 2) < 12) {
+            nextMonth = value.slice(3, 7) + '-' + (new Number(value.slice(0, 2)) + 1);
+        } else {
+            nextMonth = (new Number(value.slice(3, 7)) + 1) + '-' + '1';
+        }
+
+        this.INFO_SEARCH.currentMonth = currentMonth;
+        this.INFO_SEARCH.nextMonth = nextMonth;
+    }
+
+    handleSearchData = async () => {
+        await this.setState(state => {
             return {
                 ...state,
-                role: Number(value[0]),
-                roleName: roleName
+                role: this.INFO_SEARCH.role,
+                currentMonth: this.INFO_SEARCH.currentMonth,
+                nextMonth: this.INFO_SEARCH.nextMonth
             }
         })
     }
 
     // Thiết lập dữ liệu biểu đồ
     setDataPieChart = () => {
-        const { tasks, translate } = this.props;
+        const { translate } = this.props;
+
         let dataPieChart, numberOfInprocess = 0, numberOfWaitForApproval = 0, numberOfFinished = 0, numberOfDelayed = 0, numberOfCanceled = 0;
         let listTask;
 
-        let now = new Date();
-        let currentYear = now.getFullYear();
-        let currentMonth = now.getMonth();
-        let beginnigOfNextMonth = new Date(currentYear, currentMonth + 1);
-        let beginningOfMonth = new Date(currentYear, currentMonth);
-
-        if (tasks.responsibleTasks && tasks.accountableTasks && tasks.consultedTasks && tasks.informedTasks && tasks.creatorTasks) {
+        if (this.TASK_PROPS.responsibleTasks && this.TASK_PROPS.accountableTasks && this.TASK_PROPS.consultedTasks && this.TASK_PROPS.informedTasks && this.TASK_PROPS.creatorTasks) {
             if (this.state.role === this.ROLE.RESPONSIBLE) {
-                listTask = tasks.responsibleTasks;
+                listTask = this.TASK_PROPS.responsibleTasks;
             } else if (this.state.role === this.ROLE.ACCOUNTABLE) {
-                listTask = tasks.accountableTasks;
+                listTask = this.TASK_PROPS.accountableTasks;
             } else if (this.state.role === this.ROLE.CONSULTED) {
-                listTask = tasks.consultedTasks;
+                listTask = this.TASK_PROPS.consultedTasks;
             } else if (this.state.role === this.ROLE.INFORMED) {
-                listTask = tasks.informedTasks;
+                listTask = this.TASK_PROPS.informedTasks;
             } else if (this.state.role === this.ROLE.CREATOR) {
-                listTask = tasks.creatorTasks;
+                listTask = this.TASK_PROPS.creatorTasks;
             }
         };
 
-        // // Lọc các task trùng nhau
-        // listTask = responsibleTasks.concat(accountableTasks).concat(consultedTasks).concat(informedTasks).concat(creatorTasks);
-        // listTask.forEach(task => {
-        //     let id = task._id;
-        //     if(!(listIdTask.includes(task._id))) {
-        //         listIdTask.push(task._id)
-        //         listTaskFilter.push(task);
-        //     }
-        // });
-
         if (listTask) {
-            listTask.filter(task => {        // lấy công việc tháng hiện tại
-                if (new Date(task.startDate) < beginnigOfNextMonth && new Date(task.startDate) >= beginningOfMonth) {
-                    return 1;
-                } else if (new Date(task.endDate) < beginnigOfNextMonth && new Date(task.endDate) >= beginningOfMonth) {
-                    return 1;
-                } else if (new Date(task.endDate) >= beginnigOfNextMonth && new Date(task.startDate) < beginningOfMonth) {
-                    return 1;
-                }
-                return 0;
-            }).map(task => {
+            listTask.map(task => {
                 switch (task.status) {
                     case "Inprocess":
                         numberOfInprocess++;
@@ -233,11 +296,23 @@ class TaskStatusChart extends Component {
 
     render() {
         const { translate } = this.props;
+
+        let d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+        let defaultMonth = [month, year].join('-');
+
         return (
             <React.Fragment>
                 <div className="box-body qlcv">
-                    <div className="form-inline">
-                        <label style={{ width: "auto" }}>{translate('task.task_management.role')}</label>
+                    <section className="form-inline">
+                        <label>{translate('task.task_management.role')}</label>
                         <SelectBox
                             id={`roleOfStatusTaskSelectBox`}
                             className="form-control select2"
@@ -247,7 +322,23 @@ class TaskStatusChart extends Component {
                             onChange={this.handleSelectRole}
                             value={this.ROLE_SELECTBOX[0].value}
                         />
-                    </div>
+                    </section>
+
+                    <section className="form-inline">
+                        <div className="form-group">
+                            <label>{translate('kpi.evaluation.employee_evaluation.to')}</label>
+                            <DatePicker
+                                id="monthInStatusTask"
+                                dateFormat="month-year"             
+                                value={defaultMonth}                    
+                                onChange={this.handleSelectMonth}
+                                disabled={false}                   
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button type="button" className="btn btn-success" onClick={this.handleSearchData}>{translate('kpi.evaluation.employee_evaluation.search')}</button>
+                        </div>
+                    </section>
 
                     <section ref="chart"></section>
                 </div>
