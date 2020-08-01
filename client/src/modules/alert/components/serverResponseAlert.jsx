5c8588fc286934df104dcf94c37a507be6349963
Le Minh Duc <duclm.vnist@gmail.com>
2020-08-01 15:34:17 +0700

    Làm phần quản lý tài sản

--- client/src/modules/alert/components/serverResponseAlert.jsx
+++ client/src/modules/alert/components/serverResponseAlert.jsx
@@ -3,28 +3,27 @@ import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
 
 class ServerResponseAlert extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {};
     }
 
     checkTranslateExist = (code) => {
-        const {translate} = this.props;
         const subCode = code.split('.');
-        
-        if (subCode.length === 1 ) {
-            return translate(code) ? true : false;
+
+        if (subCode.length === 1) {
+            return code ? true : false;
         } else if (subCode.length > 1) {
             let codeData = subCode[0];
-            
-            if (!translate(codeData)) {
+
+            if (!codeData) {
                 return false;
             } else {
                 for (let i = 1; i < subCode.length; i++) {
                     codeData = codeData + '.' + subCode[i];
 
-                    if (!translate(codeData)) {
+                    if (!codeData) {
                         return false;
                     }
                 }
@@ -34,25 +33,27 @@ class ServerResponseAlert extends Component {
         }
     }
 
-    render() { 
-        const { translate } = this.props;
-        const { type=null, title=null, content=[] } = this.props;
+    render() {
+        const { type = null, title = null, content = [] } = this.props;
 
-        return ( 
+        return (
             <React.Fragment>
-                <h3>{translate(title)}</h3>
-                    {
-                        content.map((message, i) => {
-                            return this.checkTranslateExist(message) ?
-                                <p key={message}>{content.length > 1 ? `${i+1}. ` : null}{translate(message)}</p>:
-                                <p key={message}>{content.length > 1 ? `${i+1}. ` : null}{message}</p>;
-                        })
-                    }
+                <h3>{title}</h3>
+                {
+                    content.map((message, i) => {
+                        return this.checkTranslateExist(message) ?
+                            <p key={message}>{content.length > 1 ? `${i + 1}. ` : null}{message}</p> :
+                            <p key={message}>{content.length > 1 ? `${i + 1}. ` : null}{message}</p>;
+                    })
+                }
             </React.Fragment>
         );
     }
 }
- 
-const mapStateToProps = state => state;
 
-export default connect( mapStateToProps )( withTranslate(ServerResponseAlert) );
+function mapStateToProps(state) {
+    const { } = state;
+    return {};
+}
+
+export default ServerResponseAlert;
