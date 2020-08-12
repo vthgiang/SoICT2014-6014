b260c2203468acceefda96176e64b2f8c84cea32
Le Minh Duc <duclm.vnist@gmail.com>
2020-08-12 15:58:32 +0700

    Chuẩn hóa code

--- client/src/modules/asset/recommend-procure/components/RecommendProcureCreateForm.jsx
+++ client/src/modules/asset/recommend-procure/components/RecommendProcureCreateForm.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
+
 import { ButtonModal, DatePicker, DialogModal, ErrorLabel, SelectBox } from '../../../../common-components';
+
 import { RecommendProcureFromValidator } from './RecommendProcureFromValidator';
+
 import { RecommendProcureActions } from '../redux/actions';
 import { UserActions } from '../../../super-admin/user/redux/actions';
+
 class RecommendProcureCreateForm extends Component {
     constructor(props) {
         super(props);
@@ -29,10 +33,13 @@ class RecommendProcureCreateForm extends Component {
             day = '' + d.getDate(),
             year = d.getFullYear();
 
-        if (month.length < 2)
+        if (month.length < 2) {
             month = '0' + month;
-        if (day.length < 2)
+        }
+            
+        if (day.length < 2) {
             day = '0' + day;
+        }
 
         return [day, month, year].join('-');
     }
@@ -174,8 +181,8 @@ class RecommendProcureCreateForm extends Component {
 
     // Bắt sự kiện submit form
     save = () => {
-
         let dataToSubmit = { ...this.state, proponent: this.props.auth.user._id }
+
         if (this.isFormValidated() && this.validateExitsRecommendNumber(this.state.recommendNumber) === false) {
             return this.props.createRecommendProcure(dataToSubmit);
         }
@@ -183,11 +190,12 @@ class RecommendProcureCreateForm extends Component {
 
     render() {
         const { _id, translate, recommendProcure, user, auth } = this.props;
-        var userlist = user.list;
         const {
             recommendNumber, dateCreate, equipment, supplier, total, unit, estimatePrice,
             errorOnRecommendNumber, errorOnEquipment, errorOnTotal, errorOnUnit
         } = this.state;
+
+        var userlist = user.list;
 
         return (
             <React.Fragment>
@@ -199,16 +207,21 @@ class RecommendProcureCreateForm extends Component {
                     func={this.save}
                     disableSubmit={!this.isFormValidated() || this.validateExitsRecommendNumber(recommendNumber)}
                 >
+                    {/* Form thêm mới phiếu đề nghị mua sắm thiết bị */}
                     <form className="form-group" id="form-create-recommendprocure">
                         <div className="col-md-12">
+
                             <div className="col-sm-6">
-                                <div className={`form-group ${errorOnRecommendNumber === undefined ? "" : "has-error"}`}>
+                                {/* Mã phiếu */}
+                                <div className={`form-group ${!errorOnRecommendNumber ? "" : "has-error"}`}>
                                     <label>Mã phiếu<span className="text-red">*</span></label>
                                     <input type="text" className="form-control" name="recommendNumber" value={recommendNumber} onChange={this.handleRecommendNumberChange} autoComplete="off"
                                         placeholder="Mã phiếu" />
                                     <ErrorLabel content={errorOnRecommendNumber} />
                                     <ErrorLabel content={this.validateExitsRecommendNumber(recommendNumber) ? <span className="text-red">Mã phiếu đã tồn tại</span> : ''} />
                                 </div>
+
+                                {/* Ngày lập */}
                                 <div className="form-group">
                                     <label>Ngày lập<span className="text-red">*</span></label>
                                     <DatePicker
@@ -217,6 +230,8 @@ class RecommendProcureCreateForm extends Component {
                                         onChange={this.handleDateCreateChange}
                                     />
                                 </div>
+
+                                {/* Người đề nghị */}
                                 <div className={`form-group`}>
                                     <label>Người đề nghị</label>
                                     <div>
@@ -237,6 +252,7 @@ class RecommendProcureCreateForm extends Component {
                                     </div>
                                 </div>
 
+                                {/* Thiết bị đề nghị mua */}
                                 <div className={`form-group ${errorOnEquipment === undefined ? "" : "has-error"}`}>
                                     <label>Thiết bị đề nghị mua<span className="text-red">*</span></label>
                                     <textarea className="form-control" rows="3" style={{ height: 34 }} name="equipment" value={equipment} onChange={this.handleEquipmentChange} autoComplete="off"
@@ -244,21 +260,29 @@ class RecommendProcureCreateForm extends Component {
                                     <ErrorLabel content={errorOnEquipment} />
                                 </div>
                             </div>
+
                             <div className="col-sm-6">
+                                {/* Nhà cung cấp */}
                                 <div className="form-group">
                                     <label>Nhà cung cấp</label>
                                     <input type="text" className="form-control" name="supplier" value={supplier} onChange={this.handleSupplierChange} autoComplete="off" placeholder="Nhà cung cấp" />
                                 </div>
+
+                                {/* Số lượng */}
                                 <div className={`form-group ${errorOnTotal === undefined ? "" : "has-error"}`}>
                                     <label>Số lượng<span className="text-red">*</span></label>
                                     <input type="number" className="form-control" name="total" value={total} onChange={this.handleTotalChange} autoComplete="off" placeholder="Số lượng" />
                                     <ErrorLabel content={errorOnTotal} />
                                 </div>
+
+                                {/* Đơn vị tính */}
                                 <div className={`form-group ${errorOnUnit === undefined ? "" : "has-error"}`}>
                                     <label>Đơn vị tính<span className="text-red">*</span></label>
                                     <input type="text" className="form-control" name="unit" value={unit} onChange={this.handleUnitChange} autoComplete="off" placeholder="Đơn vị tính" />
                                     <ErrorLabel content={errorOnUnit} />
                                 </div>
+
+                                {/* Giá trị dự tính */}
                                 <div className="form-group">
                                     <label>Giá trị dự tính (VNĐ)</label>
                                     <input type="number" className="form-control" name="estimatePrice" value={estimatePrice}
