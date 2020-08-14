96fb7551b4a9295d41268d29988de660b42425db
hungcuong17 <cuongth.vnist@gmail.com>
2020-08-14 13:03:06 +0700

    xoa className thưa

--- client/src/layout/sidebar/components/item.jsx
+++ client/src/layout/sidebar/components/item.jsx
@@ -12,7 +12,7 @@ class Item extends Component {
         const { name, path, icon, translate, pending = false } = this.props;
         return (
             <li className={window.location.pathname === path ? "active" : ""}>
-                <Link to={path} className='threedots'>
+                <Link to={path}>
                     <i className={icon} /> <span>{translate(`menu.${name}`)}</span>
                     {
                         pending && <span className="label label-warning" title="trial - chưa đưa vào hoạt động">trial</span>
