b18d3bc3a28ae11a4b66c36166f8e10fa4957436
thainv-vnist <thainv.vnist@gmail.com>
2020-08-17 11:33:05 +0700

    cập nhật backup dữ liệu

--- server/modules/system-admin/system-setting/systemSetting.service.js
+++ server/modules/system-admin/system-setting/systemSetting.service.js
@@ -1,25 +1,69 @@
-const {backup, restore} = require('../../../helpers/backupHelper');
+const {backup, restore} = require(SERVER_HELPERS_DIR+'/backupHelper');
 const {time} = require('cron');
 const fs = require('fs');
 const exec = require('child_process').exec;
+const {Configuration} = require(SERVER_MODELS_DIR).schema;
+
+exports.getBackupSetting = async() => {
+
+}
 
 exports.backup = async (data, params) => {
     const {auto, schedule} = params;
-    console.log("params", auto, schedule)
     switch(auto) {
         case 'on':
             switch(schedule) {
                 case 'weekly':
-                    console.log("SETUP BACKUP AUTOMATIC WEEKLY");
-                    await AUTO_BACKUP_DATABASE.setTime(time(`${data.second} ${data.minute} ${data.hour} * * ${data.day}`));
+                    let timeWeekly = `${data.second} ${data.minute} ${data.hour} * * ${data.day}`;
+                    let dbWeekly = await Configuration.findOne({database: process.env.DB_NAME});
+                    SERVER_BACKUP_LIMIT = data.limit;
+                    if(dbWeekly !== null){
+                        dbWeekly.backup.time.second = data.second;
+                        dbWeekly.backup.time.minute = data.minute;
+                        dbWeekly.backup.time.hour = data.hour;
+                        dbWeekly.backup.time.date = '*';
+                        dbWeekly.backup.time.month = '*';
+                        dbWeekly.backup.time.day = data.day;
+
+                        dbWeekly.backup.limit = data.limit;
+                        
+                        await dbWeekly.save();
+                    }
+                    await AUTO_BACKUP_DATABASE.setTime(time(timeWeekly));
                     break;
                 case 'monthly':
-                    console.log("SETUP BACKUP AUTOMATIC MONTHLY");
-                    await AUTO_BACKUP_DATABASE.setTime(time(`${data.second} ${data.minute} ${data.hour} ${data.date} * *`));
+                    let timeMonthly = `${data.second} ${data.minute} ${data.hour} ${data.date} * *`;
+                    let dbMonthly = await Configuration.findOne({database: process.env.DB_NAME});
+                    SERVER_BACKUP_LIMIT = data.limit;
+                    if(dbMonthly !== null){
+                        dbMonthly.backup.time.second = data.second;
+                        dbMonthly.backup.time.minute = data.minute;
+                        dbMonthly.backup.time.hour = data.hour;
+                        dbMonthly.backup.time.date = data.date;
+                        dbMonthly.backup.time.month = '*';
+                        dbMonthly.backup.time.day = '*';
+
+                        dbMonthly.backup.limit = data.limit;
+                        await dbMonthly.save();
+                    }
+                    await AUTO_BACKUP_DATABASE.setTime(time(timeMonthly));
                     break;
                 case 'yearly':
-                    console.log("SETUP BACKUP AUTOMATIC YEARLY");
-                    await AUTO_BACKUP_DATABASE.setTime(time(`${data.second} ${data.minute} ${data.hour} ${data.date} ${data.month} *`));
+                    let timeYearly = `${data.second} ${data.minute} ${data.hour} ${data.date} ${data.month} *`;
+                    let dbYearly = await Configuration.findOne({database: process.env.DB_NAME});
+                    SERVER_BACKUP_LIMIT = data.limit;
+                    if(dbYearly !== null){
+                        dbYearly.backup.time.second = data.data.second;
+                        dbYearly.backup.time.minute = data.data.minute;
+                        dbYearly.backup.time.hour = data.data.hour;
+                        dbYearly.backup.time.date = data.data.date;
+                        dbYearly.backup.time.month = data.data.month;
+                        dbYearly.backup.time.day = '*';
+
+                        dbYearly.backup.limit = data.limit;
+                        await dbYearly.save();
+                    }
+                    await AUTO_BACKUP_DATABASE.setTime(time(timeYearly));
                     break;
                 default:
                     break;
@@ -47,8 +91,6 @@ exports.backup = async (data, params) => {
 
 exports.deleteBackup = async (version) => {
     const path = `${SERVER_BACKUP_DIR}/${version}`;
-    console.log("path:", path)
-
     if (fs.existsSync(path)) {
         exec("rm -rf " + path, function (err) { });
         return version;
@@ -74,15 +116,16 @@ exports.getRestoreData = async () => {
         });
     };
     const list = await fs.readdirSync(SERVER_BACKUP_DIR);
-    console.log("listbackup:", list)
     const backupedList = list.map( dir => {
+        const folderInfo = fs.statSync(`${SERVER_BACKUP_DIR}/${dir}`);
         const subPath = `${SERVER_BACKUP_DIR}/${dir}/README.txt`;
         const description = fs.readFileSync(subPath, {encoding:'utf8', flag:'r'});
         
         return {
             version: dir,
             path: `${SERVER_BACKUP_DIR}/${dir}`,
-            description
+            description,
+            createdAt: folderInfo.ctime
         }
     })
 
