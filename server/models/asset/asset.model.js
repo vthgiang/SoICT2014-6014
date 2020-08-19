2c14a3c52ab3a7f85739ed65899e56b91a84a6ea
Trinh Tuan Dat <trinhtuandat.bk@gmail.com>
2020-08-19 23:36:41 +0700

    Thêm nhóm tài sản

--- server/models/asset/asset.model.js
+++ server/models/asset/asset.model.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Company = require('../system-admin/company.model');
 const AssetType = require('./assetType.model');
+const AssetGroup = require('./assetGroup.model');
 const User = require('../auth/user.model');
 
 // Create Schema
 const AssetSchema = new Schema({
+    group: {
+        type: Schema.Types.ObjectId,
+        ref: AssetGroup,
+    },
 
     company: { // công ty
         type: Schema.Types.ObjectId,
