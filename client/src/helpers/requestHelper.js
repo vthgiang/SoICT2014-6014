12d16229f304ca710b4248a83e1c60ab17a4c207
thainv-vnist <thainv.vnist@gmail.com>
2020-08-19 10:02:54 +0700

    cập nhật phần component, bỏ file service thừa

--- client/src/helpers/requestHelper.js
+++ client/src/helpers/requestHelper.js
@@ -33,7 +33,6 @@ const checkErrorAuth = (code) => {
         'service_off',
         'fingerprint_invalid',
         'service_permisson_denied',
-        'url_invalid'
     ];
 
     if (error_auth.indexOf(code) !== -1) return true;
