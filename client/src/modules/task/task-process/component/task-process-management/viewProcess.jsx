fdc0ac1296db185c2e743cbdffa397429fc130b7
thanhdt3009 <thanhdt.vnist@gmail.com>
2020-08-21 15:35:19 +0700

    sua bpmn phan view task

--- client/src/modules/task/task-process/component/task-process-management/viewProcess.jsx
+++ client/src/modules/task/task-process/component/task-process-management/viewProcess.jsx
@@ -6,9 +6,9 @@ import { getStorage } from '../../../../../config';
 import { ModalDetailTask } from "../../../task-management/component/task-dashboard/modalDetailTask";
 import { taskManagementActions } from "../../../task-management/redux/actions";
 import { UserActions } from "../../../../super-admin/user/redux/actions";
-import BpmnViewer from 'bpmn-js';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
 import PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
-import customModule from '../read-only'
+import customModule from './../custom'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
 import './../processDiagram.css'
@@ -31,6 +31,7 @@ class ViewProcess extends Component {
     constructor(props) {
         super(props);
         let { data } = this.props;
+        console.log('dtaa', data);
         this.state = {
             userId: getStorage("userId"),
             currentRole: getStorage('currentRole'),
@@ -42,41 +43,47 @@ class ViewProcess extends Component {
             startDate: "",
             endDate: "",
         }
-        this.viewer = new BpmnViewer({
+        this.modeler = new BpmnModeler({
             additionalModules: [
                 customModule,
-                // { moveCanvas: [ 'value', null ] },
                 { zoomScroll: ['value', ''] }
             ],
         });
         this.generateId = 'viewtaskprocestab';
+        this.modeling = this.modeler.get("modeling")
         this.initialDiagram = data.xmlDiagram;
     }
 
     componentDidMount() {
-        // this.props.getDepartment();
-        // this.props.getAllUsersWithRole();
-        // let { user } = this.props;
-        // let defaultUnit = user && user.organizationalUnitsOfUser && user.organizationalUnitsOfUser.find(item =>
-        //     item.dean === this.state.currentRole
-        //     || item.viceDean === this.state.currentRole
-        //     || item.employee === this.state.currentRole);
-        // if (!defaultUnit && user.organizationalUnitsOfUser && user.organizationalUnitsOfUser.length > 0) { // Khi không tìm được default unit, mặc định chọn là đơn vị đầu tiên
-        //     defaultUnit = user.organizationalUnitsOfUser[0]
-        // }
-        // this.props.getChildrenOfOrganizationalUnits(defaultUnit && defaultUnit._id);
 
+        this.modeler.attachTo('#' + this.generateId);
+        var eventBus = this.modeler.get('eventBus');
+        this.modeler.on('element.click', 1000, (e) => this.interactPopup(e));
+        let infoTask = this.props.infoTask
+        console.log(infoTask)
+        infoTask.forEach(x => {
+            if (x.status === "Finished") {
+                console.log(x.codeInProcess)
+                console.log("hihihi")
+                let element1 = this.modeler.get('elementRegistry')._elements
+                console.log(element1)
+                for (let y in element1) {
+                    console.log(element1[y].element.id)
+                    if (element1[y].element.id === x.codeInProcess) {
+                       
+                         this.modeling.setColor(element1[y].element, {
+                            fill: '#dde6ca',
+                            stroke: '#6b7060'
+                        });
+                    }
+                }
+                // this.modeling.setColor(element1, {
+                //     fill: '#dde6ca',
+                //     stroke: '#6b7060'
+                // });
+            }
+        })
 
-        this.viewer.attachTo('#' + this.generateId);
-
-        var eventBus = this.viewer.get('eventBus');
-        this.viewer.on('element.click', 1000, (e) => this.interactPopup(e));
-
-        // this.viewer.on('shape.remove', 1000, (e) => this.deleteElements(e));
-
-        // this.viewer.on('commandStack.shape.delete.revert', (e) => this.handleUndoDeleteElement(e));
-
-        // this.viewer.on('shape.changed', 1000, (e) => this.changeNameElement(e));
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -119,11 +126,11 @@ class ViewProcess extends Component {
                 defaultUnit = user.organizationalUnitsOfUser[0]
             }
             this.props.getChildrenOfOrganizationalUnits(defaultUnit && defaultUnit._id);
-            this.viewer.importXML(nextProps.data.xmlDiagram, function (err) { });
+            this.modeler.importXML(nextProps.data.xmlDiagram, function (err) { });
             return true;
         }
-        if ( nextProps.data ) {
-            this.viewer.importXML(nextProps.data.xmlDiagram, function (err) { });
+        if (nextProps.data) {
+            this.modeler.importXML(nextProps.data.xmlDiagram, function (err) { });
             return true;
         }
         return true;
@@ -167,7 +174,7 @@ class ViewProcess extends Component {
     }
 
     downloadAsSVG = () => {
-        this.viewer.saveSVG({ format: true }, function (error, svg) {
+        this.modeler.saveSVG({ format: true }, function (error, svg) {
             if (error) {
                 return;
             }
@@ -192,7 +199,7 @@ class ViewProcess extends Component {
     }
 
     downloadAsBpmn = () => {
-        this.viewer.saveXML({ format: true }, function (error, xml) {
+        this.modeler.saveXML({ format: true }, function (error, xml) {
             if (error) {
                 return;
             }
@@ -200,7 +207,7 @@ class ViewProcess extends Component {
     }
 
     downloadAsImage = () => {
-        this.viewer.saveSVG({ format: true }, function (error, svg) {
+        this.modeler.saveSVG({ format: true }, function (error, svg) {
             if (error) {
                 return;
             }
@@ -246,8 +253,8 @@ class ViewProcess extends Component {
 
     handleZoomOut = async () => {
         let zstep = 0.2;
-        let canvas = this.viewer.get('canvas');
-        let eventBus = this.viewer.get('eventBus');
+        let canvas = this.modeler.get('canvas');
+        let eventBus = this.modeler.get('eventBus');
 
         // set initial zoom level
         canvas.zoom(zlevel, 'auto');
@@ -262,14 +269,14 @@ class ViewProcess extends Component {
 
     handleZoomReset = () => {
 
-        let canvas = this.viewer.get('canvas');
+        let canvas = this.modeler.get('canvas');
         canvas.zoom('fit-viewport');
     }
 
     handleZoomIn = async () => {
         let zstep = 0.2;
-        let canvas = this.viewer.get('canvas');
-        let eventBus = this.viewer.get('eventBus');
+        let canvas = this.modeler.get('canvas');
+        let eventBus = this.modeler.get('eventBus');
 
         // set initial zoom level
         canvas.zoom(zlevel, 'auto');
@@ -283,7 +290,7 @@ class ViewProcess extends Component {
 
     exportDiagram = () => {
         let xmlStr;
-        this.viewer.saveXML({ format: true }, function (err, xml) {
+        this.modeler.saveXML({ format: true }, function (err, xml) {
             if (err) {
             }
             else {
@@ -330,68 +337,68 @@ class ViewProcess extends Component {
                     hasSaveButton={false}
                     bodyStyle={{ paddingTop: 0, paddingBottom: 0 }}
                 > */}
-                    <div>
-                        { id !== undefined &&
-                            <ModalDetailTask task={(info && info[`${id}`]) && info[`${id}`]} isProcess={true} />
-                        }
+                <div>
+                    {id !== undefined &&
+                        <ModalDetailTask task={(info && info[`${id}`]) && info[`${id}`]} isProcess={true} />
+                    }
 
-                        <div className={`${isTabPane ? 'is-tabbed-pane' : 'row'}`}>
-                            {/* Quy trình công việc */}
-                            <div className={`contain-border ${isTabPane ? '' : 'col-md-8'}`}>
-                                {/* <div className="tool-bar-xml" }>
+                    <div className={`${isTabPane ? 'is-tabbed-pane' : 'row'}`}>
+                        {/* Quy trình công việc */}
+                        <div className={`contain-border ${isTabPane ? '' : 'col-md-8'}`}>
+                            {/* <div className="tool-bar-xml" }>
                                     <button onClick={this.exportDiagram}>Export XML</button>
                                     <button onClick={this.downloadAsSVG}>Save SVG</button>
                                     <button onClick={this.downloadAsImage}>Save Image</button>
                                     <button onClick={this.downloadAsBpmn}>Download BPMN</button>
                                 </div> */}
-                                <div id={this.generateId}></div>
-                                <div className="row">
-                                    <div className="io-zoom-controls">
-                                        <ul className="io-zoom-reset io-control io-control-list">
-                                            <li>
-                                                <a style={{ cursor: "pointer" }} title="Reset zoom" onClick={this.handleZoomReset}>
-                                                    <i className="fa fa-crosshairs"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a style={{ cursor: "pointer" }} title="Zoom in" onClick={this.handleZoomIn}>
-                                                    <i className="fa fa-plus"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a style={{ cursor: "pointer" }} title="Zoom out" onClick={this.handleZoomOut}>
-                                                    <i className="fa fa-minus"></i>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
+                            <div id={this.generateId}></div>
+                            <div className="row">
+                                <div className="io-zoom-controls">
+                                    <ul className="io-zoom-reset io-control io-control-list">
+                                        <li>
+                                            <a style={{ cursor: "pointer" }} title="Reset zoom" onClick={this.handleZoomReset}>
+                                                <i className="fa fa-crosshairs"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a style={{ cursor: "pointer" }} title="Zoom in" onClick={this.handleZoomIn}>
+                                                <i className="fa fa-plus"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a style={{ cursor: "pointer" }} title="Zoom out" onClick={this.handleZoomOut}>
+                                                <i className="fa fa-minus"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className={`${isTabPane ? '' : 'right-content col-md-4'}`}>
+                        <div className={`${isTabPane ? '' : 'right-content col-md-4'}`}>
 
-                                <div className="box box-solid description">
-                                    { !isTabPane &&
-                                        <div className="box-header with-border">
-                                            {translate('task_template.general_information')}
-                                        </div>
-                                    }
-                                    <div className="box-body">
-
-                                        {/**Các thông tin của mẫu công việc */}
-                                        <dt>Tên quy trình</dt>
-                                        <dd>{processName}</dd>
-
-                                        <dt>Mô tả quy trình</dt>
-                                        <dd>{processDescription}</dd>
-
-                                        <dt>Thời gian thực hiện quy trình</dt>
-                                        <dd>{this.formatDate(startDate)} <i className="fa fa-fw fa-caret-right"></i> {this.formatDate(endDate)}</dd>
+                            <div className="box box-solid description">
+                                {!isTabPane &&
+                                    <div className="box-header with-border">
+                                        {translate('task_template.general_information')}
                                     </div>
+                                }
+                                <div className="box-body">
+
+                                    {/**Các thông tin của mẫu công việc */}
+                                    <dt>Tên quy trình</dt>
+                                    <dd>{processName}</dd>
+
+                                    <dt>Mô tả quy trình</dt>
+                                    <dd>{processDescription}</dd>
+
+                                    <dt>Thời gian thực hiện quy trình</dt>
+                                    <dd>{this.formatDate(startDate)} <i className="fa fa-fw fa-caret-right"></i> {this.formatDate(endDate)}</dd>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
 
                 {/* </DialogModal> */}
