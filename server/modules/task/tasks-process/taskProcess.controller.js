e566f146155408b74f8b935b4fd026c39884d37e
quangnt-vnist <quangnt.vnist@gmail.com>
2020-09-04 08:21:38 +0700

    xóa code thừa, thêm comment phần task-process

--- server/modules/task/tasks-process/taskProcess.controller.js
+++ server/modules/task/tasks-process/taskProcess.controller.js
@@ -1,128 +1,127 @@
 const TaskProcessService = require('./taskProcess.service');
 const { LogInfo, LogError } = require('../../../logs');
 
-
 /**
  * hàm lấy tất cả các process
  * @param {*} req 
  * @param {*} res 
  */
 exports.get = async (req, res) => {
-  if (req.query.type === "template") {
-    this.getAllXmlDiagrams(req, res);
-  }
-  else if (req.query.type === "task") {
-    this.getAllTaskProcess(req, res);
-  }
+	if (req.query.type === "template") {
+		this.getAllXmlDiagrams(req, res);
+	}
+	else if (req.query.type === "task") {
+		this.getAllTaskProcess(req, res);
+	}
 }
 
 /**
  * Lấy tất cả diagram
  */
 exports.getAllXmlDiagrams = async (req, res) => {
-  try {
-    var data = await TaskProcessService.getAllXmlDiagram(req.query);
-    await LogInfo(req.user.email, `get all xml diagram `, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['export thanh công'],
-      content: data
-    });
-  } catch (error) {
-    await LogError(req.user.email, `get all xml diagram `, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['abc'],
-      content: error
-    });
-  }
+	try {
+		var data = await TaskProcessService.getAllXmlDiagram(req.query);
+		await LogInfo(req.user.email, `get all xml diagram `, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['export thanh công'],
+			content: data
+		});
+	} catch (error) {
+		await LogError(req.user.email, `get all xml diagram `, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['abc'],
+			content: error
+		});
+	}
 }
 /**
  * Lấy  diagram theo id
  */
 exports.getXmlDiagramById = async (req, res) => {
-  try {
-    var data = await TaskProcessService.getXmlDiagramById(req.params);
-    await LogInfo(req.user.email, `get all xml diagram `, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['export thanh công'],
-      content: data
-    });
-  } catch (error) {
-    await LogError(req.user.email, `get all xml diagram `, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['abc'],
-      content: error
-    });
-  }
+	try {
+		var data = await TaskProcessService.getXmlDiagramById(req.params);
+		await LogInfo(req.user.email, `get all xml diagram `, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['export thanh công'],
+			content: data
+		});
+	} catch (error) {
+		await LogError(req.user.email, `get all xml diagram `, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['abc'],
+			content: error
+		});
+	}
 }
 /**
  * tạo mới diagram
  */
 exports.createXmlDiagram = async (req, res) => {
-  // try {
-  var data = await TaskProcessService.createXmlDiagram(req.body);
-  await LogInfo(req.user.email, `create xml diagram `, req.user.company);
-  res.status(200).json({
-    success: true,
-    messages: ['export thanh công'],
-    content: data
-  });
-  // } catch (error) {
-  //   await LogError(req.user.email, `create xml diagram `, req.user.company);
-  //   res.status(400).json({
-  //     success: false,
-  //     messages: ['abc'],
-  //     content: error
-  //   });
-  // }
+	try {
+		var data = await TaskProcessService.createXmlDiagram(req.body);
+		await LogInfo(req.user.email, `create xml diagram `, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['export thanh công'],
+			content: data
+		});
+	} catch (error) {
+		await LogError(req.user.email, `create xml diagram `, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['abc'],
+			content: error
+		});
+	}
 }
 
 /**
  * chỉnh sửa mới diagram
  */
 exports.editXmlDiagram = async (req, res) => {
-  try {
-    var data = await TaskProcessService.editXmlDiagram(req.params, req.body);
-    await LogInfo(req.user.email, `edit xml diagram `, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['edit_success'],
-      content: data
-    });
-  } catch (error) {
-    await LogError(req.user.email, `edit xml diagram `, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['edit_fail'],
-      content: error
-    });
-  }
+	try {
+		var data = await TaskProcessService.editXmlDiagram(req.params, req.body);
+		await LogInfo(req.user.email, `edit xml diagram `, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['edit_success'],
+			content: data
+		});
+	} catch (error) {
+		await LogError(req.user.email, `edit xml diagram `, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['edit_fail'],
+			content: error
+		});
+	}
 }
 
 /**
  * xóa diagram
  */
 exports.deleteXmlDiagram = async (req, res) => {
-  try {
-    var data = await TaskProcessService.deleteXmlDiagram(req.params.diagramId, req.query);
+	try {
+		var data = await TaskProcessService.deleteXmlDiagram(req.params.diagramId, req.query);
 
-    await LogInfo(req.user.email, `delete xml diagram `, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['delete_success'],
-      content: data
-    });
-  } catch (error) {
-    await LogError(req.user.email, `edit xml diagram `, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['delete_fail'],
-      content: error
-    });
-  }
+		await LogInfo(req.user.email, `delete xml diagram `, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['delete_success'],
+			content: data
+		});
+	} catch (error) {
+		await LogError(req.user.email, `edit xml diagram `, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['delete_fail'],
+			content: error
+		});
+	}
 }
 
 /**
@@ -131,23 +130,23 @@ exports.deleteXmlDiagram = async (req, res) => {
  * @param {*} res 
  */
 exports.createTaskByProcess = async (req, res) => {
-  // try {
-  console.log("controller")
-  var data = await TaskProcessService.createTaskByProcess(req.params.processId, req.body);
-  await LogInfo(req.user.email, `create_task_by_process`, req.user.company);
-  res.status(200).json({
-    success: true,
-    messages: ['create_task_by_process_success'],
-    content: data,
-  });
-  // } catch (error) {
-  //   await LogError(req.user.email, `create_task_by_process`, req.user.company);
-  //   res.status(400).json({
-  //     success: false,
-  //     messages: ['create_task_by_process_fail'],
-  //     content: error,
-  //   });
-  // }
+	try {
+		console.log("controller")
+		var data = await TaskProcessService.createTaskByProcess(req.params.processId, req.body);
+		await LogInfo(req.user.email, `create_task_by_process`, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['create_task_by_process_success'],
+			content: data,
+		});
+	} catch (error) {
+		await LogError(req.user.email, `create_task_by_process`, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['create_task_by_process_fail'],
+			content: error,
+		});
+	}
 }
 
 /**
@@ -156,40 +155,45 @@ exports.createTaskByProcess = async (req, res) => {
  * @param {*} res 
  */
 exports.getAllTaskProcess = async (req, res) => {
-  try {
-    var data = await TaskProcessService.getAllTaskProcess(req.query);
-    await LogInfo(req.user.email, `create_task_by_process`, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['get_all_task_process_success'],
-      content: data,
-    });
-  } catch (error) {
-    await LogError(req.user.email, `create_task_by_process`, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['create_task_by_process_fail'],
-      content: error,
-    });
-  }
+	try {
+		var data = await TaskProcessService.getAllTaskProcess(req.query);
+		await LogInfo(req.user.email, `create_task_by_process`, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['get_all_task_process_success'],
+			content: data,
+		});
+	} catch (error) {
+		await LogError(req.user.email, `create_task_by_process`, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['create_task_by_process_fail'],
+			content: error,
+		});
+	}
 }
 
 
+/**
+ * cập nhật diagram
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.updateDiagram = async (req, res) => {
-  try {
-    var data = await TaskProcessService.updateDiagram(req.params, req.body);
-    await LogInfo(req.user.email, `update diagram`, req.user.company);
-    res.status(200).json({
-      success: true,
-      messages: ['get_all_task_process_success'],
-      content: data,
-    });
-  } catch (error) {
-    await LogError(req.user.email, `update diagram`, req.user.company);
-    res.status(400).json({
-      success: false,
-      messages: ['create_task_by_process_fail'],
-      content: error,
-    });
-  }
+	try {
+		var data = await TaskProcessService.updateDiagram(req.params, req.body);
+		await LogInfo(req.user.email, `update diagram`, req.user.company);
+		res.status(200).json({
+			success: true,
+			messages: ['get_all_task_process_success'],
+			content: data,
+		});
+	} catch (error) {
+		await LogError(req.user.email, `update diagram`, req.user.company);
+		res.status(400).json({
+			success: false,
+			messages: ['create_task_by_process_fail'],
+			content: error,
+		});
+	}
 }
