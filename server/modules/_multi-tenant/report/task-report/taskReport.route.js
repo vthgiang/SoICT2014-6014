a9c4988c359463ceeab5be68fb3c2acef114a10e
tbinhminh <minhtb.vnist@gmail.com>
2020-09-16 16:55:05 +0700

    chuyển module report qua multi-tenant

---
+++ server/modules/_multi-tenant/report/task-report/taskReport.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { auth } = require(`${SERVER_MIDDLEWARE_DIR}/_multi-tenant`);
+const TaskReportController = require('./taskReport.controller');
+
+router.get('/', auth, TaskReportController.getTaskReports);
+router.get('/:id', auth, TaskReportController.getTaskReportById);
+router.post('/', auth, TaskReportController.createTaskReport);
+router.delete('/:id', auth, TaskReportController.deleteTaskReport);
+router.patch('/:id', auth, TaskReportController.editTaskReport)
+
+module.exports = router;
