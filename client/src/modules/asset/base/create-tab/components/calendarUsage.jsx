deb1baec2cf427bc75caf34468a96e1ce98df0e6
anhptvnist <anhpt.vnist@gmail.com>
2020-09-16 21:11:20 +0700

    thay đổi fullcalendar cho lịch đăng ký sử dụng

--- client/src/modules/asset/base/create-tab/components/calendarUsage.jsx
+++ client/src/modules/asset/base/create-tab/components/calendarUsage.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-  Scheduler,
-  Resources,
-  WeekView,
-  Appointments,
-  AppointmentTooltip,
-  AppointmentForm,
-  DateNavigator,
-  Toolbar
-} from '@devexpress/dx-react-scheduler-material-ui';
-import { blue, green } from '@material-ui/core/colors';
-import IconButton from '@material-ui/core/IconButton';
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import { RecommendDistributeActions } from '../../../user/use-request/redux/actions';
 import './calendarUsage.css';
 
@@ -22,41 +12,12 @@ import './calendarUsage.css';
 class CalendarUsage extends Component {
   constructor(props) {
     super(props);
-
-    // Khởi tạo group cho phiếu đăng ký theo trạng thái
-    const locations = [
-      { text: 'Đã phê duyệt', id: 1, color: blue },
-      { text: 'Chưa phê duyệt', id: 2, color: green },
-    ]
-
     this.state = {
-      defaultCurrentDate: new Date(),
-      resources: [{
-        fieldName: 'status',
-        title: 'Test',
-        instances: locations,
-      }],
-    };
+      weekendsVisible: true,
+      currentEvents: [],
+      nowDate: new Date(),
+    }
   }
-
-  Header = (({
-    children, appointmentData, classes, props, ...restProps
-  }) => (
-      <AppointmentTooltip.Header
-        {...restProps}
-        appointmentData={appointmentData}
-      >
-        { this.props.id == `edit` &&
-          <IconButton
-            onClick={event => this.handleApprove(event, appointmentData.id)}
-          >
-            <i className="material-icons">post_add</i>
-          </IconButton>
-        }
-
-      </AppointmentTooltip.Header>
-    ));
-
   componentDidMount() {
     let data = {
       receiptsCode: "",
@@ -70,73 +31,63 @@ class CalendarUsage extends Component {
     this.props.searchRecommendDistributes(data); // Lấy phiếu đăng ký sử dụng theo tài sản
   }
 
-  handleApprove = async (event, value) => {
-    event.preventDefault();
-    const { recommendDistribute } = this.props;
-    let list, dataRecommendDistribute;
-    if (recommendDistribute && recommendDistribute.listRecommendDistributes) {
-      list = recommendDistribute.listRecommendDistributes;
-      dataRecommendDistribute = list.filter(item => item._id == value);
-    }
-    console.log("ddđ")
-    if (dataRecommendDistribute) {
-      await this.props.updateRecommendDistribute(
-        dataRecommendDistribute[0]._id,
-        {
-          recommendNumber: dataRecommendDistribute[0].recommendNumber,
-          dateCreate: dataRecommendDistribute[0].dateCreate,
-          proponent: dataRecommendDistribute[0].proponent, // Người đề nghị
-          reqContent: dataRecommendDistribute[0].reqContent,
-          asset: dataRecommendDistribute[0].asset,
-          dateStartUse: dataRecommendDistribute[0].dateStartUse,
-          dateEndUse: dataRecommendDistribute[0].dateEndUse,
-          approver: dataRecommendDistribute[0].approver, // Người phê duyệt
-          note: dataRecommendDistribute[0].note,
-          status: "Đã phê duyệt",
-        })
 
-      let data = {
-        receiptsCode: "",
-        month: "",
-        reqUseStatus: null,
-        page: 0,
-        limit: 5,
-        managedBy: this.props.managedBy ? this.props.managedBy : '',
-        assetId: this.props.id
-      }
-      await this.props.searchRecommendDistributes(data);
+  handleWeekendsToggle = () => {
+    this.setState({
+      weekendsVisible: !this.state.weekendsVisible
+    })
+  }
+
+  handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        // id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
     }
   }
 
-  handleEdit = async (value) => {
-    await this.setState(state => {
-      return {
-        ...state,
-        currentRow: value
-      }
-    });
-    window.$('#modal-edit-recommenddistributemanage').modal('show');
+  //   handleEventClick = (clickInfo) => {
+  //     if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+  //       clickInfo.event.remove()
+  //     }
+  //   }
+
+  handleEvents = (events) => {
+    this.setState({
+      currentEvents: events
+    })
   }
 
 
 
-  formatDate(value) {
-    let partTime, partDate, date, time;
-    if (value.length > 12 && value.length < 24) {
-      partTime = value.split(' ');
-      partDate = partTime[2].split('-');
-      date = [partDate[2], partDate[1], partDate[0]].join('-');
-      time = [date, partTime[0], partTime[1]].join(' ');
-    } else {
-      time = value
-    }
-    return time;
+  renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
   }
-
-
+  renderSidebarEvent(event) {
+    return (
+      <li key={event.id}>
+        <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+        <i>{event.title}</i>
+      </li>
+    )
+  }
   render() {
     const { recommendDistribute, user } = this.props;
-    const { defaultCurrentDate, resources } = this.state;
+    // const { defaultCurrentDate, resources } = this.state;
     let listRecommendDistributes, data = [], userlist = user.list;
 
     if (recommendDistribute && recommendDistribute.listRecommendDistributes) {
@@ -145,10 +96,10 @@ class CalendarUsage extends Component {
         let recommendDistribute;
         recommendDistribute = {
           id: listRecommendDistributes[i]._id,
-          status: listRecommendDistributes[i].status == "Chờ phê duyệt" ? 2 : 1,
+          color: listRecommendDistributes[i].status == "Chờ phê duyệt" ? 'green' : (listRecommendDistributes[i].status == "Đã phê duyệt" ? 'blue': 'yellow'),
           title: listRecommendDistributes[i].proponent.name,
-          startDate: new Date(this.formatDate(listRecommendDistributes[i].dateStartUse)),
-          endDate: new Date(this.formatDate(listRecommendDistributes[i].dateEndUse)),
+          start: listRecommendDistributes[i].dateStartUse,
+          end: listRecommendDistributes[i].dateEndUse,
         }
         data.push(recommendDistribute);
       }
@@ -158,47 +109,41 @@ class CalendarUsage extends Component {
       data.push({
         id: 1,
         title: userlist.filter(item => item._id === this.props.usageLogs[i].usedByUser).pop() ? userlist.filter(item => item._id === this.props.usageLogs[i].usedByUser).pop().name : "Chưa có đối tượng sử dụng",
-        status: 1,
-        startDate: new Date(this.props.usageLogs[i].startDate),
-        endDate: new Date(this.props.usageLogs[i].endDate),
+        color: 'blue',
+        start: this.props.usageLogs[i].startDate,
+        end: this.props.usageLogs[i].endDate,
       })
     }
-
+    console.log("-----", data)
     return (
-      <Paper>
-        <Scheduler
-          data={data}
-          height={500}
-        >
-          {/* Điều chỉnh lịch */}
-          <Toolbar
-            defaultCurrentDate={defaultCurrentDate}
+      <div className='demo-app'>
+        <div className='demo-app-main'>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'timeGridWeek'
+            }}
+            initialView='timeGridWeek'
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            now={this.state.nowDate}
+            weekends={this.state.weekendsVisible}
+            initialEvents={data} // alternatively, use the `events` setting to fetch from a feed
+            select={this.handleDateSelect}
+            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
           />
-          <ViewState
-            defaultCurrentDate={defaultCurrentDate}
-          />
-          <DateNavigator />
-          <WeekView
-            startDayHour={7}
-            endDayHour={23}
-          />
-          {/* Hiển thị dữ liệu và form */}
-          <Appointments />
-          <AppointmentTooltip
-            headerComponent={this.Header}
-            showCloseButton
-          />
-          <AppointmentForm />
-          {/* Phân nhóm đăng ký sử dụng */}
-          <Resources
-            data={resources}
-            mainResourceName="status"
-          />
-        </Scheduler>
-      </Paper>
-    );
+        </div>
+      </div>
+    )
   }
 }
+
+
+
 
 function mapState(state) {
   const { user, department, recommendDistribute } = state;
