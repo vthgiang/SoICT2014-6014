792961004e6c68ba2deae2f89a42122d0c1576fd
thainv-vnist <thainv.vnist@gmail.com>
2020-09-17 11:43:48 +0700

    cập nhật seed dữ liệu theo multi-tenant

--- server/models/_multi-tenant/index.js
+++ server/models/_multi-tenant/index.js
@@ -1,32 +1,14 @@
 module.exports = {
-    // asset
-    Asset: require('./asset/asset.model'),
-    AssetPurchaseRequest: require('./asset/assetPurchaseRequest.model'),
-    AssetType: require('./asset/assetType.model'),
-    AssetUseReques: require('./asset/assetUseRequest.model'),
-
-    // auth
     Privilege: require('./auth/privilege.model'),
     Role: require('./auth/role.model'),
     User: require('./auth/user.model'),
     UserRole: require('./auth/userRole.model'),
 
-    // common
-    Location: require('./common/location.model'),
-
-    // crm
-    CrmCare: require('./crm/crmCare.model'),
-    CrmCustomer: require('./crm/crmCustomer.model'),
-    CrmGroup: require('./crm/crmGroup.model'),
-    CrmLiability: require('./crm/crmLiability.model'),
-
-    // document
     Document: require('./document/document.model'),
-    DocumentArchive: require('./document/documentArchive.model'),
     DocumentCategory: require('./document/documentCategory.model'),
     DocumentDomain: require('./document/documentDomain.model'),
-    
-    // human-resource
+    DocumentArchive: require('./document/documentArchive.model'),
+
     AnnualLeave: require('./human-resource/annualLeave.model'),
     Commendation: require('./human-resource/commendation.model'),
     Discipline: require('./human-resource/discipline.model'),
@@ -35,58 +17,63 @@ module.exports = {
     Salary: require('./human-resource/salary.model'),
     Timesheet: require('./human-resource/timesheet.model'),
 
-    // kpi
     EmployeeKpi: require('./kpi/employeeKpi.model'),
     EmployeeKpiSet: require('./kpi/employeeKpiSet.model'),
     OrganizationalUnitKpi: require('./kpi/organizationalUnitKpi.model'),
     OrganizationalUnitKpiSet: require('./kpi/organizationalUnitKpiSet.model'),
 
-    // notification
-    ManualNotification: require('./notification/manualNotification.model'),
     Notification: require('./notification/notification.model'),
+    ManualNotification: require('./notification/manualNotification.model'),
 
-    // order
-    Order: require('./order/order.model'),
-
-    // plan
-    Plan: require('./plan/plan.model'),
-
-    // report
-    TaskReport: require('./report/taskReport.model'),
-
-    // super-admin
     Component: require('./super-admin/component.model'),
     Link: require('./super-admin/link.model'),
     OrganizationalUnit: require('./super-admin/organizationalUnit.model'),
     RoleType: require('./super-admin/roleType.model'),
 
-    // system-admin
     Company: require('./system-admin/company.model'),
     Configuration: require('./system-admin/configuration.model'),
-    ImportConfiguration: require('./system-admin/importConfiguration.model'),
-    RootRole: require('./system-admin/rootRole.model'),
     SystemComponent: require('./system-admin/systemComponent.model'),
     SystemLink: require('./system-admin/systemLink.model'),
+    RootRole: require('./system-admin/rootRole.model'),
 
-    // task
-    ProcessTemplate: require('./task/processTemplate.model'),
     Task: require('./task/task.model'),
     TaskComment: require('./task/taskComment.model'),
     TaskFile: require('./task/taskFile.model'),
     TaskHistory: require('./task/taskHistory.model'),
-    TaskProcess: require('./task/taskProcess.model'),
     TaskResult: require('./task/taskResult.model'),
     TaskResultInformation: require('./task/taskResultInformation.model'),
     TaskTemplate: require('./task/taskTemplate.model'),
     TaskTemplateInformation: require('./task/taskTemplateInformation.model'),
     TimesheetLog: require('./task/timesheetLog.model'),
+    TaskProcess: require("./task/taskProcess.model"),
+    ProcessTemplate: require("./task/processTemplate.model"),
 
-    // training
     Course: require('./training/course.model'),
     EducationProgram: require('./training/educationProgram.model'),
     EmployeeCourse: require('./training/employeeCourse.model'),
+    ImportConfiguraion: require('./system-admin/importConfiguration.model'),
 
+//asset
+    Asset: require('./asset/asset.model'), //tài sản
+    AssetType: require('./asset/assetType.model'), //loại tài sản
+    RecommendProcure: require('./asset/assetPurchaseRequest.model'), //đề nghị mua sắm thiết bị
+    RecommendDistribute: require('./asset/assetUseRequest.model'), //đề nghị cấp phát thiết bị
+//report
+    TaskReport: require('./report/taskReport.model'),
 
-    // warehouse
+//material
     Material: require('./warehouse/material.model'),
+// Customer Management
+    CrmCustomer: require('./crm/crmCustomer.model'),
+    CrmCare: require('./crm/crmCare.model'),
+    CrmGroup: require('./crm/crmGroup.model'),
+    CrmLiability: require('./crm/crmLiability.model'),
+
+//order
+    Order: require("./order/order.model"),
+
+    Location: require('./common/location.model'),
+
+// plan
+    Plan: require('./plan/plan.model'),
 }
