e60fe4bbd54e306248b4a982215626da384c42e4
linhbun99 <linhnq.vnist@gmail.com>
2020-09-19 16:58:18 +0700

    componet Upload file

--- client/src/modules/asset/base/create-tab/components/fileAddModal.jsx
+++ client/src/modules/asset/base/create-tab/components/fileAddModal.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
 
-import { DialogModal, ButtonModal, ErrorLabel } from '../../../../../common-components';
+import { DialogModal, ButtonModal, ErrorLabel, UploadFile } from '../../../../../common-components';
 
 import { AssetCreateValidator } from './combinedContent';
 
@@ -18,26 +18,79 @@ class FileAddModal extends Component {
     }
 
     // Bắt sự kiện thay đổi file đính kèm
+    // handleChangeFile = (e) => {
+    //     var file = e.target.files[0];
+
+    //     if (file) {
+    //         var url = URL.createObjectURL(file);
+    //         var fileLoad = new FileReader();
+    //         fileLoad.readAsDataURL(file);
+    //         fileLoad.onload = () => {
+    //             let item = {
+    //                 fileName: file.name,
+    //                 url: url,
+    //                 fileUpload: file,
+    //             }
+    //             this.setState(state => {
+    //                 return {
+    //                     ...state,
+    //                     files: [...state.files, item],
+    //                 }
+    //             })
+    //         };
+    //     } else {
+    //         let item = {
+    //             fileName: "",
+    //             url: "",
+    //             fileUpload: ""
+    //         }
+
+    //         this.setState(state => {
+    //             return {
+    //                 ...state,
+    //                 files: [...state.files, item],
+    //             }
+    //         })
+    //     }
+    // }
+
+    // // Bắt sự kiện xóa file đính kèm
+    // handleDeleteFile = (name) => {
+    //     const { files } = this.state;
+    //     let newfiles = files.filter((item) => item.fileName !== name);
+
+    //     this.setState(state => {
+    //         return {
+    //             ...state,
+    //             files: newfiles,
+    //         }
+    //     })
+    // }
+    // Bắt sự kiện thay đổi file đính kèm
     handleChangeFile = (e) => {
-        var file = e.target.files[0];
+        let file = e.target.files;
 
         if (file) {
-            var url = URL.createObjectURL(file);
-            var fileLoad = new FileReader();
-            fileLoad.readAsDataURL(file);
-            fileLoad.onload = () => {
-                let item = {
-                    fileName: file.name,
-                    url: url,
-                    fileUpload: file,
-                }
-                this.setState(state => {
-                    return {
-                        ...state,
-                        files: [...state.files, item],
+            for (let i = 0; i < file.length; i++) {
+                if (file[i]) {
+                    var url = window.URL.createObjectURL(file[i]);
+                    var fileLoad = new FileReader();
+                    fileLoad.readAsDataURL(file[i]);
+                    fileLoad.onload = () => {
+                        let item = {
+                            fileName: file[i].name,
+                            url: url,
+                            fileUpload: file[i],
+                        }
+                        this.setState(state => {
+                            return {
+                                ...state,
+                                files: [...state.files, item],
+                            }
+                        })
                     }
-                })
-            };
+                }
+            }
         } else {
             let item = {
                 fileName: "",
@@ -66,7 +119,6 @@ class FileAddModal extends Component {
             }
         })
     }
-
     // Bắt sự kiên thay đổi tên tài liệu
     handleNameFileChange = (e) => {
         let { value } = e.target;
@@ -153,16 +205,25 @@ class FileAddModal extends Component {
 
                         {/* File đính kèm */}
                         <div className="form-group">
+                            <label htmlFor="">Chọn tài liệu</label>
+                            <div><UploadFile
+                                files={this.state.files}
+                                handleChangeFile={this.handleChangeFile}
+                                handleDeleteFile={this.handleDeleteFile}
+                            /></div>
+                        </div>
+
+                        {/* <div className="form-group">
                             <label htmlFor="file">{translate('asset.general_information.attached_file')}</label>
-                            {/* <input type="file" style={{ height: 34, paddingTop: 2 }} className="form-control" name="file" onChange={this.handleChangeFile} /> */}
+                            <input type="file" style={{ height: 34, paddingTop: 2 }} className="form-control" name="file" onChange={this.handleChangeFile} />
                             <br />
                             <div className="upload btn btn-primary">
                                 <i className="fa fa-folder"></i>
                                 {" " + translate('document.choose_file')}
                                 <input className="upload" type="file" name="file" onChange={this.handleChangeFile} />
                             </div>
-                        </div>
-                        <ul style={{ listStyle: 'none' }}>
+                        </div> */}
+                        {/* <ul style={{ listStyle: 'none' }}>
                             {files.map((child, index) => {
                                 return (
                                     <React.Fragment>
@@ -174,7 +235,7 @@ class FileAddModal extends Component {
                                     </React.Fragment>
                                 )
                             })}
-                        </ul>
+                        </ul> */}
                     </form>
                 </DialogModal>
             </React.Fragment>
