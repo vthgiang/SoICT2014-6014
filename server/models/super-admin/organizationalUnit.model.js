a40bf515ea6472647f37a3b2aa536d2ba7cea009
thainv-vnist <thainv.vnist@gmail.com>
2020-11-20 11:43:27 +0700

    thêm trang quản lý tài liệu cho từng đơn vị

--- server/models/super-admin/organizationalUnit.model.js
+++ server/models/super-admin/organizationalUnit.model.js
@@ -1,40 +1,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const OrganizationalUnitSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+const OrganizationalUnitSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        deans: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Role",
+            },
+        ],
+        viceDeans: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Role",
+            },
+        ],
+        employees: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Role",
+            },
+        ],
+        parent: {
+            type: Schema.Types.ObjectId,
+            replies: this,
+        },
+        company: {
+            type: Schema.Types.ObjectId,
+            ref: "Company",
+        },
     },
-    description: {
-        type: String
-    },
-    deans: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Role'  
-    }],
-    viceDeans: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Role'
-    }],
-    employees: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Role'
-    }],
-    parent: {
-        type: Schema.Types.ObjectId,
-        replies: this
-    },
-    company: {
-        type: Schema.Types.ObjectId,
-        ref: 'Company'
-    },
-},{
-    timestamps: true
-});
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = (db) => {
-    if(!db.models.OrganizationalUnit)
-        return db.model('OrganizationalUnit', OrganizationalUnitSchema);
+    if (!db.models.OrganizationalUnit)
+        return db.model("OrganizationalUnit", OrganizationalUnitSchema);
     return db.models.OrganizationalUnit;
-}
+};
