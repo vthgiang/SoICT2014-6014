8c92300d4c4b5c6113bc76c8a5919890f0047d0a
minhtbvnist <67574123+minhtbvnist@users.noreply.github.com>
2020-12-01 11:53:14 +0700

    update model customer + thêm service lấy point của khách hàng

--- server/modules/crm/customer/customer.route.js
+++ server/modules/crm/customer/customer.route.js
@@ -11,9 +11,12 @@ const data = [
 router.get('/', auth, CustomerController.getCustomers);
 
 router.get('/:id', auth, CustomerController.getCustomerById);
+router.get('/:id/point', auth, CustomerController.getCustomerPoint);
+
 router.post('/', auth, uploadFile([{ name: 'file', path: '/crm/customer-files' }], 'array'), CustomerController.createCustomer);
 router.post('/imports', auth, CustomerController.importCustomers);
-router.patch('/:id', auth, uploadFile(data,'fields'), CustomerController.editCustomer);
+router.patch('/:id', auth, uploadFile(data, 'fields'), CustomerController.editCustomer);
+router.patch('/:id/point', auth, CustomerController.editCustomerPoint);
 router.delete('/:id', auth, CustomerController.deleteCustomer);
 
 module.exports = router;
