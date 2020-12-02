ce347f6b28aafdde7f8e76d40e0f23969f643f68
Trinh Tuan Dat <trinhtuandat.bk@gmail.com>
2020-12-02 22:01:31 +0700

    Chuẩn hóa code selectBox

--- client/src/common-components/src/select-box/selectBox.jsx
+++ client/src/common-components/src/select-box/selectBox.jsx
@@ -230,12 +230,14 @@ class SelectBox extends Component {
         return false;;
     }
 
-    checkValue = (value, multiple) => {
+    formatValue = (value, multiple) => {
         if (!multiple && !value) {
             return "";
         } else if (multiple && !value) {
             return [];
-        } else return value;
+        } else {
+            return value;
+        }
     }
 
     render() {
@@ -246,7 +248,7 @@ class SelectBox extends Component {
         return (
             <React.Fragment>
                 <div className="select2">
-                    <select className={className} style={style} ref="select" value={this.checkValue(value, multiple)} id={id} multiple={multiple} onChange={() => { }} disabled={disabled}>
+                    <select className={className} style={style} ref="select" value={this.formatValue(value, multiple)} id={id} multiple={multiple} onChange={() => { }} disabled={disabled}>
                         {!searching &&
                             <React.Fragment>
                                 {options.placeholder !== undefined && multiple === false && <option></option>} {/*Ở chế độ single selection, nếu muốn mặc định không chọn gì*/}
