1e21da72b88562ee59d5d3b2adddc89e396719ab
thainv-vnist <thainv.vnist@gmail.com>
2020-12-02 16:32:38 +0700

    sửa lỗi lấy danh sách tài sản có thông tin về khấu hao

--- server/modules/asset/asset-management/asset.service.js
+++ server/modules/asset/asset-management/asset.service.js
@@ -127,6 +127,7 @@ exports.getAssetInforById = async (portal, id) => {
  * @params : dữ liệu key tìm kiếm
  */
 exports.searchAssetProfiles = async (portal, company, params) => {
+    let {getType} = params;
     let keySearch = {};
     if (company) {
         keySearch = { ...keySearch, company: company };
@@ -294,10 +295,30 @@ exports.searchAssetProfiles = async (portal, company, params) => {
         keySearch = { ...keySearch, "incidentLogs.statusIncident": { $in: params.incidentStatus } };
     }
 
+    let totalList = 0, listAssets = [];
     // Lấy danh sách tài sản
-    let totalList = await Asset(connect(DB_CONNECTION, portal)).countDocuments(keySearch);
-    let listAssets = await Asset(connect(DB_CONNECTION, portal)).find(keySearch).populate({ path: 'assetType' })
-        .sort({ 'createdAt': 'desc' }).skip(params.page).limit(params.limit);
+    if(getType === 'depreciation') {
+        keySearch = {
+            ...keySearch,
+            $or: [
+                { depreciationType: {$ne: 'none'} },
+                { cost: {$gt: 0} },
+                { usefulLife: {$gt: 0} },
+                { residualValue: {$gt: 0} },
+                { rate: {$gt: 0} },
+                { estimatedTotalProduction: {$gt: 0} },
+                { startDepreciation: {$ne: null} },
+            ]
+        }
+        totalList = await Asset(connect(DB_CONNECTION, portal)).countDocuments(keySearch);
+        listAssets = await Asset(connect(DB_CONNECTION, portal)).find(keySearch).populate({ path: 'assetType' })
+            .sort({ 'createdAt': 'desc' }).skip(params.page).limit(params.limit);
+        console.log('list depreciation', listAssets)
+    }else{
+        totalList = await Asset(connect(DB_CONNECTION, portal)).countDocuments(keySearch);
+        listAssets = await Asset(connect(DB_CONNECTION, portal)).find(keySearch).populate({ path: 'assetType' })
+            .sort({ 'createdAt': 'desc' }).skip(params.page).limit(params.limit);
+    }
     return { data: listAssets, totalList }
 }
 
