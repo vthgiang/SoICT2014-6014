dcdb91b8ded12c42cf3272cfeb5079ab97098e74
thainv-vnist <thainv.vnist@gmail.com>
2020-12-02 09:24:18 +0700

    bỏ ràng buộc nhóm tài sản, sửa lỗi hiển thị ngày

--- client/src/modules/asset/admin/asset-information/components/assetManagement.jsx
+++ client/src/modules/asset/admin/asset-information/components/assetManagement.jsx
@@ -62,6 +62,7 @@ class AssetManagement extends Component {
 
     // Function format dữ liệu Date thành string
     formatDate(date, monthYear = false) {
+        if (!date) return null;
         var d = new Date(date),
             month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
