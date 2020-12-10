ec719b563895a35a9a09166bf00a7db0a43fcb44
Nam HoaiVu <namvh.vnist@gmail.com>
2020-12-10 23:03:42 +0700

    fix bug quill editor

--- client/src/common-components/src/quill-editor/quillEditor.jsx
+++ client/src/common-components/src/quill-editor/quillEditor.jsx
@@ -5,6 +5,8 @@ import parse from 'html-react-parser';
 
 import { configQuillEditor } from './configQuillEditor';
 import { ToolbarQuillEditor } from './toolbarQuillEditor';
+
+import './quillEditor.css';
 class QuillEditor extends Component {
     constructor(props) {
         super(props);
@@ -87,9 +89,8 @@ class QuillEditor extends Component {
                 }
             });
 
-            // Custom icon insert table
-            let insertTable = document.querySelector('#insert-table');
-            insertTable.addEventListener('click', () => {
+            // Custom insert table
+            window.$(`#insert-tabletoolbar${id}`).click(() => {
                 let table = quill.getModule('table');
                 table.insertTable(3, 3);
             });
