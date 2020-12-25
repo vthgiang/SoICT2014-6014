64016211ba85d396eb2052e46f4e1622c8f6cb76
thainv-vnist <thainv.vnist@gmail.com>
2020-12-25 08:10:14 +0700

    làm mẫu xác thực gọi api trên từng trang, thêm tab quản lý hộ gia đình

--- client/src/modules/human-resource/profile/employee-create/components/familyMemberTab.jsx
+++ client/src/modules/human-resource/profile/employee-create/components/familyMemberTab.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import withTranslate from 'react-redux-multilingual/lib/withTranslate';
-import { DataTableSetting } from '../../../../../common-components';
+import { DataTableSetting, DialogModal, ButtonModal } from '../../../../../common-components';
 
 const FamilyMemberTab = ({ id }) => {
+    let [familyMembers, setFamilyMembers] = useState([]);
+
+    const _save = () => {
+
+    }
+
+    const _openModalAddNewFamilyMembers = () => {
+        setFamilyMembers([...familyMembers, {}]);
+        window.$('#modal-add-family-members').modal('show');
+    }
+
+    const _handleChange = (e) => {
+        let index = familyMembers.length - 1;
+        console.log("length", index)
+        let { name, value } = e.target;
+        familyMembers[index][name] = value;
+        setFamilyMembers(familyMembers);
+    }
+
+    console.log("familyMembers", familyMembers)
+
     return (
         <div id={id} className="tab-pane">
             <div className="box-body">
@@ -66,6 +87,100 @@ const FamilyMemberTab = ({ id }) => {
                     <legend className="scheduler-border" >
                         <h4 className="box-title">Kê khai đầy đủ thành viên hộ gia đình trong sổ hộ khẩu hoặc sổ tạm trú</h4>
                     </legend>
+
+                    {/* Thêm mới thông tin thành viên hộ gia đình của chủ hộ */}
+                    <button className="btn btn-success" style={{ cursor: 'pointer' }} onClick={_openModalAddNewFamilyMembers}>Thêm mới</button>
+                    <DialogModal
+                        modalID="modal-add-family-members"
+                        formID="form-add-family-members"
+                        title="Thêm thành viên trong hộ gia đình"
+                        func={_save} size={75}
+                    >
+                        <form id="form-add-family-members">
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Họ và tên</label>
+                                        <input className="form-control" name="name" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Mã số BHXH</label>
+                                        <input className="form-control" name="codeSocialInsurance" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Số sổ BHXH</label>
+                                        <input className="form-control" name="bookNumberSocialInsurance" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Giới tính</label>
+                                        <input className="form-control" name="gender" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Là chủ hộ</label>
+                                        <input className="form-control" name="isHeadHousehold" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Quan hệ với chủ hộ</label>
+                                        <input className="form-control" name="relationshipWithHeadHousehold" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Ngày sinh</label>
+                                        <input className="form-control" name="birth" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>CNSS</label>
+                                        <input className="form-control" name="cnss" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Nơi cấp giấy khai sinh</label>
+                                        <input className="form-control" name="placeOfBirthCertificate" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Quốc tịch</label>
+                                        <input className="form-control" name="nationality" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Dân tộc</label>
+                                        <input className="form-control" name="nation" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                                    <div className="form-group">
+                                        <label>Số CMND/Hộ chiếu</label>
+                                        <input className="form-control" name="numberPassport" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <div className="form-group">
+                                        <label>Ghi chú</label>
+                                        <textarea className="form-control" name="note" onChange={_handleChange} />
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </DialogModal>
+
+                    {/* Bảng danh sách thông tin các thành viên trong hộ gia đình */}
                     <div id="container-employee-family-member" className="row">
                         <table className="table table-hover table-striped table-bordered" id="table-employee-family-member">
                             <thead>
