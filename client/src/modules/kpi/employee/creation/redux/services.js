3eaa2e0e07ffc98e801c08a8f4dd863485be8f19
Nam HoaiVu <namvh.vnist@gmail.com>
2020-12-30 14:14:36 +0700

    fix bug kpi cá nhân

--- client/src/modules/kpi/employee/creation/redux/services.js
+++ client/src/modules/kpi/employee/creation/redux/services.js
@@ -87,7 +87,7 @@ function createEmployeeKpi(newTarget) {
     return sendRequest({
         url: `${process.env.REACT_APP_SERVER}/kpi/employee/creation/employee-kpis`,
         method: 'POST',
-        data: JSON.stringify(newTarget)
+        data: newTarget
     }, true, true, 'kpi.employee.employee_kpi_set.messages_from_server');
 }
 
