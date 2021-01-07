8e942fe3b38b5b9c9f9538b6a8f2c1c255f81b69
Nam HoaiVu <namvh.vnist@gmail.com>
2021-01-07 16:09:16 +0700

    fix covert ảnh quill editor

--- client/src/common-components/src/quill-editor/toolbarQuillEditor.jsx
+++ client/src/common-components/src/quill-editor/toolbarQuillEditor.jsx
@@ -13,11 +13,11 @@ class ToolbarQuillEditor extends Component {
     }
 
     render() {
-        const { id, font, header, typography, fontColor, alignAndList, embeds, table } = this.props;
+        const { id, font, header, typography, fontColor, alignAndList, embeds, table, inputCssClass = "" } = this.props;
 
         return (
             <React.Fragment>
-                <div id={id}>
+                <div id={id} className={`quill-editor-toolbar ${inputCssClass}`}>
                     {/* font */
                         font &&
                             <select className="ql-font">
