a9923a700586ff764d3252a35c7ac26a4f91a079
minhtbvnist <67574123+minhtbvnist@users.noreply.github.com>
2021-01-15 12:16:19 +0700

    test guide

--- client/src/modules/user-guide/components/index.jsx
+++ client/src/modules/user-guide/components/index.jsx
@@ -1,18 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
-import FilePreviewer from 'react-file-previewer';
+import DocViewer from "./doc.jsx";
 const UserGuide = () => {
-    const url = `https://docs.google.com/gview?url=http://www.africau.edu/images/default/sample.pdf&embedded=true`;
-    const url1 = `https://docs.google.com/gview?url=${process.env.REACT_APP_SERVER}/upload/user-guide/abc.pdf&embedded=true`
     return (
         <React.Fragment>
-            <div style={{ maxWidth: '100%' }}>
-                <iframe width="100%" height="500" className="doc" src={url1}></iframe>
-            </div>
-            {/* <div style={{ marginTop: '30px' }}>
-                <iframe width="100%" height="500" class="doc" src={}></iframe>
-            </div> */}
+            <DocViewer source={`${process.env.REACT_APP_SERVER}/upload/user-guide/asset-guide.pdf`} />
+
         </React.Fragment>
 
     )
