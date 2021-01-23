5a8a5a9a08d2b24fa8a7b963058b1477cc70c7db
thainv-vnist <thainv.vnist@gmail.com>
2021-01-23 15:07:11 +0900

    filter timesheetlog by month-year

--- client/src/modules/task/task-dashboard/statistic/allTimeSheetLog.jsx
+++ client/src/modules/task/task-dashboard/statistic/allTimeSheetLog.jsx
@@ -8,23 +8,59 @@ import { convertTime } from '../../../../helpers/stringMethod';
 class AllTimeSheetLogs extends Component {
     constructor(props) {
         super(props);
-
+        let curTime = new Date();
+        let month = curTime.getMonth() + 1;
+        let year = curTime.getFullYear();
         this.state = {
-
+            time: month + "-" + year
         }
     }
 
     componentDidMount() {
-        this.props.getAllUserTimeSheet(1, 2021);
+        let { time } = this.state;
+        this.filterTimeSheetLog(time);
+    }
+
+    changeTime = (value) => {
+        this.setState({
+            time: value
+        });
+        this.filterTimeSheetLog(value);
+    }
+
+    filterTimeSheetLog = (time) => {
+        if (time) {
+            let dataTime = time.split("-");
+            let month = dataTime[0];
+            let year = dataTime[1];
+            this.props.getAllUserTimeSheet(month, year);
+        }
     }
 
     render() {
         const { tasks, translate } = this.props;
+        const { month } = this.state;
         const timesheetlogs = tasks?.allTimeSheetLogs;
 
         return (
             <div className="box" style={{ minHeight: '450px' }}>
                 <div className="box-body">
+
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                            <div className="form-group" style={{ display: "flex", alignItems: 'center' }}>
+                                <label style={{ margin: '2px 10px' }}>Tháng</label>
+                                <DatePicker
+                                    id="time-sheet-log"
+                                    dateFormat="month-year"
+                                    value={month}
+                                    onChange={this.changeTime}
+                                    disabled={false}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
                     <table className="table table-hover table-striped table-bordered" id="table-all-user-time-sheet-log">
                         <thead>
                             <tr>
