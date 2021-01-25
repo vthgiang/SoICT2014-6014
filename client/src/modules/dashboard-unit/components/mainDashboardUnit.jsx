a37124bc53b02e1857811cac0b0065f16ebd73cb
Nam HoaiVu <namvh.vnist@gmail.com>
2021-01-25 16:22:35 +0700

    chọn tất cả select multi bảng tin đơn vị

--- client/src/modules/dashboard-unit/components/mainDashboardUnit.jsx
+++ client/src/modules/dashboard-unit/components/mainDashboardUnit.jsx
@@ -84,17 +84,22 @@ class MainDashboardUnit extends Component {
         let partMonth = month.split('-');
         let newMonth = [partMonth[1], partMonth[0]].join('-');
 
+        let listUnit = arrayUnitShow;
+        if (listUnit[0] === 'selectAll') {
+            listUnit.shift();
+        }
+
         this.setState({
-            organizationalUnits: arrayUnitShow,
+            organizationalUnits: listUnit,
             monthShow: month
         });
 
-        let arrayUnit = arrayUnitShow;
-        if (arrayUnitShow.length === department.list.length) {
-            arrayUnitShow = department.list.map(x => x._id);
+        let arrayUnit = listUnit;
+        if (listUnit.length === department.list.length) {
+            listUnit = department.list.map(x => x._id);
             arrayUnit = undefined;
-        } else if (arrayUnitShow.length === 0) {
-            arrayUnitShow = childOrganizationalUnit.map(x => x.id);
+        } else if (listUnit.length === 0) {
+            listUnit = childOrganizationalUnit.map(x => x.id);
             arrayUnit = childOrganizationalUnit.map(x => x.id);
         }
 
@@ -107,23 +112,23 @@ class MainDashboardUnit extends Component {
         this.props.getAllEmployee({ status: ["active", 'maternity_leave', 'unpaid_leave', 'probationary', 'sick_leave'], page: 0, limit: 10000, birthdate: newMonth, organizationalUnits: arrayUnitShow });
 
         /* Lấy dữ liệu công việc của nhân viên trong đơn vị */
-        this.props.getAllEmployeeOfUnitByIds(arrayUnitShow);
-        this.props.getTaskInOrganizationUnitByMonth(arrayUnitShow, newMonth, newMonth, "in_month");
+        this.props.getAllEmployeeOfUnitByIds(listUnit);
+        this.props.getTaskInOrganizationUnitByMonth(listUnit, newMonth, newMonth, "in_month");
 
         /** Lấy dữ liệu công việc sắp hết hạn */
-        this.props.getTaskByUser({ organizationUnitId: arrayUnitShow, type: "organizationUnit", })
+        this.props.getTaskByUser({ organizationUnitId: listUnit, type: "organizationUnit", })
 
-        // this.props.searchAnnualLeaves({ organizationalUnits: arrayUnitShow, month: newMonth });
+        // this.props.searchAnnualLeaves({ organizationalUnits: listUnit, month: newMonth });
         /* Lấy dánh sách khen thưởng, kỷ luật */
-        this.props.getListPraise({ organizationalUnits: arrayUnitShow, month: newMonth });
-        this.props.getListDiscipline({ organizationalUnits: arrayUnitShow, month: newMonth });
+        this.props.getListPraise({ organizationalUnits: listUnit, month: newMonth });
+        this.props.getListDiscipline({ organizationalUnits: listUnit, month: newMonth });
 
         /* Lấy dữ liệu lương nhân viên*/
-        this.props.searchSalary({ callApiDashboard: true, organizationalUnits: arrayUnitShow, month: newMonth });
+        this.props.searchSalary({ callApiDashboard: true, organizationalUnits: listUnit, month: newMonth });
         this.props.searchSalary({ callApiDashboard: true, month: newMonth });
 
         /* Lấy dữ liệu nghỉ phép, tăng ca của nhân viên */
-        this.props.getTimesheets({ organizationalUnits: arrayUnitShow, startDate: newMonth, endDate: newMonth });
+        this.props.getTimesheets({ organizationalUnits: listUnit, startDate: newMonth, endDate: newMonth });
 
     }
 
@@ -272,7 +277,7 @@ class MainDashboardUnit extends Component {
         return taskNeedToDoPieChart;
     }
 
-    handleUpdateData = () => {
+    handleUpdateDataUrgent = () => {
         const { currentDate, arrayUnitForUrgentChart } = this.state;
         
         let partDate = currentDate.split('-');
@@ -283,7 +288,6 @@ class MainDashboardUnit extends Component {
             listUnitForUrgentChart.shift();
         }
 
-        console.log(listUnitForUrgentChart)
         this.props.getTaskInOrganizationUnitByDateNow(listUnitForUrgentChart, newDate)
     }
 
@@ -428,7 +432,7 @@ class MainDashboardUnit extends Component {
                                                 >
                                                 </SelectMulti>
                                             </div>
-                                            <button type="button" className="btn btn-success" onClick={this.handleUpdateData}>tìm kiếm</button>
+                                            <button type="button" className="btn btn-success" onClick={this.handleUpdateDataUrgent}>{translate('general.search')}</button>
                                         </div>
                                     </div>
 
@@ -468,7 +472,11 @@ class MainDashboardUnit extends Component {
                             <label style={{ width: "auto" }}>{translate('kpi.organizational_unit.dashboard.organizational_unit')}</label>
                             <SelectMulti id="multiSelectOrganizationalUnitInDashboardUnit"
                                 items={childOrganizationalUnit.map(item => { return { value: item.id, text: item.name } })}
-                                options={{ nonSelectedText: translate('page.non_unit'), allSelectedText: translate('page.all_unit') }}
+                                options={{
+                                    nonSelectedText: translate('page.non_unit'),
+                                    allSelectedText: translate('page.all_unit'),
+                                    selectAllButton: true
+                                }}
                                 onChange={this.handleSelectOrganizationalUnit}
                                 value={arrayUnitShow}
                             >
