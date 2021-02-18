7e15ca94561a8da15db1cff02ee104410e7c0452
namvhvnist <namvh.vnist@gmail.com>
2021-02-18 16:13:38 +0700

    thêm tính năng cập nhật độ quan trọng cho nhânviên mới

--- client/src/modules/kpi/organizational-unit/creation/component/employeeImportancesModal.jsx
+++ client/src/modules/kpi/organizational-unit/creation/component/employeeImportancesModal.jsx
@@ -7,73 +7,76 @@ import { createUnitKpiActions } from '../redux/actions.js';
 
 import { DialogModal } from '../../../../../common-components';
 
-import getEmployeeSelectBoxItems from '../../../../task/organizationalUnitHelper';
-
-
 function EmployeeImportancesModal(props) {
     const { translate, user, createKpiUnit } = props;
     const { organizationalUnit, organizationalUnitId, month } = props;
 
     const [employeeImportancesState, setEmployeeImportancesState] = useState(null);
     const [state, setState] = useState({
+        updateEmployee: false,
         importance: 0,
         editting: false
     });
-    const { editting, importance } = state;
+    const { editting, importance, updateEmployee } = state;
 
     useEffect(() => {
-        props.getAllUserOfDepartment([props.organizationalUnitId]);
-
         setEmployeeImportancesState(null);
-    }, [props.organizationalUnitId])
+    }, [props.organizationalUnitId, props.createKpiUnit?.currentKPI])
 
     useEffect(() => {
-        if (!employeeImportancesState && user && user.userdepartments) {
-            let userdepartments, unitMembers, currentKPI, listEmployeeImportances, employeeImportances = {};
-
-            if (user) {
-                userdepartments = user.userdepartments;
-            }
-            if (createKpiUnit) {
-                currentKPI = createKpiUnit.currentKPI;
-            }
-            if (currentKPI) {
-                listEmployeeImportances = currentKPI.employeeImportances;
-            }
+        // Khởi tạo dữ liệu table độ quan trọng nhân viên
+        if (!employeeImportancesState && createKpiUnit?.currentKPI) {
+            let currentKpiUnit, employees = [];
         
-        
-            // Lấy danh sách nhân viên và độ quan trọng hiện có
-            if (listEmployeeImportances && listEmployeeImportances.length !== 0) {
-                listEmployeeImportances.map(item => {
-                    let temp = {};
-                    temp[item.employee] = item.importance;
-                    Object.assign(employeeImportances, temp);
+            currentKpiUnit = createKpiUnit.currentKPI;
+            if (currentKpiUnit) {
+                employees = currentKpiUnit?.employeeImportances?.map(item => {
+                    return { value: item?.employee?._id, text: item?.employee?.name, importance: item?.importance }
                 })
             }
-        
-        
-            // Khởi tạo select box chọn nhân viên
-            if (userdepartments && Array.isArray(userdepartments) && userdepartments.length !== 0) {
-                unitMembers = getEmployeeSelectBoxItems(userdepartments);
-                // Lấy mảng các nhân viên
-                if (unitMembers && unitMembers.length !== 0 && unitMembers[0] && unitMembers[0].value) {
-                    unitMembers = unitMembers[0].value;
-                }
-                // Thêm mảng nhân viên thuộc tính importances
-                if (unitMembers && unitMembers.length !== 0) {
-                    unitMembers.map(item => {
-                        Object.assign(item, {
-                            importance: employeeImportances[item.value] ? employeeImportances[item.value] : 100
-                        })
-                    })
+            setEmployeeImportancesState(employees)
+        }
 
-                    setEmployeeImportancesState(unitMembers)
+        // Cập nhât dữ liệu table khí thêm nhân viên mới
+        if (employeeImportancesState && user?.employeesOfUnitsUserIsManager && user?.employeesOfUnitsUserIsManager?.[0]?.idUnit === organizationalUnitId && updateEmployee) {
+            let userdepartments, unitMembers, currentKPI, listEmployeeImportances, employeeImportances, employee;
+            employeeImportances = employeeImportancesState;
+            employee = employeeImportancesState.map(item => item?.value);
+
+            // Lấy các nhân viên thuộc phòng ban hiện tại
+            if (user) {
+                userdepartments = user.employeesOfUnitsUserIsManager;
+                if (userdepartments) {
+                    unitMembers = userdepartments.map(item => item?.userId)
                 }
+            }
+            
+            // Lấy danh sách nhân viên có độ quan trọng lưu trong DB
+            if (createKpiUnit) {
+                currentKPI = createKpiUnit.currentKPI;
+                if (currentKPI) {
+                    listEmployeeImportances = currentKPI?.employeeImportances?.map(item => item?.employee?._id);
+                }
+            }
+
+            // Thêm những nhân viên chưa có độ quan trọng trong DB
+            if (unitMembers?.length !== 0) {
+                unitMembers.map(item => {
+                    if (!listEmployeeImportances || (listEmployeeImportances.indexOf(item?._id) === -1 && employee.indexOf(item?._id) === -1)) {
+                        employeeImportances.push({ value: item?._id, text: item?.name, importance: 100 })
+                    }   
+                })
+
+                // Reset state trước khi lưu state mới
+                setEmployeeImportancesState(employeeImportances);
+                setState({
+                    ...state,
+                    updateEmployee: false
+                });
             }
         }
     })
     
-   
     
     const formatDate = (date) => {
         var d = new Date(date),
@@ -126,6 +129,15 @@ function EmployeeImportancesModal(props) {
         })
     }
 
+    const handleUpdateEmployee = (e) => {
+        e.preventDefault();
+        setState({
+            ...state,
+            updateEmployee: true
+        })
+        props.getAllEmployeeOfUnitByIds([props.organizationalUnitId], true);
+    }
+
     const handleSubmit = () => {
         const { createKpiUnit } = props;
         let data, currentKPI;
@@ -158,6 +170,8 @@ function EmployeeImportancesModal(props) {
             >
                 {/* Form khởi tạo KPI đơn vị */}
                 <form id="form-employee-importances" onSubmit={() => handleSubmit(translate('kpi.organizational_unit.create_organizational_unit_kpi_set_modal.success'))}>
+                    <button className='btn btn-primary pull-right' style={{ marginBottom: '15px' }} onClick={(e) => handleUpdateEmployee(e)}>Cập nhật nhân viên mới</button>
+
                     <table className="table table-hover table-bordered">
                         <thead>
                             <tr>
@@ -204,26 +218,15 @@ function EmployeeImportancesModal(props) {
     );
 }
 
-function shouldComponentUpdate(prevProps, nextProps) {
-    // Call API khi organizationalUnitId thay đổi
-    console.log("8888", nextProps.organizationalUnitId ,prevProps.organizationalUnitId)
-    if (nextProps.organizationalUnitId !== prevProps.organizationalUnitId) {
-        console.log("99999")
-        nextProps.getAllUserOfDepartment([nextProps.organizationalUnitId]);
-        return false;
-    }
-
-    return false;
-}
 
 function mapState(state) {
     const { user, createKpiUnit } = state;
     return { user, createKpiUnit }
 }
 const actions = {
-    getAllUserOfDepartment: UserActions.getAllUserOfDepartment,
+    getAllEmployeeOfUnitByIds: UserActions.getAllEmployeeOfUnitByIds,
     editKPIUnit: createUnitKpiActions.editKPIUnit
 }
 
-const connectedEmployeeImportancesModal = connect(mapState, actions)(withTranslate(memo(EmployeeImportancesModal, shouldComponentUpdate)));
+const connectedEmployeeImportancesModal = connect(mapState, actions)(withTranslate(EmployeeImportancesModal));
 export { connectedEmployeeImportancesModal as EmployeeImportancesModal }
