fdb128de0b5e3d03c382cd7416076f9fddac98c9
namvhvnist <namvh.vnist@gmail.com>
2021-03-08 09:00:21 +0700

    bỏ validate cũ, dùng validateHelpers module task-template

--- client/src/modules/task/task-template/component/taskTemplateFormValidator.js
+++ client/src/modules/task/task-template/component/taskTemplateFormValidator.js
@@ -1,18 +1,7 @@
-import { VALIDATOR } from '../../../../helpers/validator';
 var mexp = require('math-expression-evaluator'); // native js package
 
 export const TaskTemplateFormValidator = {
-    validateTaskTemplateUnit,
-    validateTaskTemplateRead,
-    validateTaskTemplateName,
-    validateTaskTemplateDescription,
     validateTaskTemplateFormula,
-    validateActionName,
-    validateActionDescription,
-    validateInfoName,
-    validateInfoDescription,
-    validateInfoSetOfValues,
-    validateTaskTemplateNumberOfDaysTaken,
 }
 
 const calculateExpression = (expression) => {
@@ -36,39 +25,6 @@ const calculateExpression = (expression) => {
     }
 }
 
-function validateTaskTemplateUnit(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Cần chọn đơn vị";
-    }
-    return msg;
-}
-
-function validateTaskTemplateRead(value) {
-    let msg = undefined;
-    if (value.length === 0) {
-        msg = "Cần chỉ rõ người được xem mẫu công việc";
-    }
-    return msg;
-}
-
-function validateTaskTemplateName(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Tên không được để trống";
-    } else if (!VALIDATOR.isValidName(value)) {
-        msg = "Tên không chứa ký tự đặc biệt";
-    }
-    return msg;
-}
-
-function validateTaskTemplateDescription(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Mô tả không được để trống";
-    }
-    return msg;
-}
 
 function validateTaskTemplateFormula(value) {
     let msg = undefined;
@@ -99,59 +55,6 @@ function validateTaskTemplateFormula(value) {
         if (typeof automaticPoint !== "number") {
             msg = "Công thức không hợp lệ. Hãy kiểm tra lại các toán tử và toán hạng của công thức."
         }
-    }
-    return msg;
-}
-
-function validateTaskTemplateNumberOfDaysTaken(value) {
-    let msg = undefined;
-    let parseValue = parseInt(value);
-    console.log('val', value);
-    if (parseValue <= 0) {
-        msg = "Số ngày thực hiện dự kiến phải lớn hơn 0";
-    }
-    // else if (value.trim() === ""){
-    //     msg = "Số ngày hoàn thành công việc dự kiến không được để trống";
-    // }
-    return msg;
-}
-
-function validateActionName(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Tên không được để trống";
-    }
-    return msg;
-}
-
-function validateActionDescription(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Mô tả không được để trống";
-    }
-    return msg;
-}
-
-function validateInfoName(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Tên không được để trống";
-    }
-    return msg;
-}
-
-function validateInfoDescription(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Mô tả không được để trống";
-    }
-    return msg;
-}
-
-function validateInfoSetOfValues(value) {
-    let msg = undefined;
-    if (value.trim() === "") {
-        msg = "Tập giá trị không được để trống";
     }
     return msg;
 }
