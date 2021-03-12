d15619775b0c0e541275fede6711e657b4b68aba
quangnt-vnist <quangnt.vnist@gmail.com>
2021-03-12 20:29:34 +0700

    chỉnh sửa lỗi lấy ngày kết thúc công việc để đáh giá

--- client/src/modules/task/task-perform/component/evaluateByResponsibleEmployee.jsx
+++ client/src/modules/task/task-perform/component/evaluateByResponsibleEmployee.jsx
@@ -827,11 +827,42 @@ class EvaluateByResponsibleEmployee extends Component {
     }
 
     getEndTask = async () => {
+        let { translate } = this.props;
         let { task } = this.state;
         let end = task?.endDate;
         let endDate = this.formatDate(new Date(end));
         let endTime = this.formatTime(new Date(end));
-        this.setState({ endDate: endDate, endTime: endTime });
+
+        let { evaluatingMonth, startDate, startTime, idUser } = this.state;
+
+        let err = this.validateDateTime(evaluatingMonth, startDate, startTime, endDate, endTime, "end");
+
+        let data = this.getData(endDate, this.state.storedEvaluatingMonth);
+
+        let automaticPoint = data.automaticPoint;
+        let taskInfo = {
+            task: data.task,
+            progress: this.state.progress,
+            date: endDate,
+            time: endTime,
+            info: this.state.info,
+        };
+
+        automaticPoint = AutomaticTaskPointCalculator.calcAutoPoint(taskInfo);
+        if (isNaN(automaticPoint)) automaticPoint = undefined
+        if (automaticPoint < 0) automaticPoint = 0;
+
+        this.setState(state => {
+            return {
+                ...state,
+                errorOnEndDate: err,
+                endDate: endDate,
+                endTime: endTime,
+                autoPoint: automaticPoint,
+                oldAutoPoint: data.autoPoint,
+                indexReRender: state.indexReRender + 1,
+            }
+        });
     }
 
     // hàm cập nhật điểm tự đánh giá
