90eb08afafaf65f7b2980c1c2e618733d2e190a6
TranNgocCong <55443845+TranNgocCong@users.noreply.github.com>
2021-03-30 22:32:08 +0700

    Change module asset/admin/asset-information to Functional Component

--- client/src/modules/asset/admin/asset-information/components/index.jsx
+++ client/src/modules/asset/admin/asset-information/components/index.jsx
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
 import { AssetManagement } from './combinedContent';
 
-class AssetManager extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
+function AssetManager(props) {
+    const [state, setState] =useState({})
         return (
             <React.Fragment>
                 <AssetManagement />
             </React.Fragment>
         );
-    }
 }
 
 export default connect(null, null)(withTranslate(AssetManager)); 
