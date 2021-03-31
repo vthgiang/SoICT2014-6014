8a60b61f135db77530923d9005a3eb3353e8e849
minhtbvnist <67574123+minhtbvnist@users.noreply.github.com>
2021-03-31 14:14:47 +0700

    Cố định header bảng thống kê bấm giờ, sắp xếp người có thời gian bấm giờ giảm dần

--- client/src/modules/task/task-dashboard/statistic/allTimeSheetLog.jsx
+++ client/src/modules/task/task-dashboard/statistic/allTimeSheetLog.jsx
@@ -4,7 +4,7 @@ import { taskManagementActions } from '../../task-management/redux/actions';
 import { withTranslate } from 'react-redux-multilingual';
 import { DatePicker, ToolTip } from '../../../../common-components';
 import { convertTime } from '../../../../helpers/stringMethod';
-
+import _deepClone from 'lodash/cloneDeep';
 
 class AllTimeSheetLogs extends Component {
     constructor(props) {
@@ -44,8 +44,15 @@ class AllTimeSheetLogs extends Component {
     render() {
         const { tasks, translate } = this.props;
         const { time } = this.state;
-        const timesheetlogs = tasks?.allTimeSheetLogs;
+        let timesheetlogs = _deepClone(tasks?.allTimeSheetLogs);
+        timesheetlogs.sort((a, b) => (a.duration < b.duration) ? 1 : -1);
 
+        const thStyle = {
+            position: "sticky",
+            top: "-0.1em",
+            background: "#fff",
+            zIndex: 1,
+        };
         return (
             <div className="box" style={{ minHeight: '450px' }}>
                 <div className="box-body">
@@ -61,30 +68,32 @@ class AllTimeSheetLogs extends Component {
                                     disabled={false}
                                 />
                             </div>
-                            <table className="table table-hover table-striped table-bordered" id="table-all-user-time-sheet-log">
-                                <thead>
-                                    <tr>
-                                        <th style={{ width: '60px' }}>STT</th>
-                                        <th>{translate('manage_user.name')}</th>
-                                        <th>{translate('manage_user.email')}</th>
-                                        <th>Tổng thời gian bấm giờ</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        Array.isArray(timesheetlogs) && timesheetlogs.map((tsl, index) => {
-                                            return (
-                                                <tr key={index}>
-                                                    <td>{index + 1}</td>
-                                                    <td>{tsl?.creator?.name}</td>
-                                                    <td>{tsl?.creator?.email}</td>
-                                                    <td>{convertTime(tsl?.duration)}</td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </table>
+                            <div className="StyleScrollDiv StyleScrollDiv-y" style={{ maxHeight: '600px' }}>
+                                <table className="table table-hover table-striped table-bordered" id="table-all-user-time-sheet-log">
+                                    <thead>
+                                        <tr>
+                                            <th style={{ width: '60px', ...thStyle }}>STT</th>
+                                            <th style={thStyle}>{translate('manage_user.name')}</th>
+                                            <th style={thStyle}>{translate('manage_user.email')}</th>
+                                            <th style={thStyle}>Tổng thời gian bấm giờ</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            Array.isArray(timesheetlogs) && timesheetlogs.map((tsl, index) => {
+                                                return (
+                                                    <tr key={index}>
+                                                        <td>{index + 1}</td>
+                                                        <td>{tsl?.creator?.name}</td>
+                                                        <td>{tsl?.creator?.email}</td>
+                                                        <td>{convertTime(tsl?.duration)}</td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
