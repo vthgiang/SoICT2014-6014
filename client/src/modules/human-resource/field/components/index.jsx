849ca4f957eeebe8559a26ac70e9ad735050ce2e
tokhanh <kto2303@gmail.com>
2021-04-10 15:21:50 +0700

    convert class to function component

--- client/src/modules/human-resource/field/components/index.jsx
+++ client/src/modules/human-resource/field/components/index.jsx
@@ -4,19 +4,12 @@ import { withTranslate } from 'react-redux-multilingual';
 
 import { FieldManagement } from './combinedContent';
 
-class SalaryField extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <FieldManagement />
-            </React.Fragment>
-        );
-    }
+const SalaryField = (props) => {
+    return (
+        <React.Fragment>
+            <FieldManagement />
+        </React.Fragment>
+    );
 }
 
 export default connect(null, null)(withTranslate(SalaryField)); 
