30cb206437320f63362d1ad60e882db2249563b8
minhtbvnist <67574123+minhtbvnist@users.noreply.github.com>
2021-04-15 12:09:22 +0700

    Sửa cấu hình webpack cho server

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,49 +1,42 @@
-var webpack = require('webpack');
-var path = require('path');
-var fs = require('fs');
+var path = require("path");
+var fs = require("fs");
+var nodeExternals = require("webpack-node-externals");
 
 var nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function (x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function (mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
+fs.readdirSync("node_modules")
+  .filter(function (x) {
+    return [".bin"].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = "commonjs " + mod;
+  });
 
 module.exports = {
-    entry: './index.js',
-    target: 'node',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'backend.js'
-    },
-    // externals: nodeModules,
-    module: {
-        rules: [
-            {
-                test: /\.txt$/i,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.log$/i,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.js$/,
-                exclude: [
-                    path.resolve(__dirname, '/global.js'),
-                ], // bỏ qua các thư mục node_module
-                enforce: 'pre',
-                use: ['source-map-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new webpack.IgnorePlugin(/\.(css|less)$/),
-        // new webpack.BannerPlugin('require("source-map-support").install();',
-        //                          { raw: true, entryOnly: false })
+  entry: "./index.js",
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "backend.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/i,
+        use: "raw-loader",
+      },
+      {
+        test: /\.log$/i,
+        use: "raw-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, "/global.js")],
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
     ],
-    mode: 'production',
-    devtool: 'inline-source-map'
-}
+  },
+  mode: "production",
+  devtool: "inline-source-map",
+};
