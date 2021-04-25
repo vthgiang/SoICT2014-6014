6a071f4b73a7a06ca041c30e254ae19efeb94920
khanhlt12102000 <khanhlt.vnist@gmail.com>
2021-04-25 16:53:03 +0700

    Fix warning khi mở form xem nhân viên

--- client/src/modules/human-resource/profile/employee-info/components/experienceTab.jsx
+++ client/src/modules/human-resource/profile/employee-info/components/experienceTab.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
 
-class ExperiencTab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+function ExperiencTab(props) {
+    const [state, setState] = useState({
+
+    })
 
     /**
      * Function format dữ liệu Date thành string
      * @param {*} date : Ngày muốn format
      * @param {*} monthYear : true trả về tháng năm , false trả về ngày tháng năm
      */
-    formatDate(date, monthYear = false) {
+
+    const formatDate = (date, monthYear = false) => {
         if (date) {
             let d = new Date(date),
                 month = '' + (d.getMonth() + 1),
@@ -33,83 +33,79 @@ class ExperiencTab extends Component {
 
     }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.id !== prevState.id) {
+    useEffect(() => {
+        setState(state => {
             return {
-                ...prevState,
-                id: nextProps.id,
-                experiences: nextProps.employee.experiences,
-                educationalLevel: nextProps.employee.educationalLevel,
-                foreignLanguage: nextProps.employee.foreignLanguage,
-                professionalSkill: nextProps.employee.professionalSkill,
+                ...state,
+                id: props.id,
+                experiences: props.employee.experiences,
+                educationalLevel: props.employee.educationalLevel,
+                foreignLanguage: props.employee.foreignLanguage,
+                professionalSkill: props.employee.professionalSkill,
             }
-        } else {
-            return null;
-        }
-    }
+        })
+    }, [props.id])
 
-    render() {
-        const { translate } = this.props;
+    const { translate } = props;
 
-        const { id, educationalLevel, foreignLanguage, professionalSkill, experiences } = this.state;
+    const { id, educationalLevel, foreignLanguage, professionalSkill, experiences } = state;
 
-        return (
-            <div id={id} className="tab-pane">
-                <div className="box-body">
-                    <fieldset className="scheduler-border">
-                        <legend className="scheduler-border"><h4 className="box-title">{translate('human_resource.profile.academic_level')}</h4></legend>
-                        {/* Trình độ văn hoá */}
-                        <div className="form-group">
-                            <strong>{translate('human_resource.profile.educational_level')}&emsp; </strong>
-                            {educationalLevel}
-                        </div>
-                        {/* Trình độ ngoại ngữ */}
-                        <div className="form-group" >
-                            <strong>{translate('human_resource.profile.language_level')}&emsp; </strong>
-                            {foreignLanguage}
-                        </div>
-                        {/* Trình độ chuyên môn */}
-                        <div className="form-group">
-                            <strong>{translate('human_resource.profile.qualification')}&emsp; </strong>
-                            {translate(`human_resource.profile.${professionalSkill}`)}
-                        </div>
-                    </fieldset>
-                    {/* Danh sách kinh nghiệm làm việc */}
-                    <fieldset className="scheduler-border">
-                        <legend className="scheduler-border"><h4 className="box-title">{translate('human_resource.profile.work_experience')}</h4></legend>
-                        <table className="table table-striped table-bordered table-hover" style={{ marginBottom: 0 }}>
-                            <thead>
-                                <tr>
-                                    <th>{translate('human_resource.profile.from_month_year')}</th>
-                                    <th>{translate('human_resource.profile.to_month_year')}</th>
-                                    <th>{translate('human_resource.profile.unit')}</th>
-                                    <th>{translate('table.position')}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    (experiences && experiences.length !== 0) &&
-                                    experiences.map((x, index) => (
-                                        <tr key={index}>
-                                            <td>{this.formatDate(x.startDate, true)}</td>
-                                            <td>{this.formatDate(x.endDate, true)}</td>
-                                            <td>{x.company}</td>
-                                            <td>{x.position}</td>
-                                        </tr>
+    return (
+        <div id={id} className="tab-pane">
+            <div className="box-body">
+                <fieldset className="scheduler-border">
+                    <legend className="scheduler-border"><h4 className="box-title">{translate('human_resource.profile.academic_level')}</h4></legend>
+                    {/* Trình độ văn hoá */}
+                    <div className="form-group">
+                        <strong>{translate('human_resource.profile.educational_level')}&emsp; </strong>
+                        {educationalLevel}
+                    </div>
+                    {/* Trình độ ngoại ngữ */}
+                    <div className="form-group" >
+                        <strong>{translate('human_resource.profile.language_level')}&emsp; </strong>
+                        {foreignLanguage}
+                    </div>
+                    {/* Trình độ chuyên môn */}
+                    <div className="form-group">
+                        <strong>{translate('human_resource.profile.qualification')}&emsp; </strong>
+                        {translate(`human_resource.profile.${professionalSkill}`)}
+                    </div>
+                </fieldset>
+                {/* Danh sách kinh nghiệm làm việc */}
+                <fieldset className="scheduler-border">
+                    <legend className="scheduler-border"><h4 className="box-title">{translate('human_resource.profile.work_experience')}</h4></legend>
+                    <table className="table table-striped table-bordered table-hover" style={{ marginBottom: 0 }}>
+                        <thead>
+                            <tr>
+                                <th>{translate('human_resource.profile.from_month_year')}</th>
+                                <th>{translate('human_resource.profile.to_month_year')}</th>
+                                <th>{translate('human_resource.profile.unit')}</th>
+                                <th>{translate('table.position')}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                (experiences && experiences.length !== 0) &&
+                                experiences.map((x, index) => (
+                                    <tr key={index}>
+                                        <td>{formatDate(x.startDate, true)}</td>
+                                        <td>{formatDate(x.endDate, true)}</td>
+                                        <td>{x.company}</td>
+                                        <td>{x.position}</td>
+                                    </tr>
 
-                                    ))
-                                }
+                                ))
+                            }
 
-                            </tbody>
-                        </table>
-                        {
-                            (!experiences || experiences.length === 0) && <div className="table-info-panel">{translate('confirm.no_data')}</div>
-                        }
-                    </fieldset>
-                </div>
+                        </tbody>
+                    </table>
+                    {
+                        (!experiences || experiences.length === 0) && <div className="table-info-panel">{translate('confirm.no_data')}</div>
+                    }
+                </fieldset>
             </div>
-        );
-    }
+        </div>
+    );
 };
 
 const tabExperience = connect(null, null)(withTranslate(ExperiencTab));
