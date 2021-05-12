cce4f1547f95d0b3cdbc5f8433f2913d7b86f362
kienndt-vnist <kienndt.vnist@gmail.com>
2021-05-12 23:05:03 +0700

    sinh kế hoạch tự động v1

--- client/src/modules/production/transport/transportHelper/compareDateTimeZone.js
+++ client/src/modules/production/transport/transportHelper/compareDateTimeZone.js
@@ -35,3 +35,10 @@ export const getListDateBetween = (startDate, endDate) => {
     }
     return result;
 }
+
+export const isDateEqual = (date1, date2) => {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    if (d1.getDate() === d2.getDate() && d1.getMonth()===d2.getMonth() && d1.getFullYear()===d2.getFullYear()) return true;
+    else return false;
+}
--- client/src/modules/production/transport/transportHelper/generatePlan.js
+++ client/src/modules/production/transport/transportHelper/generatePlan.js
@@ -50,7 +50,52 @@ getBusyday = (date, listPlan, listCarriers, listVehicles) => { // date timezone
         })
         
 }
-
+const calDistanceGeocode = (lat1, lng1, lat2, lng2) => {
+    return Math.sqrt((lat2- lat1)*(lat2- lat1) + (lng2-lng1)*(lng2-lng1));
+}
 exports.generatePlan = (listRequirement, listPlan, listVehicle, listCarriers) => {
-
+    const CURRENT_DAY = new Date();
+    let flag = true;
+    let nextDay = 0;
+    let listRequirementCal = []
+    for (let i=0; i< listRequirement.length;i++){
+        listRequirementCal.push({
+            requirement: listRequirement[i],
+            plan: null,
+        })
+    }
+    while (flag){
+        let result = [];
+        let calArr = [];
+        let needArrangeRequirement = listRequirementCal.filter(r=> r.plan === null);
+        if(needArrangeRequirement && needArrangeRequirement.length !==0){
+            needArrangeRequirement.map((requirement, index)=> {
+                let mark = 0;
+                if (requirement.timeRequests && requirement.timeRequests.length !==0){
+                    requirement.timeRequests.map(time => {
+                        let timeRequest = new Date(time.timeRequest);
+                        if(timeRequest.getTime() === date.getTime()){
+                            mark = 5*86400000;
+                        }
+                    })
+                }
+                const createdAt = new Date(requirement.createdAt);
+                mark += date.getTime() - createdAt.getTime();
+                calArr.push({
+                    requirement: requirement,
+                    mark: mark,
+                })
+            })
+            calArr.sort((a, b)=> {
+                return b.mark-a.mark;
+              });
+            for (let i=0;i<calArr.length;i++){
+                result.push(calArr[i].requirement);
+            }
+        }
+        else {
+            flag = false;
+            break;
+        }
+    }
 }
