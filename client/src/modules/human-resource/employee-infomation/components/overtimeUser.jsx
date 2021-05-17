819c9e698ef53c0b072190f4c097919c3bdd8835
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-05-17 10:10:34 +0700

    thêm xu hướng làm việc trong nhân sự đơn vị

--- client/src/modules/human-resource/employee-infomation/components/overtimeUser.jsx
+++ client/src/modules/human-resource/employee-infomation/components/overtimeUser.jsx
@@ -3,10 +3,12 @@ import { connect } from "react-redux";
 import { withTranslate } from "react-redux-multilingual";
 import { TimesheetsActions } from "../../timesheets/redux/actions";
 import { AnnualLeaveActions } from "../../annual-leave/redux/actions";
-
+import c3 from 'c3';
+import 'c3/c3.css';
 function TakeLeaveUser(props) {
     const [showOverTimeAndHourTime, setShowOverTimeAndHourTime] = useState({ isLoading: true ,show:false})
     const [showAnnualLeave, setShowAnnualLeave] = useState({ isLoading: true , show:true})
+    const [lineChart, setLineChart] = useState(true)
     const formatDate2 = (date, monthYear = false) => {
         if (date) {
             let d = new Date(date),
@@ -52,17 +54,6 @@ function TakeLeaveUser(props) {
             setShowOverTimeAndHourTime({...showOverTimeAndHourTime,isLoading:false})
         }
     }, [props.email, props.startDate, props.endDate])
-    const { timesheets, translate, annualLeave } = props
-    const { listTimesheetsByEmployeeIdAndTime } = timesheets
-    let overTime = 0, hoursOff = 0, totalHours = 0;
-    const { listAnnualLeaveOfNumberMonth } = annualLeave
-    if (listTimesheetsByEmployeeIdAndTime.length > 0) {
-        for (let i = 0; i < listTimesheetsByEmployeeIdAndTime.length; i++) {
-            overTime = overTime + listTimesheetsByEmployeeIdAndTime[i].totalOvertime
-            hoursOff = hoursOff + listTimesheetsByEmployeeIdAndTime[i].totalHoursOff
-            totalHours = totalHours + listTimesheetsByEmployeeIdAndTime[i].totalHours
-        }
-    }
     const showAnnualLeaveUser = (listAnnualLeaveOfNumberMonth, showAnnualLeave) => {
         return (
             <React.Fragment>
@@ -146,6 +137,97 @@ function TakeLeaveUser(props) {
         setShowOverTimeAndHourTime({...showOverTimeAndHourTime,show:false})
         setShowAnnualLeave({...showAnnualLeave,show:true})
     }
+   
+    const renderChart = (data) => {
+        data.data1.shift();
+        data.data2.shift();
+        let fakeData1 = data.data1.map(x => 2 * x);
+        let fakeData2 = data.data2.map(x => x / 2);
+        let chart = c3.generate({
+            bindto: "#trendWork",
+            data: {
+                x: 'x',
+                columns: [],
+                hide: true,
+                type: data.lineChart === true ? '' : 'bar',
+                names: {
+                    data1: data.nameData1,
+                    data2: data.nameData2,
+                },
+            },
+            bar: {
+                width: {
+                    ratio: 0.8
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%m - %Y',
+                        outer: false,
+                        rotate: -45,
+                        multiline: false
+                    },
+                },
+                y: {
+                    tick: {
+                        outer: false
+                    },
+                }
+            },
+        });
+
+        setTimeout(function () {
+            chart.load({
+                columns: [data.ratioX, ['data1', ...fakeData1],
+                ['data2', ...fakeData2]
+                ],
+            });
+        }, 100);
+        setTimeout(function () {
+            chart.load({
+                columns: [data.ratioX, ['data1', ...data.data1],
+                ['data2', ...data.data2]
+                ],
+            });
+        }, 300);
+    };
+    const handleChangeViewChart = (data) =>{
+        setLineChart(data)
+    }
+    const { timesheets, translate, annualLeave } = props
+    const { listTimesheetsByEmployeeIdAndTime } = timesheets
+    
+    let overTime = 0, hoursOff = 0, totalHours = 0;
+    const { listAnnualLeaveOfNumberMonth } = annualLeave
+    // if (timesheets.){}
+    if (listTimesheetsByEmployeeIdAndTime.length > 0) {
+        for (let i = 0; i < listTimesheetsByEmployeeIdAndTime.length; i++) {
+            overTime = overTime + listTimesheetsByEmployeeIdAndTime[i].totalOvertime
+            hoursOff = hoursOff + listTimesheetsByEmployeeIdAndTime[i].totalHoursOff
+            totalHours = totalHours + listTimesheetsByEmployeeIdAndTime[i].totalHours
+        }
+    }
+    if (listTimesheetsByEmployeeIdAndTime.length !== 0) {
+        let ratioX = ['x', ...timesheets.arrMonthById];
+        let data1 = ['data1'], data2 = ['data2'];
+        timesheets.arrMonthById.forEach(x => {
+            let month = `${new Date(x).getFullYear()}-${new Date(x).getMonth()}`;
+            let data = listTimesheetsByEmployeeIdAndTime.find(x => `${new Date(x.month).getFullYear()}-${new Date(x.month).getMonth()}` === month);
+            if (data) {
+                data1 = [...data1, data.totalHours];
+                data2 = [...data2, data.totalOvertime ? data.totalOvertime : 0];
+            } else {
+                data1 = [...data1, 0];
+                data2 = [...data2, 0];
+            }
+        })
+        let nameData1=`${translate('human_resource.dashboard_personal.total_hours_works')}`
+        let nameData2=`${translate('human_resource.dashboard_personal.overtime_total')}`
+        renderChart({ nameData1, nameData2, ratioX, data1, data2, lineChart });
+    }
+    let nameChart=`${translate('human_resource.dashboard_personal.trend_of_work')}`
     return (
         <div className="box-header with-border">
             <div className="col-md-2 col-sm-4 col-xs-4 statistical-item" onClick={()=>{handChangeShowAnnualLeave()}} >
@@ -205,7 +287,17 @@ function TakeLeaveUser(props) {
             </div>
             { showAnnualLeave.show && showAnnualLeaveUser(listAnnualLeaveOfNumberMonth, showAnnualLeave)}
             { showOverTimeAndHourTime.show && showTimeSheetsUser(listTimesheetsByEmployeeIdAndTime,showOverTimeAndHourTime)}
-
+            <h3 className="box-title">{`${nameChart} ${props.startDate}`}<i className="fa fa-fw fa-caret-right"></i>{props.endDate}</h3>
+            <div className="dashboard_box_body">
+                <p className="pull-left" style={{ marginBottom: 0 }}><b>ĐV tính: Số giờ</b></p>
+                <div className="box-tools pull-right">
+                    <div className="btn-group pull-rigth">
+                        <button type="button" className={`btn btn-xs ${lineChart ? "active" : "btn-danger"}`} onClick={() => handleChangeViewChart(false)}>Bar chart</button>
+                        <button type="button" className={`btn btn-xs ${lineChart ? 'btn-danger' : "active"}`} onClick={() => handleChangeViewChart(true)}>Line chart</button>
+                    </div>
+                </div>
+                <div id="trendWork"></div>
+            </div>
         </div>
     )
 }
