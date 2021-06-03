7982052fc64c0a2db8765de3a110ba527cf6ca60
TranNgocCong <55443845+TranNgocCong@users.noreply.github.com>
2021-06-03 23:14:32 +0700

    chuyển module quản lí lịch sản xuất sang functional component

--- client/src/modules/production/manufacturing/work-schedule/components/index.jsx
+++ client/src/modules/production/manufacturing/work-schedule/components/index.jsx
@@ -5,31 +5,29 @@ import { connect } from 'react-redux';
 import { withTranslate } from 'react-redux-multilingual';
 import WorkerScheduleList from './worker-schedule';
 
-class WorkSchedule extends Component {
-    render() {
-        const { translate } = this.props;
-        return (
-            <div className="nav-tabs-custom">
-                <ul className="nav nav-tabs">
-                    <li className="active"><a href="#list-manufacturing-mill-schedule" data-toggle="tab" onClick={() => forceCheckOrVisible(true, false)}>{translate('manufacturing.work_schedule.manufacturing_mill_schedule_list')}</a></li>
-                    <li ><a href="#list-workder-schedule" data-toggle="tab" onClick={() => forceCheckOrVisible(true, false)}>{translate('manufacturing.work_schedule.worker_schedule_list')}</a></li>
-                </ul>
-                <div className="tab-content">
-                    <div className="tab-pane active" id="list-manufacturing-mill-schedule">
-                        <LazyLoadComponent
-                        >
-                            <ManufacturingMillScheduleList />
-                        </LazyLoadComponent>
-                    </div>
-                    <div className="tab-pane" id="list-workder-schedule">
-                        <LazyLoadComponent
-                        >
-                            <WorkerScheduleList />
-                        </LazyLoadComponent>
-                    </div>
+function WorkSchedule(props) {
+    const { translate } = props;
+    return (
+        <div className="nav-tabs-custom">
+            <ul className="nav nav-tabs">
+                <li className="active"><a href="#list-manufacturing-mill-schedule" data-toggle="tab" onClick={() => forceCheckOrVisible(true, false)}>{translate('manufacturing.work_schedule.manufacturing_mill_schedule_list')}</a></li>
+                <li ><a href="#list-workder-schedule" data-toggle="tab" onClick={() => forceCheckOrVisible(true, false)}>{translate('manufacturing.work_schedule.worker_schedule_list')}</a></li>
+            </ul>
+            <div className="tab-content">
+                <div className="tab-pane active" id="list-manufacturing-mill-schedule">
+                    <LazyLoadComponent
+                    >
+                        <ManufacturingMillScheduleList />
+                    </LazyLoadComponent>
+                </div>
+                <div className="tab-pane" id="list-workder-schedule">
+                    <LazyLoadComponent
+                    >
+                        <WorkerScheduleList />
+                    </LazyLoadComponent>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 export default connect(null, null)(withTranslate(WorkSchedule));
