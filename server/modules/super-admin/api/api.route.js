2355d02ff7b8649b92abf33e7d5c24ebe585157e
vunam2000 <vunam722000@gmail.com>
2021-07-21 21:47:43 +0700

    update service phan quyen api

--- server/modules/super-admin/api/api.route.js
+++ server/modules/super-admin/api/api.route.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const ApiController = require('./api.controller');
 const { auth } = require(`../../../middleware`);
 
-
 router.get("/companies", auth, ApiController.getApis);
-
-router.get("/privilege-apis", auth, ApiController.getApiRegistration);
-
-router.post("/privilege-apis/register", auth, ApiController.registerToUseApi);
 
 module.exports = router;
