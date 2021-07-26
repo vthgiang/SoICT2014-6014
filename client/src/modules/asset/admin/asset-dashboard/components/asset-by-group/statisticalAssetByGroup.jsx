73152149711fb363e9bc696d9dc78678a570b936
minhtbvnist <67574123+minhtbvnist@users.noreply.github.com>
2021-07-26 08:28:44 +0700

    thêm chú thích nhóm tài sản dashboard tài sản

--- client/src/modules/asset/admin/asset-dashboard/components/asset-by-group/statisticalAssetByGroup.jsx
+++ client/src/modules/asset/admin/asset-dashboard/components/asset-by-group/statisticalAssetByGroup.jsx
@@ -4,6 +4,9 @@ import { withTranslate } from "react-redux-multilingual";
 import useDeepCompareEffect from 'use-deep-compare-effect';
 import c3 from 'c3';
 import 'c3/c3.css'
+import Swal from 'sweetalert2';
+
+
 function StatisticalAssetByGroup(props) {
 
     const [state, setState] = useState([]);
@@ -98,10 +101,37 @@ function StatisticalAssetByGroup(props) {
         });
     }
 
+
+    const showDetailAssetGroup = () => {
+        Swal.fire({
+            icon: "question",
+
+            html: `<h3 style="color: red"><div>Các nhóm tài sản</div> </h3>
+            <div style="font-size: 1.3em; text-align: left; margin-top: 15px; line-height: 1.7">
+            <ul>
+                <li><b>Mặt bằng: </b>bao gồm nhà, công trình xây dựng, vật kiến trúc,...</li>
+                <li><b>Xe cộ: </b>bao gồm các phương tiện vận tải</li>
+                <li><b>Máy móc: </b>bao gồm các loại máy móc, thiết bị văn phòng, thiết bị truyền dẫn, thiết bị động lực, thiết bị chuyên dùng, thiết bị đo lường thí nghiệm,...</li>
+                <li><b>Khác: </b> bao gồm cây lâu năm, súc vật, trang thiết bị dễ hỏng dễ vỡ hay các tài sản cố định hữu hình khác, các tài sản cố định vô hình, các tài sản cố định đặc thù,...</li>
+            </ul>
+            <p>Ví dụ một số tài sản được phân lần lượt vào các nhóm như sau:</p>
+            <ul>
+               <li><b>Mặt bằng: </b>như nhà văn hóa,....</li>
+                <li><b>Xe cộ: </b>một số tài sản như xe ô tô, xe mô tô/gắn máy,...</li>
+                <li><b>Máy móc: </b>bao gồm một số loại tài sản như máy sưởi, máy hút bụi,....</li>
+                <li><b>Khác: </b> gồm một số tài sản như cây xanh, bản quyền phần mềm, ứng dụng,....</li>
+            </ul>`,
+            width: "50%",
+        })
+    }
+
     return (
         <div className="box box-solid">
             <div className="box-header">
-                <div className="box-title">Thống kê tài sản theo nhóm</div>
+                <div style={{ marginRight: '5px' }} className="box-title">Thống kê tài sản theo nhóm</div>
+                <a className="text-red" title={'Giải thích các nhóm tài sản'} onClick={showDetailAssetGroup}>
+                    <i className="fa fa-question-circle" style={{ cursor: 'pointer', color: '#dd4b39' }} />
+                </a>
             </div>
             <div className="box-body qlcv">
                 <div id="statisticalAssetByGroup"></div>
