2453c381f6fcc5b1672ddb673a92f621cd977c02
Phuc Nguyen <phucnm10@fpt.com.vn>
2021-11-08 21:51:00 +0700

    update import links

--- client/src/modules/super-admin/link/components/modalImportLinkPrivilege.jsx
+++ client/src/modules/super-admin/link/components/modalImportLinkPrivilege.jsx
@@ -20,6 +20,7 @@ function ModalImportLinkPrivilege(props) {
 
 
 
+    // config data for import
     function configurationImport() {
         let config = {
             sheets: { // Tên các sheet
@@ -62,6 +63,41 @@ function ModalImportLinkPrivilege(props) {
         return config;
     }
 
+    // config data for show
+    function configurationShow() {
+        let config = {
+            sheets: { // Tên các sheet
+                description: 'Thông tin phân quyền trang',
+                value: ["Thông tin phân quyền trang"]
+            },
+            rowHeader: { // Số dòng tiêu đề của bảng
+                description: 'Số dòng tiêu đề của bảng',
+                value: 1
+            },
+            linkUrl: {
+                columnName: translate('manage_link.url'),
+                description: translate('manage_link.url'),
+                value: translate('manage_link.url'),
+            },
+            linkCategory: {
+                columnName: translate('manage_link.category'),
+                description: translate('manage_link.category'),
+                value: translate('manage_link.category'),
+            },
+            linkDescription: {
+                columnName: translate('manage_link.description'),
+                description: translate('manage_link.description'),
+                value: translate('manage_link.description'),
+            },
+            linkRoles: {
+                columnName: translate('manage_link.roles'),
+                description: translate('manage_link.roles'),
+                value: translate('manage_link.roles'),
+            }
+        }
+        return config;
+    }
+
     const getLinkRoleId = (linkRole) => {
         const { list } = role;
         let result = [];
@@ -101,10 +137,12 @@ function ModalImportLinkPrivilege(props) {
 
                 // Lấy link import
                 let linkImport = link.list.filter(link => link.url === x.linkUrl);
-                let linkDescription = x.linkDescription.columnName;
+                let linkDescription = x.linkDescription;
+                let linkCategory = x.linkCategory;
                 let xLinkRolesConcatenate = [];
+
                 role.list?.length && role.list.forEach((y, index) => {
-                    // Thêm các column là tên các role trong hệ thống
+                    // Kiểm tra nếu cột của role đánh x thì concat linkRole
                     if (x[y.name] === "x") {
                         xLinkRolesConcatenate[index] = y.name
                     }
@@ -118,11 +156,15 @@ function ModalImportLinkPrivilege(props) {
 
                 // Kiểm tra link hợp lệ: link có trong ds các link trong hệ thống không
                 let validLink = link.list.some(link => link.url === x.linkUrl);
+
+                // Kiểm tra danh mục
+                let validCategory = link.list.some(link => link.category === linkCategory);
+
                 // Kiểm tra role hợp lệ, role nhập vào có trong ds các role có trong hệ thống ko
                 let validRole = linkRoles.every(linkRoleId => role.list.map(role => role._id).includes(linkRoleId));
 
 
-                if (x.linkUrl === null || !validLink || (linkRoles.length > 0 && !validRole)) {
+                if (x.linkUrl === null || linkCategory === null || !validCategory || !validLink || (linkRoles.length > 0 && !validRole)) {
                     rowError = [...rowError, index + 1];
                     x = { ...x, error: true };
                 }
@@ -133,20 +175,25 @@ function ModalImportLinkPrivilege(props) {
                 if (!validLink) {
                     errorAlert = [...errorAlert, 'Tên đường link của trang không hợp lệ'];
                 }
+                if (linkCategory === null) {
+                    errorAlert = [...errorAlert, 'Tên danh mục của trang không được để trống'];
+                }
+                if (!validCategory) {
+                    errorAlert = [...errorAlert, 'Tên danh mục của trang không hợp lệ'];
+                }
                 if (linkRoles.length > 0 && !validRole) {
                     errorAlert = [...errorAlert, 'Tên role được truy cập trang không hợp lệ'];
                 }
 
-                console.log(x);
                 // dữ liệu nguyên thủy như trong file import để show ra
                 valueShow = [
                     ...valueShow,
                     {
-                        ...x,
-                        // linkCategory: x.linkCategory,
-                        // linkDescription: x.linkDescription,
-                        // linkRoles: x.linkRoles,
-                        // linkUrl: x.linkUrl,
+                        linkCategory: x.linkCategory,
+                        linkDescription: x.linkDescription,
+                        linkRoles: x.linkRoles,
+                        linkUrl: x.linkUrl,
+                        error: x.error,
                         errorAlert: errorAlert
                     }
                 ]
@@ -201,7 +248,7 @@ function ModalImportLinkPrivilege(props) {
 
         if (valueImport) {
             for (let k = 0; k < valueImport.length; k++) {
-                // chỉ update những link có thay đổi so với các link hiện tại. Kiểm tra thỏa mãn cả 3 điều kiện id, mô tả, roles thì không update
+                // chỉ update những link có thay đổi so với các link hiện tại và hợp lệ. Kiểm tra thỏa mãn cả 3 điều kiện id, mô tả, roles thì không update
                 if (valueImport[k].valid && !link.list.some(link => link._id === valueImport[k].linkId && link.description === valueImport[k].description && isEqualArray(link.roles.map(role => role && role.roleId ? role.roleId._id : ""), valueImport[k].roles))) {
                     props.editLink(valueImport[k].linkId, {
                         url: valueImport[k].url,
@@ -219,6 +266,7 @@ function ModalImportLinkPrivilege(props) {
 
 
     const config = configurationImport();
+    const configShow = configurationShow();
     if (link?.error?.rowError !== undefined) {
         rowError = link.error.rowError;
         valueShow = valueShow.map((x, index) => {
@@ -430,7 +478,7 @@ function ModalImportLinkPrivilege(props) {
             <div className="col-md-12 col-xs-12">
                 <ShowImportData
                     id={`import_list_task`}
-                    configData={config}
+                    configData={configShow}
                     importData={valueShow}
                     rowError={rowError}
                     scrollTable={true}
