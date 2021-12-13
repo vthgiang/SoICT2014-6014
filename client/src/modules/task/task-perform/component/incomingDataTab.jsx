21a9f78635fb1db884de53beb5510fb8ddf4b0ff
TheNhatAT <nhatnt.vnist@gmail.com>
2021-12-13 13:04:45 +0700

    nhat: sửa phần nhận diện đuôi file khi hiển thị

--- client/src/modules/task/task-perform/component/incomingDataTab.jsx
+++ client/src/modules/task/task-perform/component/incomingDataTab.jsx
@@ -26,7 +26,7 @@ function IncomingDataTab(props) {
         }
     }
     const isImage = (src) => {
-        let string = src.split(".")
+        let string = src.toLowerCase().split(".");
         let image = ['jpg', 'jpeg', 'png', 'tiff', 'gif']
         if (image.indexOf(string[string.length - 1]) !== -1) {
             return true;
