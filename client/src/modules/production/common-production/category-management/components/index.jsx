3c93a848536956973d50e32e7f25c0120ed48a69
nghianmVnist <nghianm.vnist@gmail.com>
2022-01-27 10:15:17 +0700

    can chinh code

--- client/src/modules/production/common-production/category-management/components/index.jsx
+++ client/src/modules/production/common-production/category-management/components/index.jsx
@@ -4,29 +4,25 @@ import { withTranslate } from 'react-redux-multilingual';
 import CategoryManagementTable from './categoryManagementTable';
 import CategoryManagementTree from './categoryManagementTree';
 import {CategoryActions} from '../redux/actions';
-
 import { LazyLoadComponent, forceCheckOrVisible } from '../../../../../common-components/index';
 
-
 function CategoryManagement(props) {
-    
     const [state, setState] = useState({  
         currentRole: localStorage.getItem("currentRole"),
         page: 1,
         limit: 5
-    })
+    });
 
     const updateCategoryTree = async () => {
         let { page, limit, currentRole } = state;
         await props.getCategoryToTree({ page, limit, managementLocation: currentRole });
         forceCheckOrVisible(true, false);
-    }
+    };
 
     const updateCategoryTable = async () => {
         await props.getCategories();
         forceCheckOrVisible(true, false);
-    }
-
+    };
 
     const { translate } = props;
     return (
@@ -36,19 +32,14 @@ function CategoryManagement(props) {
                     <li className="active"><a href="#category-table" data-toggle="tab" onClick={() => updateCategoryTable()}>{translate('manage_warehouse.bin_location_management.category_table')}</a></li>
                     <li><a href="#category-tree" data-toggle="tab" onClick={() => updateCategoryTree()}>{translate('manage_warehouse.bin_location_management.category_tree')}</a></li>
                 </ul>
-
                 <div className="tab-content">
-
                     <div className="tab-pane active" id="category-table">
-                        <LazyLoadComponent
-                        >
+                        <LazyLoadComponent>
                             <CategoryManagementTable />
                         </LazyLoadComponent>
                     </div>
-
                     <div className="tab-pane" id="category-tree">
-                        <LazyLoadComponent
-                        >
+                        <LazyLoadComponent>
                             <CategoryManagementTree />
                         </LazyLoadComponent>
                     </div>
