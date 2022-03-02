135031895ff7456e7b9453dca448f39d7d8ae537
Tuananh <anhnt.vnist@gmail.com>
2022-03-02 18:34:39 +0700

    download file

--- server/modules/human-resource/bidding-package/biddingPackage.controller.js
+++ server/modules/human-resource/bidding-package/biddingPackage.controller.js
@@ -197,8 +197,13 @@ exports.getBiddingPackageDocument = async (req, res) => {
                 setTimeout(() => {
                     console.log("gửi file");
                     res.download(rootPath + "/document.zip");
-                    // xong rồi xóa thư mục đi
-                    if (fs.existsSync(`${SERVER_BACKUP_DIR}/${req.portal}/document`)) {
+                }, 3000);
+                // xong rồi xóa thư mục đi
+                setTimeout(() => {
+                    console.log("xóa file");
+                    if (
+                        fs.existsSync(`${SERVER_BACKUP_DIR}/${req.portal}/document`)
+                    ) {
                         exec(
                             `rm -rf ${SERVER_BACKUP_DIR}/${req.portal}/document`,
                             function (err) {
