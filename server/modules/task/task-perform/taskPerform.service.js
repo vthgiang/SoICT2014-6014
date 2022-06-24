4cdf3900a162544d7e75a26e5b5405d58de0c9cf
hoanganh121199 <anhvmh.vnist@gmail.com>
2022-06-24 15:13:16 +0700

    lấy chi tiết thông tin quy trình mẫu trong gettaskbyid

--- server/modules/task/task-perform/taskPerform.service.js
+++ server/modules/task/task-perform/taskPerform.service.js
@@ -78,7 +78,7 @@ exports.getTaskById = async (portal, id, userId, thirdParty = false) => {
       { path: "hoursSpentOnTask.contributions.employee", select: "name" },
       {
         path: "process",
-        populate: {
+        populate: [{
           path: "tasks",
           populate: [
             { path: "parent", select: "name" },
@@ -136,7 +136,8 @@ exports.getTaskById = async (portal, id, userId, thirdParty = false) => {
               select: "name email avatar",
             },
           ],
-        },
+        },{ path: 'processTemplate' },
+            { path: 'processTemplate', populate: { path: 'processTemplates.process' } }],
       },
       { path: "overallEvaluation.responsibleEmployees.employee", select: "_id name" },
       { path: "overallEvaluation.accountableEmployees.employee", select: "_id name" },
