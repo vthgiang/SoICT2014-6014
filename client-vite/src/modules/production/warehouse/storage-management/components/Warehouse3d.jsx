221b353ce71c9729e58217debb146dd3aebd19e6
janglee83 <giangle602@gmail.com>
2024-03-31 19:42:01 +0700

    Hotfix: fix jquery
    (cherry picked from commit da16ad91427408dac6d87e2737d4e42e6d92ebbd)

--- client-vite/src/modules/production/warehouse/storage-management/components/Warehouse3d.jsx
+++ client-vite/src/modules/production/warehouse/storage-management/components/Warehouse3d.jsx
@@ -1,19 +1,14 @@
-import React, { useEffect } from 'react';
-import $ from 'jquery';
+import React, { useEffect } from 'react'
+import $ from 'jquery'
 
 const Warehouse3d = () => {
-    useEffect(() => {
-        // var scriptType = "";
-        if (window.location.pathname.includes('/storage-management')) {
-            $.getScript("library/warehouse/myWarehouseVisualizer.js");
-        }
-    }, []);
+  useEffect(() => {
+    if (window.location.pathname.includes('/storage-management')) {
+      $.getScript('library/warehouse/myWarehouseVisualizer.js')
+    }
+  }, [])
 
-    return (
-        <div>
-
-        </div>
-    )
+  return <div></div>
 }
 
-export default Warehouse3d;
+export default Warehouse3d
--- client-vite/vite.config.js
+++ client-vite/vite.config.js
@@ -1,23 +1,30 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import inject from "@rollup/plugin-inject";
+import inject from '@rollup/plugin-inject'
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        define: {
-            'process.env.REACT_APP_SERVER': JSON.stringify(env.REACT_APP_SERVER)
-        },
-        plugins: [
-            inject({   // => that should be first under plugins array
-                $: 'jquery',
-                jQuery: 'jquery',
-            }),
-            react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      'process.env.REACT_APP_SERVER': JSON.stringify(env.REACT_APP_SERVER)
+    },
+    resolve: {
+      alias: {
+        jquery: 'jquery/dist/jquery.js' // Use the full version path
+      }
+    },
+    plugins: [
+      inject({
+        // => that should be first under plugins array
+        $: 'jquery',
+        jQuery: 'jquery'
+      }),
+      react()
+    ],
 
-        server: {
-            port: 3000,
-            open: true // Automatically open the default browser when starting the server
-        }
+    server: {
+      port: 3000,
+      open: true // Automatically open the default browser when starting the server
     }
+  }
 })
