766bf36f2dc41babd73332c4b9978725e2ae41e5
TuPhamPC <96640532+TuPhamPC@users.noreply.github.com>
2024-05-25 01:38:18 +0700

    feat/build-sales-statistics

---
+++ client-vite/src/modules/production/order/forecast/components/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+function Profit(props) {
+  return (
+    <div className='box' style={{ minHeight: '450px' }}>
+      <div className='box-body'>
+        <h1>Forecast</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Profit
