46b66c092e700f979dd347bd6c748e6dc294991d
TuPhamPC <96640532+TuPhamPC@users.noreply.github.com>
2024-05-25 01:38:18 +0700

    feat/build-sales-statistics

---
+++ client-vite/src/modules/production/order/sales-statistics/components/optionalAttributeCreateForm.jsx
@@ -0,0 +1,203 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { withTranslate } from 'react-redux-multilingual'
+import { ErrorLabel, SelectBox } from '../../../../../common-components'
+import Swal from 'sweetalert2'
+import ValidationHelper from '../../../../../helpers/validationHelper'
+
+function OptionalAttributeCreateForm(props) {
+  const [state, setState] = useState({
+    detailInfo: []
+  })
+
+  const handleAddDetailInfo = () => {
+    var detailInfo = state.detailInfo
+
+    if (detailInfo.length !== 0) {
+      let result
+
+      for (let n in detailInfo) {
+        result = validateNameField(detailInfo[n].nameField, n) && validateValue(detailInfo[n].value, n)
+        if (!result) {
+          validateNameField(detailInfo[n].nameField, n)
+          validateValue(detailInfo[n].value, n)
+          break
+        }
+      }
+
+      if (result) {
+        setState((state) => {
+          return {
+            ...state,
+            detailInfo: [...detailInfo, { nameField: '', value: '' }]
+          }
+        })
+      }
+    } else {
+      setState((state) => {
+        return {
+          ...state,
+          detailInfo: [...detailInfo, { nameField: '', value: '' }]
+        }
+      })
+    }
+  }
+
+  const handleChangeNameField = (e, index) => {
+    var { value } = e.target
+    validateNameField(value, index)
+  }
+  const validateNameField = (value, className, willUpdateState = true) => {
+    let { message } = ValidationHelper.validateEmpty(props.translate, value)
+
+    if (willUpdateState) {
+      var { detailInfo } = state
+      detailInfo[className] = { ...detailInfo[className], nameField: value }
+      setState((state) => {
+        return {
+          ...state,
+          errorOnNameField: message,
+          errorOnNameFieldPosition: message ? className : null,
+          detailInfo: detailInfo
+        }
+      })
+      props.handleChange(detailInfo)
+    }
+    return message === undefined
+  }
+
+  const handleChangeValue = (e, index) => {
+    var { value } = e.target
+    validateValue(value, index)
+  }
+
+  const validateValue = (value, className, willUpdateState = true) => {
+    let { message } = ValidationHelper.validateEmpty(props.translate, value)
+
+    if (willUpdateState) {
+      var { detailInfo } = state
+      detailInfo[className] = { ...detailInfo[className], value: value }
+      setState((state) => {
+        return {
+          ...state,
+          errorOnValue: message,
+          errorOnValuePosition: message ? className : null,
+          detailInfo: detailInfo
+        }
+      })
+      props.handleChange(detailInfo)
+    }
+    return message === undefined
+  }
+
+  const delete_function = (index) => {
+    var { detailInfo } = state
+    detailInfo.splice(index, 1)
+    if (detailInfo.length !== 0) {
+      for (let n in detailInfo) {
+        validateNameField(detailInfo[n].nameField, n)
+        validateValue(detailInfo[n].value, n)
+      }
+    } else {
+      setState((state) => {
+        return {
+          ...state,
+          detailInfo: detailInfo,
+          errorOnValue: undefined,
+          errorOnNameField: undefined
+        }
+      })
+    }
+  }
+
+  const showListExplainOptionalAttribute = () => {
+    Swal.fire({
+      icon: 'question',
+
+      html: `<h3 style="color: red"><div>Các thuộc tính tùy chọn</div> </h3>
+            <div style="font-size: 1.3em; text-align: left; margin-top: 15px; line-height: 1.7">
+            <p>Cho phép người dùng tạo các thuộc tính thêm của hàng hóa ngoài các thuộc tính mặc định có sẵn</p>`,
+      width: '50%'
+    })
+  }
+
+  const { translate, id } = props
+  let { detailInfo, errorOnNameField, errorOnNameFieldPosition, errorOnValue, errorOnValuePosition } = state
+  return (
+    <fieldset className='scheduler-border'>
+      <legend className='scheduler-border'>
+        {'Thêm thuộc tính tùy chọn'}
+        <a onClick={() => showListExplainOptionalAttribute()}>
+          <i className='fa fa-question-circle' style={{ cursor: 'pointer', marginLeft: '5px' }} />
+        </a>
+      </legend>
+
+      <div className='form-group'>
+        <p type='button' className='btn btn-success' onClick={handleAddDetailInfo}>
+          Thêm mới
+        </p>
+      </div>
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th style={{ paddingLeft: '0px' }}>{translate('asset.asset_info.field_name')}</th>
+            <th style={{ paddingLeft: '0px' }}>{translate('asset.asset_info.value')}</th>
+            <th style={{ width: '120px', textAlign: 'center' }}>{translate('table.action')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!detailInfo || detailInfo.length === 0 ? (
+            <tr>
+              <td colSpan={3}>
+                <center> {translate('table.no_data')}</center>
+              </td>
+            </tr>
+          ) : (
+            detailInfo.map((x, index) => {
+              return (
+                <tr key={index}>
+                  <td style={{ paddingLeft: '0px' }}>
+                    <div className={`form-group ${parseInt(errorOnNameFieldPosition) === index && errorOnNameField ? 'has-error' : ''}`}>
+                      <input
+                        className='form-control'
+                        type='text'
+                        value={x.nameField}
+                        name='nameField'
+                        style={{ width: '100%' }}
+                        onChange={(e) => handleChangeNameField(e, index)}
+                      />
+                      {parseInt(errorOnNameFieldPosition) === index && errorOnNameField && <ErrorLabel content={errorOnNameField} />}
+                    </div>
+                  </td>
+
+                  <td style={{ paddingLeft: '0px' }}>
+                    <div className={`form-group ${parseInt(errorOnValuePosition) === index && errorOnValue ? 'has-error' : ''}`}>
+                      <input
+                        className='form-control'
+                        type='text'
+                        value={x.value}
+                        name='value'
+                        style={{ width: '100%' }}
+                        onChange={(e) => handleChangeValue(e, index)}
+                      />
+                      {parseInt(errorOnValuePosition) === index && errorOnValue && <ErrorLabel content={errorOnValue} />}
+                    </div>
+                  </td>
+
+                  <td style={{ textAlign: 'center' }}>
+                    <a className='delete' title='Delete' data-toggle='tooltip' onClick={() => delete_function(index)}>
+                      <i className='material-icons'></i>
+                    </a>
+                  </td>
+                </tr>
+              )
+            })
+          )}
+        </tbody>
+      </table>
+    </fieldset>
+  )
+}
+
+export default connect(null, null)(withTranslate(OptionalAttributeCreateForm))
---
+++ client-vite/src/modules/production/order/sales-statistics/redux/services.js
@@ -0,0 +1,184 @@
+import { sendRequest } from '../../../../../helpers/requestHelper'
+
+export const GoodServices = {
+  getGoodsByType,
+  getAllGoods,
+  getAllGoodsByType,
+  getAllGoodsByCategory,
+  getGoodDetail,
+  deleteGood,
+  getTaxByGoodsId,
+  getSlaByGoodsId,
+  getDiscountByGoodsId,
+  getGoodByManageWorkRole,
+  getManufacturingWorksByProductId,
+  getNumberGoods,
+  importGood
+}
+
+function getGoodsByType(params) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods`,
+      method: 'GET',
+      params
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getAllGoods(params) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/all-goods`,
+      method: 'GET',
+      params
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getAllGoodsByType(query) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-type`,
+      method: 'GET',
+      params: query
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getAllGoodsByCategory(id) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-category/${id}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+
+
+function getGoodDetail(id) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+
+
+function deleteGood(id) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
+      method: 'DELETE'
+    },
+    true,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getTaxByGoodsId(goodId) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/tax/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.tax'
+  )
+}
+
+function getSlaByGoodsId(goodId) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/sla/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.sla'
+  )
+}
+
+function getDiscountByGoodsId(goodId) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/discount/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.discount'
+  )
+}
+
+function getGoodByManageWorkRole(roleId) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-manage-works-role/role/${roleId}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getManufacturingWorksByProductId(productId) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/get-works-by-product-id/${productId}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+function getNumberGoods() {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/get-number-good`,
+      method: 'GET'
+    },
+    false,
+    false,
+    'manage_warehouse.good_management'
+  )
+}
+
+function importGood(data) {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/imports`,
+      method: 'POST',
+      data
+    },
+    true,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
