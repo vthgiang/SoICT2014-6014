c7352141397173287673ffc078346b0b2c9ddb1b
tungnguyensnk <tungnguyensnk@gmail.com>
2024-06-04 09:01:02 +0700

    vehicle component

--- client-vite/src/modules/transport3/vehicle/components/vehicleManagementTable.jsx
+++ client-vite/src/modules/transport3/vehicle/components/vehicleManagementTable.jsx
@@ -1,265 +1,160 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { withTranslate } from "react-redux-multilingual";
-import { vehicleActions } from "../redux/actions";
-import { DataTableSetting, DeleteNotification, PaginateBar } from "../../../../common-components";
-import VehicleCreateForm from "./vehicleCreateForm";
-import VehicleEditForm from "./vehicleEditForm";
-import VehicleDetailInfo from "./vehicleDetailInfo";
-import { useEffect } from "react";
-import { getTableConfiguration } from '../../../../helpers/tableConfiguration';
-import { CategoryActions } from '../../../production/common-production/category-management/redux/actions';
-import { CalculateVehicleCost } from "./calculateCost";
-import { TransportationCostManagementActions } from "../../cost/redux/actions";
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {withTranslate} from 'react-redux-multilingual';
+import {vehicleActions} from '../redux/actions';
+import {DataTableSetting, DeleteNotification, PaginateBar} from '@common-components';
+import VehicleDetailInfo from './vehicleDetailInfo';
+import {useEffect} from 'react';
+import {getTableConfiguration} from '@helpers/tableConfiguration';
+import {CategoryActions} from '@modules/production/common-production/category-management/redux/actions';
+import {formatDate} from '@helpers/formatDate.js';
 
 const VehicleManagementTable = (props) => {
-    const getTableId = "table-manage-vehicle";
-    const defaultConfig = { limit: 5 }
-    const getLimit = getTableConfiguration(getTableId, defaultConfig).limit;
+  const getTableId = 'table-manage-vehicle';
+  const defaultConfig = {limit: 5}
+  const getLimit = getTableConfiguration(getTableId, defaultConfig).limit;
 
-    const { vehicle, translate, categories, transportationCostManagement } = props;
+  const {vehicle, translate, categories, transportationCostManagement} = props;
 
-    const [state, setState] = useState({
-        vehicleName: "",
-        description: "",
-        page: 1,
-        perPage: getLimit,
-        vehicleEdit: null,
-        vehicleId: null,
-        tableId: getTableId,
-        vehiclesWithCosts: [],
-        costFormula: "",
-    })
-    const { vehicleName, page, perPage, vehicleEdit, vehicleId, vehiclesWithCosts, costFormula } = state;
+  const [state, setState] = useState({
+    vehicleName: '',
+    description: '',
+    page: 1,
+    perPage: getLimit,
+    vehicleId: null,
+    tableId: getTableId,
+  })
+  const {vehicleName, page, perPage, vehicleEdit, vehicleId, vehicleWithCosts, costFormula} = state;
 
-    useEffect(() => {
-        let { vehicleName, perPage } = state;
-        props.getAllVehicle({ page, perPage });
-        props.getCategories();
-        props.getAllVehicleWithCostList();
-        props.getFormula();
-    }, [])
-    useEffect(() => {
-        setState({
-            ...state,
-            vehiclesWithCosts: vehicle.vehicleWithCosts ? vehicle.vehicleWithCosts : [],
-            costFormula: transportationCostManagement?.formula ? transportationCostManagement.formula.vehicle : "",
-        })
-    }, [vehicle, transportationCostManagement])
+  useEffect(() => {
+    let {perPage} = state;
+    props.getAllVehicle({page, perPage});
+  }, [])
 
-    const handleChangeVehicleName = (e) => {
-        const { value } = e.target;
-        setState({
-            ...state,
-            vehicleName: value
-        });
-    }
+  console.log(props.listVehicle)
 
-    const handleSubmitSearch = () => {
-        setState({
-            ...state,
-            page: 1
-        });
-        props.getAllVehicle(state);
-    }
+  const setPage = (pageNumber) => {
+    setState({
+      ...state,
+      page: parseInt(pageNumber)
+    });
+    props.getAllVehicle({page: parseInt(pageNumber), perPage: perPage});
+  }
 
-    const setPage = (pageNumber) => {
-        setState({
-            ...state,
-            page: parseInt(pageNumber)
-        });
-        props.getAllVehicle({page: parseInt(pageNumber), perPage: perPage});
-    }
+  const setLimit = (number) => {
+    setState({
+      ...state,
+      perPage: parseInt(number)
+    });
+    props.getAllVehicle({page: page, perPage: parseInt(number)});
+  }
 
-    const setLimit = (number) => {
-        setState({
-            ...state,
-            perPage: parseInt(number)
-        });
-        props.getAllVehicle({page: page, perPage:  parseInt(number)});
-    }
+  const handleShowDetailInfo = (id) => {
 
-    const handleDelete = (id) => {
-        props.deleteVehicles({
-            vehicleIds: [id]
-        });
-        props.getAllVehicle({
-            vehicleName,
-            perPage,
-            page: vehicle?.lists?.length === 1 ? page - 1 : page
-        });
-    }
+    setState({
+      ...state,
+      vehicleId: id
+    });
 
-    const handleEdit = (vehicle) => {
-        setState({
-            ...state,
-            vehicleEdit: vehicle
-        });
-        window.$('#modal-edit-vehicle').modal('show');
-    }
+    window.$(`#modal-detail-info-vehicle`).modal('show');
+  }
 
-    const handleShowDetailInfo = (id) => {
+  const columns = [
+    'STT',
+    'Mã phương tiện',
+    'Tên phương tiện',
+    'Trọng tải',
+    'Thể tích thùng',
+    'Mức tiêu thụ nhiên liệu',
+    'Chi phí vận chuyển',
+    'Hạn bảo hành',
+    'Tốc độ',
+    'Trạng thái',
+  ]
 
-        setState({
-            ...state,
-            vehicleId: id
-        });
-
-        window.$(`#modal-detail-info-vehicle`).modal('show');
-    }
-
-    const updateOperationCost = () => {
-        let vehiclesOperationCost = [];
-        if (costFormula == "" || vehiclesWithCosts.length == 0) {
-            return;
+  return (
+    <>
+      <VehicleDetailInfo
+        vehicleId={state.vehicleId}
+        categories={categories}
+      />
+      <div className="box-body qlcv">
+        <div className="form-inline">
+          <div className="form-group">
+            {/* Button thêm mới */}
+            <button type="button" className="btn btn-success"
+                    title={translate('manage_transportation.vehicle_management.add')}
+                    onClick={() => window.$('#modal-add-vehicle').modal('show')}>
+              {translate('manage_transportation.vehicle_management.add')}
+            </button>
+          </div>
+        </div>
+        <table id={state.tableId} className="table table-striped table-bordered table-hover"
+               style={{marginTop: '20px'}}>
+          <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={index}>{column}</th>
+            ))}
+            <th style={{width: '120px', textAlign: 'center'}}>{translate('table.action')}
+              <DataTableSetting
+                tableId={state.tableId}
+                columnArr={columns}
+                setLimit={setLimit}
+              />
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          {props.listVehicle && props.listVehicle.length !== 0 &&
+            props.listVehicle.map((vehicle, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{vehicle.code}</td>
+                <td>{vehicle.assetName}</td>
+                <td>{vehicle.tonnage}</td>
+                <td>{vehicle.volume}</td>
+                <td>{vehicle.averageGasConsume}</td>
+                <td>{vehicle.averageFeeTransport}</td>
+                <td>{formatDate(vehicle.warrantyExpirationDate)}</td>
+                <td>{vehicle.minVelocity} - {vehicle.maxVelocity}</td>
+                <td>{vehicle.status === 'ready_to_use' ? 'Sẵn sàng sử dụng' : 'Không sẵn sàng sử dụng'}</td>
+                <td style={{textAlign: 'center'}}>
+                  <a onClick={() => handleShowDetailInfo(vehicle._id)}><i className="material-icons">visibility</i></a>
+                  <a><i className="material-icons">edit</i></a>
+                  <DeleteNotification
+                    content={translate('manage_transportation.vehicle_management.delete')}
+                    data={{id: vehicle._id}}
+                    // func={handleDeleteVehicle}
+                  />
+                </td>
+              </tr>
+            ))
+          }
+          </tbody>
+        </table>
+        {!props.listVehicle || props.listVehicle.length === 0 &&
+          <div className="table-info-panel">{translate('confirm.no_data')}</div>
         }
-        let formula = costFormula;
-        vehiclesWithCosts.forEach((vehicleWithCost) => {
-            let calculatedCost = CalculateVehicleCost.result(formula, vehicleWithCost);
-            vehiclesOperationCost.push({
-                _id: vehicleWithCost._id,
-                vehicleCost: calculatedCost.fixedCost,
-                averageFeeTransport: calculatedCost.operationCost
-            })
-        });
-        props.calculateVehiclesCost(vehiclesOperationCost);
-    }
-
-
-    let lists = [];
-    if (vehicle.isLoading === false) {
-        lists = vehicle.listVehicle;
-    }
-    let listLicenseTexts = [];
-    if (lists) {
-        lists.forEach((vehicle) => {
-            let listLicenseText = "";
-            if (vehicle.requireLicense) {
-                vehicle.requireLicense.forEach((license) => listLicenseText+= `Hạng ${license}, `);
-                listLicenseText = listLicenseText.slice(0, -2);
-            }
-            if (listLicenseText) {
-                listLicenseTexts.push({vehicle: vehicle._id, text: listLicenseText});
-            }
-        })
-    }
-
-    const totalPage = Math.ceil(vehicle.totalList / perPage);
-    const { tableId } = state;
-    return (
-        <React.Fragment>
-            {
-                <VehicleEditForm
-                    vehicleEdit={vehicleEdit}
-                    categories={categories}
-                />
-            }
-            {
-                <VehicleDetailInfo
-                    vehicleId={state.vehicleId}
-                    categories={categories}
-                />
-            }
-            <div className="box-body qlcv">
-                <VehicleCreateForm
-                    page={page}
-                    perPage={perPage}
-                    categories={categories}
-                />
-                <div className="form-inline">
-                    <div className="form-group">
-                        <label className="form-control-static">{translate('manage_transportation.vehicle_management.name')}</label>
-                        <input type="text" className="form-control" name="vehicleName" onChange={handleChangeVehicleName} placeholder={translate('manage_transportation.vehicle_management.name')} autoComplete="off" />
-                    </div>
-                    <div className="form-group">
-                        {/* Button thêm mới */}
-                        <button type="button" className="btn btn-success" title={translate('manage_transportation.vehicle_management.search')} onClick={handleSubmitSearch}>{translate('manage_transportation.vehicle_management.search')}</button>
-                        {/* Button cập nhật chi phí vận hành đội xe */}
-                        <button type="button" className="btn btn-info" title={translate('manage_transportation.vehicle_management.search')} onClick={updateOperationCost}>{translate('manage_transportation.vehicle_management.update_operation_cost_button')}</button>
-                    </div>
-                </div>
-                <table id={tableId} className="table table-striped table-bordered table-hover" style={{marginTop: "20px"}}>
-                    <thead>
-                        <tr>
-                            <th className="col-fixed" style={{ width: 60 }}>{translate('manage_transportation.vehicle_management.index')}</th>
-                            <th>{translate('manage_transportation.vehicle_management.name')}</th>
-                            <th>{translate('manage_transportation.vehicle_management.operation_cost')}</th>
-                            <th>{translate('manage_transportation.vehicle_management.require_license')}</th>
-                            <th style={{ width: "120px", textAlign: "center" }}>{translate('table.action')}
-                                <DataTableSetting
-                                    tableId={tableId}
-                                    columnArr={[
-                                        translate('manage_transportation.vehicle_management.index'),
-                                        translate('manage_transportation.vehicle_management.name'),
-                                        translate('manage_transportation.vehicle_management.operation_cost'),
-                                        translate('manage_transportation.vehicle_management.require_license'),
-                                    ]}
-                                    setLimit={setLimit}
-                                />
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(lists && lists.length !== 0) &&
-                            lists.map((vehicle, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1 + (page - 1) * state.perPage}</td>
-                                    <td>{vehicle.name}</td>
-                                    <td>{vehicle.vehicleCost}</td>
-                                    <td>
-                                        {
-                                            listLicenseTexts && listLicenseTexts.filter((license) => license.vehicle == vehicle._id)[0] ?
-                                            listLicenseTexts.filter((license) => license.vehicle == vehicle._id)[0].text : ""
-                                        }
-                                    </td>
-                                    <td style={{ textAlign: "center" }}>
-                                        <a className="edit text-green" style={{ width: '5px' }} title={translate('manage_transportation.vehicle_management.detail_info_vehicle')} onClick={() => handleShowDetailInfo(vehicle._id)}><i className="material-icons">visibility</i></a>
-                                        <a className="edit text-yellow" style={{ width: '5px' }} title={translate('manage_transportation.vehicle_management.edit')} onClick={() => handleEdit(vehicle)}><i className="material-icons">edit</i></a>
-                                        <DeleteNotification
-                                            content={translate('manage_transportation.vehicle_management.delete')}
-                                            data={{
-                                                id: vehicle._id,
-                                                info: vehicle.name
-                                            }}
-                                            func={handleDelete}
-                                        />
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-                {vehicle.isLoading ?
-                    <div className="table-info-panel">{translate('confirm.loading')}</div> :
-                    (typeof lists === 'undefined' || lists.length === 0) && <div className="table-info-panel">{translate('confirm.no_data')}</div>
-                }
-                <PaginateBar
-                    pageTotal={totalPage ? totalPage : 0}
-                    currentPage={page}
-                    func={setPage}
-                />
-            </div>
-        </React.Fragment>
-    )
+        <PaginateBar
+          pageTotal={vehicle.totalPage ? vehicle.totalPage : 0}
+          currentPage={state.page}
+          func={setPage}
+        />
+      </div>
+    </>
+  )
 
 }
 
 
 function mapStateToProps(state) {
-    const vehicle = state.vehicle;
-    const categories = state.categories;
-    const transportationCostManagement = state.transportationCostManagement;
-    return { vehicle, categories, transportationCostManagement }
+  const {listVehicle} = state.T3vehicle;
+  return {listVehicle, vehicle: state.T3vehicle};
 }
 
 const mapDispatchToProps = {
-    getAllVehicle: vehicleActions.getAllVehicle,
-    deleteVehicles: vehicleActions.deleteVehicles,
-    getCategories: CategoryActions.getCategories,
-    getAllFreeVehicleSchedule: vehicleActions.getAllFreeVehicleSchedule,
-    getAllVehicleWithCostList: vehicleActions.getAllVehicleWithCostList,
-    getFormula: TransportationCostManagementActions.getFormula,
-    calculateVehiclesCost: vehicleActions.calculateVehiclesCost,
+  getAllVehicle: vehicleActions.getAllVehicle
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withTranslate(VehicleManagementTable));
--- client-vite/src/modules/transport3/vehicle/redux/reducers.js
+++ client-vite/src/modules/transport3/vehicle/redux/reducers.js
@@ -1,132 +1,32 @@
-import { vehicleConstants } from './constants';
-
-var findIndex = (array, id) => {
-    var result = -1;
-    array.forEach((value, index) => {
-        if (value._id === id) {
-            result = index;
-        }
-    });
-    return result;
-}
+import { vehicleConstants } from './constants'
 
 const initialState = {
-    listVehicle: [],
-    vehicleWithCosts: [],
-    todayVehiclesSchedule: [],
-    isLoading: false,
-    error: null,
-    totalList: 0,
+  listVehicle: [],
+  isLoading: false,
+  error: null,
+  totalList: 0
 }
 
 export function vehicle(state = initialState, action) {
-    let index = -1;
-    switch (action.type) {
-        case vehicleConstants.GET_ALL_ONLY_VEHICLE_NAME_REQUEST:
-        case vehicleConstants.DELETE_VEHICLE_REQUEST:
-        case vehicleConstants.CREATE_VEHICLE_REQUEST:
-        case vehicleConstants.EDIT_VEHICLE_REQUEST:
-        case vehicleConstants.GET_VEHICLE_DETAIL_REQUEST:
-        case vehicleConstants.GET_ALL_VEHICLE_WITH_CONDITION_REQUEST:
-        case vehicleConstants.GET_ALL_VEHICLE_SCHEDULE_REQUEST:
-        case vehicleConstants.GET_VEHICLES_COST_REQUEST:
-        case vehicleConstants.CALCULATE_VEHICLES_COST_REQUEST:
-            return {
-                ...state,
-                isLoading: true
-            }
-        case vehicleConstants.GET_ALL_ONLY_VEHICLE_NAME_FAILURE:
-        case vehicleConstants.DELETE_VEHICLE_FAILURE:
-        case vehicleConstants.CREATE_VEHICLE_FAILURE:
-        case vehicleConstants.EDIT_VEHICLE_FAILURE:
-        case vehicleConstants.GET_ALL_VEHICLE_WITH_CONDITION_FAILURE:
-        case vehicleConstants.GET_ALL_VEHICLE_SCHEDULE_FAILURE:
-        case vehicleConstants.GET_VEHICLES_COST_FAILURE:
-        case vehicleConstants.CALCULATE_VEHICLES_COST_FAILURE:
-            return {
-                ...state,
-                isLoading: false,
-                error: action.error
-            }
-        // Danh sách toàn bộ xe
-        case vehicleConstants.GET_ALL_ONLY_VEHICLE_NAME_SUCCESS:
-            return {
-                ...state,
-                listVehicle: action.payload.data,
-                totalList: action.payload.totalList,
-                isLoading: false
-            }
-        case vehicleConstants.GET_VEHICLES_COST_SUCCESS:
-            return {
-                ...state,
-                vehicleWithCosts: action.payload.data,
-                isLoading: false
-            }
-        case vehicleConstants.GET_ALL_VEHICLE_WITH_CONDITION_SUCCESS:
-            return {
-                ...state,
-                listVehicle: action.payload.data,
-                totalList: action.payload.totalList,
-                isLoading: false
-            }
-        case vehicleConstants.DELETE_VEHICLE_SUCCESS:
-            return {
-                ...state,
-                listVehicle: state.listVehicle.filter(vehicle => !action.vehicleIds.includes(vehicle?._id)),
-                isLoading: false
-            }
-        case vehicleConstants.CREATE_VEHICLE_SUCCESS:
-            return {
-                ...state,
-                listVehicle: [
-                    ...state.listVehicle,
-                    action.payload
-                ],
-                isLoading: false
-            }
-        case vehicleConstants.EDIT_VEHICLE_SUCCESS:
-            index = findIndex(state.listVehicle, action.payload?._id);
-            if (index !== -1) {
-                state.listVehicle[index] = action.payload
-            }
-            return {
-                ...state,
-                isLoading: false
-            }
-        case vehicleConstants.GET_VEHICLE_DETAIL_SUCCESS:
-            return {
-                ...state,
-                currentDetailVehicle: action.payload,
-                isLoading: false
-            }
-        case vehicleConstants.GET_ALL_VEHICLE_SCHEDULE_SUCCESS: {
-            return {
-                ...state,
-                todayVehiclesSchedule: action.payload,
-                isLoading: false
-            }
-        }
-        case vehicleConstants.CALCULATE_VEHICLES_COST_SUCCESS: {
-            return {
-                ...state,
-                listVehicle: action.payload,
-                isLoading: false
-            }
-        }
-        case vehicleConstants.UPDATE_REALTIME_VEHICLE_STATUS: {
-            let listVehicleUpdate = state.listVehicle;
-            console.log("Vehicle", action.payload);
-            let updateData = action.payload;
-            let index = listVehicleUpdate.findIndex((vehicle) => vehicle._id == updateData.vehicleId);
-            if (index != -1) {
-                listVehicleUpdate[index].status = updateData.status
-            }
-            return {
-                ...state,
-                listVehicle: listVehicleUpdate
-            }
-        }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case vehicleConstants.GET_ALL_VEHICLE_REQUEST:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case vehicleConstants.GET_ALL_VEHICLE_SUCCESS:
+      return {
+        ...state,
+        listVehicle: action.payload.data,
+        isLoading: false
+      }
+    case vehicleConstants.GET_ALL_VEHICLE_FAILURE:
+      return {
+        ...state,
+        error: action.error,
+        isLoading: false
+      }
+    default:
+      return state
+  }
 }
