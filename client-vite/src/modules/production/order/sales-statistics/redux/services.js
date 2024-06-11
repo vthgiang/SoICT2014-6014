7ca04b590155afa3e6f8353d4b04931f7f79907a
TuPhamPC <96640532+TuPhamPC@users.noreply.github.com>
2024-06-11 14:48:16 +0700

    fix sales-statistics service

--- client-vite/src/modules/production/order/sales-statistics/redux/services.js
+++ client-vite/src/modules/production/order/sales-statistics/redux/services.js
@@ -1,5 +1,168 @@
 import { sendRequest } from '../../../../../helpers/requestHelper'
 
+const getGoodsByType = (params) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods`,
+      method: 'GET',
+      params
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getAllGoods = (params) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/all-goods`,
+      method: 'GET',
+      params
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getAllGoodsByType = (query) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-type`,
+      method: 'GET',
+      params: query
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getAllGoodsByCategory = (id) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-category/${id}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getGoodDetail = (id) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const deleteGood = (id) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
+      method: 'DELETE'
+    },
+    true,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getTaxByGoodsId = (goodId) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/tax/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.tax'
+  )
+}
+
+const getSlaByGoodsId = (goodId) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/sla/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.sla'
+  )
+}
+
+const getDiscountByGoodsId = (goodId) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/discount/get-by-good-id`,
+      method: 'GET',
+      params: { goodId }
+    },
+    false,
+    true,
+    'manage_order.discount'
+  )
+}
+
+const getGoodByManageWorkRole = (roleId) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/by-manage-works-role/role/${roleId}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getManufacturingWorksByProductId = (productId) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/get-works-by-product-id/${productId}`,
+      method: 'GET'
+    },
+    false,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
+const getNumberGoods = () => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/get-number-good`,
+      method: 'GET'
+    },
+    false,
+    false,
+    'manage_warehouse.good_management'
+  )
+}
+
+const importGood = (data) => {
+  return sendRequest(
+    {
+      url: `${process.env.REACT_APP_SERVER}/goods/imports`,
+      method: 'POST',
+      data
+    },
+    true,
+    true,
+    'manage_warehouse.good_management'
+  )
+}
+
 export const GoodServices = {
   getGoodsByType,
   getAllGoods,
@@ -14,171 +177,4 @@ export const GoodServices = {
   getManufacturingWorksByProductId,
   getNumberGoods,
   importGood
-}
-
-function getGoodsByType(params) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods`,
-      method: 'GET',
-      params
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getAllGoods(params) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/all-goods`,
-      method: 'GET',
-      params
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getAllGoodsByType(query) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/by-type`,
-      method: 'GET',
-      params: query
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getAllGoodsByCategory(id) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/by-category/${id}`,
-      method: 'GET'
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-
-
-function getGoodDetail(id) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
-      method: 'GET'
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-
-
-function deleteGood(id) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/${id}`,
-      method: 'DELETE'
-    },
-    true,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getTaxByGoodsId(goodId) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/tax/get-by-good-id`,
-      method: 'GET',
-      params: { goodId }
-    },
-    false,
-    true,
-    'manage_order.tax'
-  )
-}
-
-function getSlaByGoodsId(goodId) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/sla/get-by-good-id`,
-      method: 'GET',
-      params: { goodId }
-    },
-    false,
-    true,
-    'manage_order.sla'
-  )
-}
-
-function getDiscountByGoodsId(goodId) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/discount/get-by-good-id`,
-      method: 'GET',
-      params: { goodId }
-    },
-    false,
-    true,
-    'manage_order.discount'
-  )
-}
-
-function getGoodByManageWorkRole(roleId) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/by-manage-works-role/role/${roleId}`,
-      method: 'GET'
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getManufacturingWorksByProductId(productId) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/get-works-by-product-id/${productId}`,
-      method: 'GET'
-    },
-    false,
-    true,
-    'manage_warehouse.good_management'
-  )
-}
-
-function getNumberGoods() {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/get-number-good`,
-      method: 'GET'
-    },
-    false,
-    false,
-    'manage_warehouse.good_management'
-  )
-}
-
-function importGood(data) {
-  return sendRequest(
-    {
-      url: `${process.env.REACT_APP_SERVER}/goods/imports`,
-      method: 'POST',
-      data
-    },
-    true,
-    true,
-    'manage_warehouse.good_management'
-  )
 }
