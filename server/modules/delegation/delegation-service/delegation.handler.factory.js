12641346037ac47e802a9481c640275ef5a10c84
Duc Lam <dinhduclam2001@gmail.com>
2024-06-17 05:28:18 +0700

    makes delegation handler factory create singleton handlers

--- server/modules/delegation/delegation-service/delegation.handler.factory.js
+++ server/modules/delegation/delegation-service/delegation.handler.factory.js
@@ -2,14 +2,26 @@ const { RoleDelegationHandler } = require('./role.delegation.handler')
 const { TaskDelegationHandler } = require('./task.delegation.handler')
 const { ResourceDelegationHandler } = require('./resource.delegation.handler')
 
+let roleDelegationHandler = new RoleDelegationHandler();
+let taskDelegationHandler = new TaskDelegationHandler();
+let resourceDelegationHandler = new ResourceDelegationHandler();
 exports.createDelegationHandler = (type) => {
     switch (type) {
         case 'Role':
-            return new RoleDelegationHandler();
+            if (!roleDelegationHandler) {
+                roleDelegationHandler = new RoleDelegationHandler();
+            }
+            return roleDelegationHandler;
         case 'Task':
-            return new TaskDelegationHandler();
+            if (!taskDelegationHandler) {
+                taskDelegationHandler = new TaskDelegationHandler();
+            }
+            return taskDelegationHandler;
         case 'Resource':
-            return new ResourceDelegationHandler();
+            if (!resourceDelegationHandler) {
+                resourceDelegationHandler = new ResourceDelegationHandler();
+            }
+            return resourceDelegationHandler;
         default:
             throw ['delegate_type_invalid'];
     }
