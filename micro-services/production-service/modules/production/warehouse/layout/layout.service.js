feea7db7114ca0324725986e58426a41fe3c884a
giangltvnist <152389528+giangltvnist@users.noreply.github.com>
2024-07-09 17:06:52 +0000

    Update code

--- micro-services/production-service/modules/production/warehouse/layout/layout.service.js
+++
@@ -1,9 +0,0 @@
-const { Layout } = require("../../../../models");
-const { connect } = require("../../../../helpers/dbHelper");
-const { Routes } = require("../../../../models");
-exports.getAllLayouts = async (portal) => {
-  return Layout(connect(DB_CONNECTION, portal))
-    .find()
-    .select("-_id -inventory -__v");
-};
-
