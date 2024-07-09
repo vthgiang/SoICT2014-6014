8dede68071d8cff7750be899f62a2651a3aecf6c
Duc Lam <dinhduclam2001@gmail.com>
2024-07-10 01:32:55 +0700

    fix missing function

--- micro-services/security-control-service/modules/auth-service/resource/resource.service.js
+++ micro-services/security-control-service/modules/auth-service/resource/resource.service.js
@@ -8,3 +8,33 @@ exports.findByIds = async (portal, ids) => {
     .populate('refId');
   return resources.map((x) => enrich(x));
 };
+
+const enrich = (resource) => {
+  let enrichResource = {
+    id: resource.id,
+    name: resource.name,
+    attributes: resource.attributes,
+    type: resource.type
+  };
+
+  let additionalInfo = {};
+  switch (resource.type) {
+    case 'Component':
+      additionalInfo.description = resource.refId?.description;
+      break;
+    case 'Link':
+      additionalInfo.description = resource.refId?.description;
+      additionalInfo.category = resource.refId?.category;
+      break;
+    case 'SystemApi':
+      additionalInfo.method = resource.refId?.method;
+      additionalInfo.category = resource.refId?.category;
+      break;
+    case 'Task':
+      additionalInfo.status = resource.refId?.status;
+      break;
+  }
+
+  enrichResource.additionalInfo = additionalInfo;
+  return enrichResource;
+}
