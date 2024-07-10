41fb529f4f49632af3cc1a5c3d7517816e2c4c29
tungnguyensnk <tungnguyensnk@gmail.com>
2024-07-11 01:23:20 +0700

    add map order detail

--- client-vite/src/modules/transport3/order/components/orderDetail.jsx
+++ client-vite/src/modules/transport3/order/components/orderDetail.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import {withTranslate} from 'react-redux-multilingual'
-import {formatDate, formatToTimeZoneDate} from '@helpers/formatDate'
+import {formatDate} from '@helpers/formatDate'
 import {DialogModal} from '@common-components'
 import '@modules/crm/customer/components/customer.css'
-import {MapContainer} from 'react-leaflet';
+import {MapContainer, Marker, TileLayer, useMap} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function OrderDetail(props) {
   const {order} = props
@@ -43,70 +43,90 @@ function OrderDetail(props) {
           <>
             <div className="row">
               <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Mã đơn hàng: </strong>
-                  <span>{order.code}</span>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Mã đơn hàng: </strong>
+                      <span>{order.code}</span>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Loại hình vận chuyển: </strong>
+                      <span>{transportType[order.transportType]}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Khách hàng: </strong>
+                      <span>{order.customer.name}</span>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Địa chỉ: </strong>
+                      <span>{order.address}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Độ ưu tiên: </strong>
+                      <span>{priority[order.priority]}</span>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Ghi chú địa chỉ: </strong>
+                      <span>{order.noteAddress}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Ghi chú vận đơn: </strong>
+                      <span>{order.note}</span>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Thời gian giao hàng dự kiến: </strong>
+                      <span>{order.deliveryTime ? formatDate(new Date(order.deliveryTime * 1000)) : '---'}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Trạng thái: </strong>
+                      <span>{status[order.status]}</span>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <strong>Thời gian tạo: </strong>
+                      <span>{formatDate(order.createdAt)}</span>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="form-group">
-                  <strong>Loại hình vận chuyển: </strong>
-                  <span>{transportType[order.transportType]}</span>
+                  <MapContainer center={[order.lat, order.lng]} zoom={13} style={{height: '300px', width: '600px'}}>
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <CenteredMarker position={[order.lat, order.lng]}
+                                    iconUrl={order.transportType === 1 ? '/image/delivery.png' : '/image/receive.png'}/>
+                  </MapContainer>
                 </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Khách hàng: </strong>
-                  <span>{order.customer.name}</span>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Địa chỉ: </strong>
-                  <span>{order.address}</span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Độ ưu tiên: </strong>
-                  <span>{priority[order.priority]}</span>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Trạng thái: </strong>
-                  <span>{status[order.status]}</span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <strong>Thời gian giao hàng dự kiến: </strong>
-                  <span>{order.deliveryTime ? formatDate(new Date(order.deliveryTime * 1000)) : '---'}</span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div className="form-group">
-                  <strong>Ghi chú vận đơn: </strong>
-                  <span>{order.note}</span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div className="form-group">
-                  <strong>Ghi chú địa chỉ: </strong>
-                  <span>{order.noteAddress}</span>
-                </div>
-              </div>
-            </div>
+
             <div className="row">
               <div className="col-md-12">
                 <div className="form-group">
@@ -143,9 +163,22 @@ function OrderDetail(props) {
   )
 }
 
-function mapStateToProps(state) {
-}
+const CenteredMarker = ({position, iconUrl}) => {
+  const map = useMap();
 
-const mapDispatchToProps = {}
+  React.useEffect(() => {
+    map.setView(position, map.getZoom());
+  }, [position, map]);
 
-export default connect(mapStateToProps, mapDispatchToProps)(withTranslate(OrderDetail))
+  return (
+    <Marker
+      position={position}
+      icon={L.icon({
+        iconUrl: iconUrl,
+        iconSize: [40, 40]
+      })}
+    />
+  );
+};
+
+export default withTranslate(OrderDetail)
