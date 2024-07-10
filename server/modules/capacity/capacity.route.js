2da6afabe647ae318c18830bffc36e07afad5c94
chumanhtien <tiencm194182@gmail.com>
2024-07-10 23:59:32 +0700

    Add CRUD capacity APIs server

--- server/modules/capacity/capacity.route.js
+++ server/modules/capacity/capacity.route.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const CapacityController = require('./capacity.controller')
 const { auth } = require('../../middleware')
 
-router.get('/', auth, CapacityController.getListCapacities)
+router.get('/', CapacityController.getListCapacities)
+router.get('/:id', CapacityController.getOneCapacity)
+router.post('/', CapacityController.createNewCapacity)
+router.patch('/:id', CapacityController.updateCapacity)
+router.delete('/:id', CapacityController.deleteCapacity)
 
 module.exports = router
--- server/modules/capacity/capacity.service.js
+++ server/modules/capacity/capacity.service.js
@@ -56,3 +56,68 @@ exports.getAllCapacities = async (portal, params) => {
     }
   }
 }
+
+exports.createNewCapacity = async (portal, data) => {
+  try {
+    const existingCapacity = await Capacity(connect(DB_CONNECTION, portal)).findOne({
+      $or: [
+        { name: data.name },
+        { key: data.key }
+      ]
+    });
+
+    if (existingCapacity) {
+      throw ['capacity_already_exist']
+    }
+
+    const newCapacity = await Capacity(connect(DB_CONNECTION, portal)).create({
+      ...data
+    });
+    return newCapacity
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.getOneCapacity = async (portal, id) => {
+  try {
+    let findCapacity = await Capacity(connect(DB_CONNECTION, portal)).findOne({
+      _id: id
+    })
+    return findCapacity
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.updateCapacity = async (portal, id, data) => {
+  try {
+    let findCapacity = await Capacity(connect(DB_CONNECTION, portal)).findOne({
+      _id: id
+    })
+    if (!findCapacity) {
+      throw ['capacity_not_found']
+    }
+    const updatedCapacity = await Capacity(connect(DB_CONNECTION, portal)).findByIdAndUpdate(id, data, { new: true });
+    return updatedCapacity
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.deleteCapacity = async (portal, id) => {
+  try {
+    let findCapacity = await Capacity(connect(DB_CONNECTION, portal)).findOne({
+      _id: id
+    })
+    if (!findCapacity) {
+      throw ['capacity_not_found']
+    }
+    await Capacity(connect(DB_CONNECTION, portal)).deleteOne({
+      _id: id,
+    })
+    return findCapacity
+  } catch (error) {
+    throw error
+  }
+} 
